e5db7460e3c594c62c521c1547c445f063318b27
Ellieutt <elliottdenis@hotmail.fr>
2017-11-10 15:55:09 +0100

    readd interface type

--- src/types.ts
+++ src/types.ts
@@ -4,3 +4,10 @@ export type CallbackTransactionHash = (transactionHash:string) => void;
 export type CallbackTransactionReceipt = (receipt:Object) => void;
 export type CallbackTransactionConfirmation = (confirmationNumber:number, receipt:Object) => void;
 export type CallbackTransactionError = (error:Error) => void;
+
+export interface Artifact {
+    abi: any;
+    networks: {[networkId: number]: {
+        address: string;
+    }};
+}
