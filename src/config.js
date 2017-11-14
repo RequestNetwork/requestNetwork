6123600876a220ae42dc6a528147f7874736e4db
Ellieutt <elliottdenis@hotmail.fr>
2017-11-14 15:09:02 +0100

    delete duplicate js files

--- src/config.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-exports.__esModule = true;
-// import * as Config from './config.json';
-var Config = require('./config.json');
-exports["default"] = Config;
--- src/types.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var State;
-(function (State) {
-    State[State["Created"] = 0] = "Created";
-    State[State["Accepted"] = 1] = "Accepted";
-    State[State["Declined"] = 2] = "Declined";
-    State[State["Canceled"] = 3] = "Canceled";
-})(State = exports.State || (exports.State = {}));
