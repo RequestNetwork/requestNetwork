bf3218a62b0f0412eff222a9b31aa510559a6cfc
vrolland <vincent.rolland.38@gmail.com>
2018-01-02 12:05:33 +0100

    doc ipfs

--- src/servicesExternal/ipfs-service.ts
+++ src/servicesExternal/ipfs-service.ts
@@ -3,27 +3,46 @@ import * as Types from '../types';
 // import ipfs from 'ipfs-api';
 import ipfsAPI = require('ipfs-api');
 
-
+/**
+ * The Ipfs class is the singleton class containing the ipfs node interface
+ */
 export default class Ipfs {
 	private static _instance:Ipfs;
 
 	public ipfs: any;
 
+    /**
+     * Private constructor to Instantiates a new Ipfs 
+     * @param   _publicIpfs   if true, use the public node otherwise a private one
+     */
 	private constructor(_publicIpfs : boolean) {
 		let ipfsConfig = config.ipfs.nodeUrlDefault[_publicIpfs?'public':'private'];
 		this.ipfs = ipfsAPI(ipfsConfig.host, ipfsConfig.port, {protocol: ipfsConfig.protocol})
 	}
 
+    /**
+     * Initialized the class Web3Single
+     * @param   _publicIpfs    _publicIpfs   if true, use the public node otherwise a private one
+     */
     public static init(_publicIpfs : boolean = true) 
     {   
         this._instance = new this(_publicIpfs);
     }
 
+    /**
+     * get the instance of Ipfs
+     * @return  The instance of the Ipfs class.
+     */
     public static getInstance() 
     {
         return this._instance;
     }
 
+    /**
+     * Add a file in ipfs
+     * @param    _data    data that will be store in ipfs
+     * @return   promise of the hash of the created file
+     */
 	public addFile(_data:string) : Promise<any>
 	{
         return new Promise((resolve, reject) => {
@@ -39,6 +58,11 @@ export default class Ipfs {
 		});
 	}
 
+    /**
+     * get a file from ipfs from its hash
+     * @param    _hash	hash of the file to get	
+     * @return   promise of the data of the file 
+     */
 	public getFile(_hash:string) : Promise<string>
 	{
         return new Promise((resolve, reject) => {
