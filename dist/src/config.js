2484c34988125842f90374593a62d46d5ae1073c
vrolland <vincent.rolland.38@gmail.com>
2018-01-12 10:30:30 +0800

    Delete dist/ on github

--- dist/src/config.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// import * as config from './config.json';
-var config = require('./config.json');
-exports.default = config;
-//# sourceMappingURL=config.js.map
