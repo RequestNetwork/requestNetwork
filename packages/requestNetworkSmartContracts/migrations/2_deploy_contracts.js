0e292a1ab0a8abb846289b77d047d2af61f0ee01
vrolland <vincent.rolland.38@gmail.com>
2018-02-21 11:14:48 +0800

    migration script with fees off the core

--- packages/requestNetworkSmartContracts/migrations/2_deploy_contracts.js
+++ packages/requestNetworkSmartContracts/migrations/2_deploy_contracts.js
@@ -1,6 +1,5 @@
 var RequestCore = artifacts.require("./RequestCore.sol");
 var RequestEthereum = artifacts.require("./RequestEthereum.sol");
-var RequestBurnManagerSimple = artifacts.require("./RequestBurnManagerSimple.sol");
 
 var addressContractBurner = 0;
 var feesPerTenThousand = 10; // 0.1 %
@@ -8,16 +7,12 @@ var feesPerTenThousand = 10; // 0.1 %
 
 var requestCore;
 var requestEthereum;
-var requestBurnManagerSimple;
-
 module.exports = function(deployer) {
     deployer.deploy(RequestCore).then(function() {
         return deployer.deploy(RequestEthereum, RequestCore.address).then(function() {
-            return deployer.deploy(RequestBurnManagerSimple, addressContractBurner).then(function() {
-                createInstances().then(function() {
-                    setupContracts().then(function() {
-                        checks()
-                    });
+            createInstances().then(function() {
+                setupContracts().then(function() {
+                    checks()
                 });
             });
         });
@@ -29,33 +24,20 @@ var createInstances = function() {
         requestCore = instance;
         return RequestEthereum.deployed().then(function(instance) {
             requestEthereum = instance;
-            return RequestBurnManagerSimple.deployed().then(function(instance) {
-                requestBurnManagerSimple = instance;
-                console.log("Instances set.")
-            });
+            console.log("Instances set.")
         });
     });
 }
 
 var setupContracts = function() {
-    return requestBurnManagerSimple.setFeesPerTenThousand(feesPerTenThousand).then(function() {
-        return requestCore.setBurnManager(requestBurnManagerSimple.address).then(function() {
-            return requestCore.adminAddTrustedCurrencyContract(requestEthereum.address).then(function() {
-                console.log("Contracts set up.")
-            });
-        });
+    return requestCore.adminAddTrustedCurrencyContract(requestEthereum.address).then(function() {
+        console.log("Contracts set up.")
     });
 }
 
 var checks = function() {
   requestCore.getStatusContract(requestEthereum.address).then(function(d) {
     console.log("getStatusContract: " + requestEthereum.address + " => " + d)
-  });
-  requestBurnManagerSimple.feesPer10000().then(function(d) {
-    console.log("trustedNewBurnManager %% => " + d);
-  });
-  requestCore.trustedNewBurnManager().then(function(d) {
-    console.log("trustedNewBurnManager manager => " + d);
     console.log("Checks complete")
   });
 }
