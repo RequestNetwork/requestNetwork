a05e93e65627811bd83b64ec96da29bbe96e7cd8
vrolland <vincent.rolland.38@gmail.com>
2018-03-15 11:24:40 +0800

    testunit cancel erc20

---
+++ packages/requestNetwork.js/test/unit/erc20Services/cancel.ts
@@ -0,0 +1,202 @@
+import {expect} from 'chai';
+import 'mocha';
+import requestArtifacts from 'requestnetworkartifacts';
+import RequestNetwork from '../../../src/requestNetwork';
+import Erc20Service from '../../../src/servicesExternal/erc20-service';
+import * as utils from '../../utils';
+
+const WEB3 = require('web3');
+const BN = WEB3.utils.BN;
+
+const addressRequestERC20 = requestArtifacts('private', 'last-RequestErc20').networks.private.address;
+const addressRequestCore = requestArtifacts('private', 'last-RequestCore').networks.private.address;
+
+let rn: any;
+let web3: any;
+let defaultAccount: string;
+let payer: string;
+let payee: string;
+let payee2: string;
+let payee3: string;
+let otherGuy: string;
+
+let coreVersion: any;
+let currentNumRequest: any;
+
+let requestId: any;
+
+describe('cancel', () => {
+    const arbitraryAmount = 100000000;
+    rn = new RequestNetwork('http://localhost:8545', 10000000000, false);
+    web3 = rn.requestERC20Service.web3Single.web3;
+    const testToken = new Erc20Service('0xf25186B5081Ff5cE73482AD761DB0eB0d25abfBF');
+    const addressTestToken = testToken.getAddress();
+
+    beforeEach(async () => {
+        const accounts = await web3.eth.getAccounts();
+        defaultAccount = accounts[0].toLowerCase();
+        payer = accounts[2].toLowerCase();
+        payee = accounts[3].toLowerCase();
+        otherGuy = accounts[4].toLowerCase();
+        
+        currentNumRequest = await rn.requestCoreService.getCurrentNumRequest();
+
+        const req = await rn.requestERC20Service.createRequestAsPayee(
+            addressTestToken,
+            [payee],
+            [arbitraryAmount],
+            payer,
+            [],
+            0,
+            '',
+            '',
+            [],
+            {from: payee});
+
+        requestId = req.request.requestId;
+    });
+
+    it('cancel request with not valid requestId', async () => {
+        try {
+            const result = await rn.requestERC20Service.cancel(
+                                '0x00000000000000',
+                                {from: payer});
+            expect(false, 'exception not thrown').to.be.true; 
+        } catch (e) {
+            utils.expectEqualsObject(e, Error('_requestId must be a 32 bytes hex string (eg.: \'0x0000000000000000000000000000000000000000000000000000000000000000\''),'exception not right');
+        }
+    });
+
+    it('cancel request by payer when created', async () => {
+        const result = await rn.requestERC20Service.cancel(
+                                requestId,
+                                {from: payer})
+            .on('broadcasted', (data: any) => {
+                expect(data.transaction, 'data.transaction.hash is wrong').to.have.property('hash');
+            });
+
+        utils.expectEqualsBN(result.request.payee.expectedAmount, arbitraryAmount, 'expectedAmount is wrong');
+        utils.expectEqualsBN(result.request.payee.balance, 0, 'balance is wrong');
+        expect(result.request.creator.toLowerCase(), 'creator is wrong').to.equal(payee);
+        expect(result.request.extension, 'extension is wrong').to.be.undefined;
+        expect(result.request.payee.address.toLowerCase(), 'payee is wrong').to.equal(payee);
+        expect(result.request.payer.toLowerCase(), 'payer is wrong').to.equal(payer);
+        expect(result.request.requestId, 'requestId is wrong').to.equal(utils.getRequestId(addressRequestCore, ++currentNumRequest));
+        expect(result.request.state, 'state is wrong').to.equal(2);
+        expect(result.request.currencyContract.address.toLowerCase(), 'currencyContract is wrong').to.equal(addressRequestERC20);
+
+        expect(result.transaction, 'result.transaction.hash is wrong').to.have.property('hash');
+    });
+
+    it('cancel request by otherGuy', async () => {
+        try {
+            const result = await rn.requestERC20Service.cancel(
+                                requestId,
+                                {from: otherGuy});
+            expect(false, 'exception not thrown').to.be.true; 
+        } catch (e) {
+            utils.expectEqualsObject(e, Error('account must be the payer or the payee'),'exception not right');
+        }
+    })
+
+    it('cancel request by payer when not created', async () => {
+        await rn.requestERC20Service.accept(
+                                requestId,
+                                {from: payer});
+
+        try {
+            const result = await rn.requestERC20Service.cancel(
+                                requestId,
+                                {from: payer});
+            expect(false, 'exception not thrown').to.be.true; 
+        } catch (e) {
+            utils.expectEqualsObject(e, Error('payer can cancel request in state \'created\''),'exception not right');
+        }
+    })
+
+    it('cancel request by payee when cancel', async () => {
+        await rn.requestERC20Service.cancel(
+                                requestId,
+                                {from: payer});
+
+        try {
+            const result = await rn.requestERC20Service.cancel(
+                                requestId,
+                                {from: payee});
+            expect(false, 'exception not thrown').to.be.true; 
+        } catch (e) {
+            utils.expectEqualsObject(e, Error('payee cannot cancel request already canceled'),'exception not right');
+        }
+    })
+
+    it('cancel request by payee when created', async () => {
+        const result = await rn.requestERC20Service.cancel(
+                                requestId,
+                                {from: payee});
+
+        utils.expectEqualsBN(result.request.payee.expectedAmount, arbitraryAmount, 'expectedAmount is wrong');
+        utils.expectEqualsBN(result.request.payee.balance, 0, 'balance is wrong');
+        expect(result.request.creator.toLowerCase(), 'creator is wrong').to.equal(payee);
+        expect(result.request.extension, 'extension is wrong').to.be.undefined;
+        expect(result.request.payee.address.toLowerCase(), 'payee is wrong').to.equal(payee);
+        expect(result.request.payer.toLowerCase(), 'payer is wrong').to.equal(payer);
+        expect(result.request.requestId, 'requestId is wrong').to.equal(utils.getRequestId(addressRequestCore, ++currentNumRequest));
+        expect(result.request.state, 'state is wrong').to.equal(2);
+        expect(result.request.currencyContract.address.toLowerCase(), 'currencyContract is wrong').to.equal(addressRequestERC20);
+
+        expect(result.transaction, 'result.transaction.hash is wrong').to.have.property('hash');
+    });
+
+    it('cancel request by payee when accepted and balance == 0', async () => {
+        await rn.requestERC20Service.accept(
+                                requestId,
+                                {from: payer})
+            .on('broadcasted', (data: any) => {
+                expect(data.transaction, 'data.transaction.hash is wrong').to.have.property('hash');
+            });
+
+        const result = await rn.requestERC20Service.cancel(
+                                requestId,
+                                {from: payee})
+            .on('broadcasted', (data: any) => {
+                expect(data.transaction, 'data.transaction.hash is wrong').to.have.property('hash');
+            });
+
+        utils.expectEqualsBN(result.request.payee.expectedAmount, arbitraryAmount, 'expectedAmount is wrong');
+        
+        utils.expectEqualsBN(result.request.payee.balance, 0, 'balance is wrong');
+        expect(result.request.creator.toLowerCase(), 'creator is wrong').to.equal(payee);
+        expect(result.request.extension, 'extension is wrong').to.be.undefined;
+        expect(result.request.payee.address.toLowerCase(), 'payee is wrong').to.equal(payee);
+        expect(result.request.payer.toLowerCase(), 'payer is wrong').to.equal(payer);
+        expect(result.request.requestId, 'requestId is wrong').to.equal(utils.getRequestId(addressRequestCore, ++currentNumRequest));
+        expect(result.request.state, 'state is wrong').to.equal(2);
+        expect(result.request.currencyContract.address.toLowerCase(), 'currencyContract is wrong').to.equal(addressRequestERC20);
+
+        expect(result.transaction, 'result.transaction.hash is wrong').to.have.property('hash');
+    });
+
+    // TODO TODO TODO TODO
+    // it('cancel request by payee when accepted and balance != 0', async () => {
+
+    //     await rn.requestERC20Service.accept(
+    //                             requestId,
+    //                             {from: payer});
+
+    //     await rn.requestERC20Service.paymentAction(
+    //                     requestId,
+    //                     [1],
+    //                     [0],
+    //                     {from: payer});
+
+    //     try {
+    //         const result = await rn.requestERC20Service.cancel(
+    //                             requestId,
+    //                             {from: payee});
+    //         expect(false, 'exception not thrown').to.be.true; 
+    //     } catch (e) {
+    //         utils.expectEqualsObject(e, Error('impossible to cancel a Request with a balance != 0'),'exception not right');
+    //     }
+    // });
+
+});
