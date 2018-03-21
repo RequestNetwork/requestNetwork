f154b1c3a0b0c3bac3f5fe8725c2879c132d8fe0
romaric juniet <romaricjuniet@gmail.com>
2018-03-21 19:02:08 +0800

    Test refactor

--- packages/requestNetworkSmartContracts/test/core/burn-test.js
+++ packages/requestNetworkSmartContracts/test/core/burn-test.js
@@ -12,29 +12,33 @@ const KyberContract = artifacts.require("./test/vendor/KyberMock.sol");
 
 contract('burn contract ', accounts => {
   const admin = accounts[0];
+  let reqInstance = null;
+  let burnInstance = null;
 
-	// Creation and event
-	it("converts the ETH and burns the REQ", async function () {
+  beforeEach(async () => {
     // Create 1000 REQ
-    const reqInstance = await RequestToken.new(
+    reqInstance = await RequestToken.new(
       1000,
       0,
       admin,
       admin,
     );
-
+    
     // Create kyberMock
     const kyberInstance = await KyberContract.new();
     
     // Give 100 REQ (/e+18) to kyberMock
     reqInstance.transfer(kyberInstance.address, 100);
-
+    
     // Create burn contract
-    const burnInstance = await BurnContract.new(reqInstance.address, kyberInstance.address);
+    burnInstance = await BurnContract.new(reqInstance.address, kyberInstance.address);
     
     // Send 100 ETH (/e+18) to burn contract (simulates request creations fees)
     await burnInstance.send(100);
-    
+  })
+
+	// Creation and event
+	it("converts the ETH and burns the REQ", async () => {
     // Call burn. It will burn all 100 ETH
     await burnInstance.doBurn(0,0,0);
 
@@ -46,27 +50,7 @@ contract('burn contract ', accounts => {
   });
   
 	// Creation and event
-	it("converts a maximum of `maxSrcAmount` ETH", async function () {
-    // Create 1000 REQ
-    const reqInstance = await RequestToken.new(
-      1000,
-      0,
-      admin,
-      admin,
-    );
-
-    // Create kyberMock
-    const kyberInstance = await KyberContract.new();
-    
-    // Give 100 REQ (/e+18) to kyberMock
-    reqInstance.transfer(kyberInstance.address, 100);
-
-    // Create burn contract
-    const burnInstance = await BurnContract.new(reqInstance.address, kyberInstance.address);
-    
-    // Send 100 ETH (/e+18) to burn contract (simulates Request creation fees)
-    await burnInstance.send(100);
-    
+	it("converts a maximum of `maxSrcAmount` ETH", async () => {
     // Call burn. It will burn only 5 ETH
     await burnInstance.doBurn(5,0,0);
 
