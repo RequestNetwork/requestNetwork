cbc6424c1ebec723690efd7c20f0719c8a91f916
vrolland <vincent.rolland.38@gmail.com>
2018-04-17 16:24:22 +0800

    testunit ERC20CreateRequestAsPayer

--- packages/requestNetworkSmartContracts/test/config.js
+++ packages/requestNetworkSmartContracts/test/config.js
@@ -24,6 +24,7 @@ var config = {
 	//		},
 	//	 	"erc20.js": {
 				"requestERC20CreateRequestAsPayee.js":false,
+				"requestERC20CreateRequestAsPayer.js":false,
 				"requestERC20BroadcastSignedRequestAsPayer.js":false,
 				"requestERC20Accept.js":false,
 				"requestERC20Cancel.js":false,
