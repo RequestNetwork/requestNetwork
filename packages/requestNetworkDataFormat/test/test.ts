5d0a04142e4b446be5d997e301ae0252db800dbd
Bernardo Rittmeyer <bernardo@rittme.com>
2018-08-14 10:25:51 +0800

    Add currency to rnf_invoice items (#113)
    Add currency parameter to rnf_invoice invoiceItems objects.

--- packages/requestNetworkDataFormat/test/test.ts
+++ packages/requestNetworkDataFormat/test/test.ts
@@ -3,34 +3,34 @@ import 'mocha';
 import RequestNetworkDataFormat from '../src/index';
 
 describe('Request Network Data Validator', () => {
-    it('should valid a right json', () => {
+    it('should validate a correct json', () => {
       const dataJson = require('./data/example-valid.json');
       const result = RequestNetworkDataFormat.validate(dataJson);
       expect(result.valid, 'result.valid should be true').to.be.true;
     });
 
-    it('should not valid a json without meta', () => {
+    it('should not validate a json without meta', () => {
       const dataJson = require('./data/example-no-meta.json');
       const result = RequestNetworkDataFormat.validate(dataJson);
       expect(result.valid, 'result.valid should be false').to.be.false;
       expect(result.errors[0].message, 'result.errors is wrong').to.equal('meta not found');
     });
 
-    it('should not valid a json with meta unknown', () => {
+    it('should not validate a json with meta unknown', () => {
       const dataJson = require('./data/example-meta-unknown.json');
       const result = RequestNetworkDataFormat.validate(dataJson);
       expect(result.valid, 'result.valid should be false').to.be.false;
       expect(result.errors[0].message, 'result.errors is wrong').to.equal('format not found');
     });
 
-    it('should not valid a json with schema error', () => {
+    it('should not validate a json with schema error', () => {
       const dataJson = require('./data/example-schema-errors.json');
       const result = RequestNetworkDataFormat.validate(dataJson);
       expect(result.valid, 'result.valid should be false').to.be.false;
       expect(result.errors[0].message, 'result.errors is wrong').to.equal('should match format "date-time"');
     });
 
-    it('should not valid a json with required parameter missing', () => {
+    it('should not validate a json with required parameter missing', () => {
       const dataJson = require('./data/example-schema-missing.json');
       const result = RequestNetworkDataFormat.validate(dataJson);
       expect(result.valid, 'result.valid should be false').to.be.false;
