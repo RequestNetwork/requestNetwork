4ee6331b15d5b0d2e2e4c14efa2fda962b229da8
Romaric Juniet <romaric-juniet@users.noreply.github.com>
2018-12-19 18:33:04 +0800

    build: add webpack build (#188)
    Build a minified js file with webpack
    Example of usage in index.html
    Output is currently 525KB

--- packages/client-side/src/index.ts
+++ packages/client-side/src/index.ts
@@ -1,5 +1,6 @@
+import * as Types from '@requestnetwork/types';
 import Request from './api/request';
 import Utils from './api/utils';
 import { default as RequestNetwork } from './http-request-network';
 
-export { Request, RequestNetwork, Utils };
+export { Request, RequestNetwork, Types, Utils };
---
+++ packages/client-side/webpack.config.js
@@ -0,0 +1,59 @@
+/**
+ * This is to generate the umd bundle only
+ * From https://github.com/0xProject/0x-monorepo/blob/development/packages/0x.js/webpack.config.js
+ */
+const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const DuplicatePackageCheckerPlugin = require('duplicate-package-checker-webpack-plugin');
+
+module.exports = {
+  entry: {
+    'requestnetwork.min': './src/index.ts',
+  },
+  mode: 'production',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+    libraryTarget: 'umd',
+    library: 'RequestNetwork',
+    umdNamedDefine: true,
+  },
+  resolve: {
+    extensions: ['.ts', '.js', '.json'],
+    alias: {
+      // Dedup packages
+      'bn.js': path.resolve(__dirname, '../../node_modules/bn.js'),
+      'ethereumjs-util': path.resolve(__dirname, '../../node_modules/ethereumjs-util'),
+    },
+  },
+  devtool: 'source-map',
+  optimization: {
+    minimizer: [new TerserPlugin({ sourceMap: true })],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: [
+          {
+            loader: 'awesome-typescript-loader',
+            // tsconfig.json contains some options required for
+            // project references which do not work with webback.
+            // We override those options here.
+            query: {
+              declaration: false,
+              declarationMap: false,
+              composite: false,
+            },
+          },
+        ],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new BundleAnalyzerPlugin({ analyzerMode: 'static', openAnalyzer: false }),
+    new DuplicatePackageCheckerPlugin(),
+  ],
+};
