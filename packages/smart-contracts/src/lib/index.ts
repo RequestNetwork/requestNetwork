c47108cbcc0a1f71d38af6f4292e65df636de7f5
Lucas Bertrand <lucas.bertrand@ensimag.grenoble-inp.fr>
2019-12-04 18:02:44 +0800

    Move artifacts utilitaries

---
+++ packages/smart-contracts/src/lib/index.ts
@@ -0,0 +1,7 @@
+import * as requestHashStorageArtifact from './RequestHashStorageArtifact';
+import * as requestHashSubmitterArtifact from './RequestHashSubmitterArtifact';
+
+export default {
+  requestHashStorageArtifact,
+  requestHashSubmitterArtifact,
+};
