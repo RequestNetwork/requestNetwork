e4b1996925b8e92a805b4a9b2898e68c35eacfcf
Lucas Bertrand <lucas.bertrand@ensimag.grenoble-inp.fr>
2019-12-05 15:03:25 +0800

    Fix name of the package

--- packages/smart-contracts/src/lib/index.ts
+++ packages/smart-contracts/src/lib/index.ts
@@ -1,7 +1,7 @@
 import * as requestHashStorageArtifact from './RequestHashStorageArtifact';
 import * as requestHashSubmitterArtifact from './RequestHashSubmitterArtifact';
 
-export default {
+export {
   requestHashStorageArtifact,
   requestHashSubmitterArtifact,
 };
