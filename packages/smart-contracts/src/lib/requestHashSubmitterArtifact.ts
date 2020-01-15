3925655d0f977803e32e884719d91436df9c5e82
vrolland <vincent.rolland.38@gmail.com>
2020-01-15 15:23:09 +0800

    fix json require to work with webpack

--- packages/smart-contracts/src/lib/requestHashSubmitterArtifact.ts
+++ packages/smart-contracts/src/lib/requestHashSubmitterArtifact.ts
@@ -1,4 +1,4 @@
-import * as path from 'path';
+// import * as path from 'path';
 
 const artifactsRequestOpenHashSubmitter = require('../../artifacts/RequestHashSubmitter/RequestOpenHashSubmitter/artifacts.json');
 const ARTIFACTS_OPEN_STORAGE_LAST_VERSION: string = artifactsRequestOpenHashSubmitter.lastVersion;
@@ -11,10 +11,10 @@ export function getContractAbi(): any {
   const artifactFilename: string =
     artifactsRequestOpenHashSubmitter[ARTIFACTS_OPEN_STORAGE_LAST_VERSION].artifact;
 
-  const artifact = require(path.join(
-    '../../artifacts/RequestHashSubmitter/RequestOpenHashSubmitter/',
-    artifactFilename,
-  ));
+  const artifact = require(`../../artifacts/RequestHashSubmitter/RequestOpenHashSubmitter/${artifactFilename.replace(
+    /\.[^/.]+$/,
+    '',
+  )}.json`);
 
   // Check the abi exists inside the artifact file
   if (!artifact.abi) {
