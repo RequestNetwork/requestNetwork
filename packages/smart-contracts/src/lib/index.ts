a891d91988dd6aeb42ba5873f0c1c2b3be2b02be
vrolland <vincent.rolland.38@gmail.com>
2020-02-11 17:54:12 +0800

    add ethereum proxy contract

--- packages/smart-contracts/src/lib/index.ts
+++ packages/smart-contracts/src/lib/index.ts
@@ -1,8 +1,10 @@
 import * as erc20ProxyArtifact from './erc20ProxyArtifact';
+import * as ethProxyArtifact from './ethProxyArtifact';
 import * as requestHashStorageArtifact from './requestHashStorageArtifact';
 import * as requestHashSubmitterArtifact from './requestHashSubmitterArtifact';
 
 export {
+  ethProxyArtifact,
   erc20ProxyArtifact,
   requestHashStorageArtifact,
   requestHashSubmitterArtifact,
