89d40034542f62398ca7d12d80daeeaa273b97d1
Benjamin LEVESQUE <14175665+benjlevesque@users.noreply.github.com>
2020-04-14 19:20:09 +0200

    typo

--- packages/payment-processor/src/payment/transaction-overrides.ts
+++ packages/payment-processor/src/payment/transaction-overrides.ts
@@ -1,4 +1,4 @@
 import { TransactionRequest } from 'ethers/providers';
 
-/** Custom values to pass to transcaction */
+/** Custom values to pass to transaction */
 export interface ITransactionOverrides extends Omit<TransactionRequest, 'to' | 'data' | 'value'> {}
