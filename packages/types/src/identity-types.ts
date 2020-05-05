6913c1b71d8e7ae08dffc6ae52d789534d5b7cd9
vrolland <vincent.rolland.38@gmail.com>
2020-05-05 10:20:02 +0200

    to-delete

--- packages/types/src/identity-types.ts
+++ packages/types/src/identity-types.ts
@@ -4,9 +4,12 @@ export interface IIdentity {
   type: TYPE;
   // the identification itself
   value: string;
+  // used only for the type ETHEREUM_SMART_CONTRACT
+  network?: string;
 }
 
 /** Supported identity types */
 export enum TYPE {
   ETHEREUM_ADDRESS = 'ethereumAddress',
+  ETHEREUM_SMART_CONTRACT = 'ethereumSmartContract',
 }
