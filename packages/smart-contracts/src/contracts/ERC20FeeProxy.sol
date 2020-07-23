e90235a9ea637d74fe97bfbb2aa909701c3dad2c
Bernardo Rittmeyer <bernardo@rittme.com>
2020-07-23 18:40:50 +0800

    make contract notice clearer

--- packages/smart-contracts/src/contracts/ERC20FeeProxy.sol
+++ packages/smart-contracts/src/contracts/ERC20FeeProxy.sol
@@ -24,7 +24,7 @@ contract ERC20FeeProxy {
   }
 
   /**
-    * @notice Performs a ERC20 token transfer with a reference and fee
+    * @notice Performs a ERC20 token transfer with a reference and a transfer to a second address for the payment of a fee
     * @param _tokenAddress Address of the ERC20 token smart contract
     * @param _to Transfer recipient
     * @param _amount Amount to transfer
