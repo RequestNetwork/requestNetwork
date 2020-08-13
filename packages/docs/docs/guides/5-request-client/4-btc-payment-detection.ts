adea8126b52e308b853cf2aba9a76214b3021335
Bernardo Rittmeyer <bernardo@rittme.com>
2020-08-13 13:53:00 +0800

    chore: upgrade docs

--- packages/docs/docs/guides/5-request-client/4-btc-payment-detection.ts
+++ packages/docs/docs/guides/5-request-client/4-btc-payment-detection.ts
@@ -86,7 +86,7 @@ const requestInfo: RequestNetwork.Types.IRequestInfo = {
 
 // If you want to create a request with testnet bitcoin you must use the payment network:
 // RequestNetwork.Types.Payment.PAYMENT_NETWORK_ID.TESTNET_BITCOIN_ADDRESS_BASED
-const bitcoinAddressBasedPaymentNetwork: RequestNetwork.Types.Payment.IPaymentNetworkCreateParameters = {
+const paymentNetwork: RequestNetwork.Types.Payment.IPaymentNetworkCreateParameters = {
   id: RequestNetwork.Types.Payment.PAYMENT_NETWORK_ID.BITCOIN_ADDRESS_BASED,
   parameters: {
     paymentAddress: '16uyvigo8mMAfE3Ctr5Rwgab1aWNDPDMZD',
@@ -94,7 +94,7 @@ const bitcoinAddressBasedPaymentNetwork: RequestNetwork.Types.Payment.IPaymentNe
 };
 
 const bitcoinAddressBasedCreateParams = {
-  bitcoinAddressBasedPaymentNetwork,
+  paymentNetwork,
   requestInfo,
   signer: payeeIdentity,
 };
