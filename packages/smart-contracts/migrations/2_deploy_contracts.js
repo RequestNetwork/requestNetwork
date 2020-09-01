9e461b360f73bb485f3aea57d7780011633556cc
Bernardo Rittmeyer <bernardo@rittme.com>
2020-09-01 18:10:12 +0800

    add tests for all non-standard ERC20 responses

--- packages/smart-contracts/migrations/2_deploy_contracts.js
+++ packages/smart-contracts/migrations/2_deploy_contracts.js
@@ -1,10 +1,15 @@
 const RequestHashStorage = artifacts.require('./RequestHashStorage.sol');
 const RequestOpenHashSubmitter = artifacts.require('./RequestOpenHashSubmitter.sol');
-const erc20 = artifacts.require('./TestERC20.sol');
-const BadERC20 = artifacts.require('./BadERC20.sol');
 const ERC20Proxy = artifacts.require('./ERC20Proxy.sol');
 const EthereumProxy = artifacts.require('./EthereumProxy.sol');
 const ERC20FeeProxy = artifacts.require('./ERC20FeeProxy.sol');
+
+const erc20 = artifacts.require('./TestERC20.sol');
+const BadERC20 = artifacts.require('./BadERC20.sol');
+const ERC20True = artifacts.require('ERC20True');
+const ERC20False = artifacts.require('ERC20False');
+const ERC20NoReturn = artifacts.require('ERC20NoReturn');
+const ERC20Revert = artifacts.require('ERC20Revert');
 
 const addressContractBurner = '0xfCb4393e7fAef06fAb01c00d67c1895545AfF3b8';
 
@@ -51,16 +56,29 @@ module.exports = async function(deployer) {
     );
 
     // Deploy Ethereym proxy contract
-    const instanceRequestEthereumProxy = await deployer.deploy(EthereumProxy);
+    await deployer.deploy(EthereumProxy);
     console.log('EthereumProxy Contract deployed: ' + EthereumProxy.address);
 
     // Deploy ERC20 Fee proxy contract
-    const instanceRequestERC20FeeProxy = await deployer.deploy(ERC20FeeProxy);
+    await deployer.deploy(ERC20FeeProxy);
     console.log('ERC20FeeProxy Contract deployed: ' + ERC20FeeProxy.address);
 
     // Deploy the BadERC20 contract
-    const instanceBadERC20 = await deployer.deploy(BadERC20, 1000, 'BadERC20', 'BAD', 8);
+    await deployer.deploy(BadERC20, 1000, 'BadERC20', 'BAD', 8);
     console.log('BadERC20 Contract deployed: ' + BadERC20.address);
+
+    // Deploy test ERC20 contracts
+    await deployer.deploy(ERC20True);
+    console.log('ERC20True Contract deployed: ' + ERC20True.address);
+
+    await deployer.deploy(ERC20False);
+    console.log('ERC20False Contract deployed: ' + ERC20False.address);
+
+    await deployer.deploy(ERC20NoReturn);
+    console.log('ERC20NoReturn Contract deployed: ' + ERC20NoReturn.address);
+
+    await deployer.deploy(ERC20Revert);
+    console.log('ERC20Revert Contract deployed: ' + ERC20Revert.address);
 
     // ----------------------------------
     console.log('Contracts initialized');
@@ -72,6 +90,10 @@ module.exports = async function(deployer) {
       EthereumProxy:            ${EthereumProxy.address}
       ERC20FeeProxy:            ${ERC20FeeProxy.address}
       BadERC20:                 ${BadERC20.address}
+      ERC20True:                ${ERC20True.address}
+      ERC20False:               ${ERC20False.address}
+      ERC20NoReturn:            ${ERC20NoReturn.address}
+      ERC20Revert:              ${ERC20Revert.address}
     `);
   } catch (e) {
     console.error(e);
