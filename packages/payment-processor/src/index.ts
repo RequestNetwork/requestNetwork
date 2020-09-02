3f89e64d2b26e81835cae6ede360e3185beda6c0
Bernardo Rittmeyer <bernardo@rittme.com>
2020-09-02 15:26:57 +0800

    add erc20 to export

--- packages/payment-processor/src/index.ts
+++ packages/payment-processor/src/index.ts
@@ -1,5 +1,6 @@
 export * from './payment';
 export * from './payment/btc-address-based';
+export * from './payment/erc20';
 export * from './payment/erc20-proxy';
 export * from './payment/erc20-fee-proxy';
 export * from './payment/eth-input-data';
