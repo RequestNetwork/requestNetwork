94c572287da0362c664d9a0419a788ede6dcf6af
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2020-09-09 00:15:17 +0200

    multi-format

--- packages/multi-format/test/encryption/ecies-format.test.ts
+++ packages/multi-format/test/encryption/ecies-format.test.ts
@@ -1,5 +1,4 @@
 import { EncryptionTypes, MultiFormatTypes } from '@requestnetwork/types';
-import { expect } from 'chai';
 
 import EciesFormat from '../../src/encryption/ecies-format';
 let eciesFormat: EciesFormat;
@@ -12,85 +11,85 @@ describe('encryption/ecies-format', () => {
 
   describe('isDeserializableString', () => {
     it('should return true if a correct format is given', () => {
+      // 'isDeserializableString() error'
       expect(
         eciesFormat.isDeserializableString(
           '02af91330fe78ccde898f10a39d6088568e24275a6cfbe9e80f4c2f42a4308f907',
         ),
-        'isDeserializableString() error',
-      ).to.be.true;
+      ).toBe(true);
     });
 
     it('should return false if an incorrect format is given', () => {
+      // 'should be false with an incorrect prefix'
       expect(
         eciesFormat.isDeserializableString(
           '00af91330fe78ccde898f10a39d6088568e24275a6cfbe9e80f4c2f42a4308f907',
         ),
-        'should be false with an incorrect prefix',
-      ).to.be.false;
+      ).toBe(false);
     });
   });
 
   describe('isSerializableObject', () => {
     it('should return true if a correct format is given', () => {
+      // 'isDeserializableString() error'
       expect(
         eciesFormat.isSerializableObject({
           type: EncryptionTypes.METHOD.ECIES,
           value: 'af91330fe78ccde898f10a39d6088568e24275a6cfbe9e80f4c2f42a4308f907',
         }),
-        'isDeserializableString() error',
-      ).to.be.true;
+      ).toBe(true);
     });
 
     it('should return false if an incorrect format is given', () => {
+      // 'should be false with an incorrect type'
       expect(
         eciesFormat.isSerializableObject({
           type: MultiFormatTypes.PlainTypes.TYPE.PLAIN_TEXT,
           value: 'af91330fe78ccde898f10a39d6088568e24275a6cfbe9e80f4c2f42a4308f907',
         }),
-        'should be false with an incorrect type',
-      ).to.be.false;
+      ).toBe(false);
     });
   });
 
   describe('serialize', () => {
     it('can serialize', () => {
+      // 'serialize() error'
       expect(
         eciesFormat.serialize({
           type: EncryptionTypes.METHOD.ECIES,
           value: 'af91330fe78ccde898f10a39d6088568e24275a6cfbe9e80f4c2f42a4308f907',
         }),
-        'serialize() error',
-      ).to.be.equal('02af91330fe78ccde898f10a39d6088568e24275a6cfbe9e80f4c2f42a4308f907');
+      ).toBe('02af91330fe78ccde898f10a39d6088568e24275a6cfbe9e80f4c2f42a4308f907');
     });
 
     it('cannot serialize a with incorrect type', () => {
+      // 'serialize() error'
       expect(() => {
         eciesFormat.serialize({
           type: MultiFormatTypes.HashTypes.TYPE.KECCAK256,
           value: 'af91330fe78ccde898f10a39d6088568e24275a6cfbe9e80f4c2f42a4308f9071',
         });
-      }, 'serialize() error').to.throw('object is not a serializable object');
+      }).toThrowError('object is not a serializable object');
     });
   });
 
   describe('deserialize', () => {
     it('can deserialize', () => {
       const formatted = '02af91330fe78ccde898f10a39d6088568e24275a6cfbe9e80f4c2f42a4308f907';
-      expect(eciesFormat.deserialize(formatted), 'deserialize(formatted) error').to.be.deep.equal({
+      // 'deserialize(formatted) error'
+      expect(eciesFormat.deserialize(formatted)).toEqual({
         type: EncryptionTypes.METHOD.ECIES,
         value: 'af91330fe78ccde898f10a39d6088568e24275a6cfbe9e80f4c2f42a4308f907',
       });
     });
 
-    it(
-      'should return false if an incorrect format is given to isDeserializableString',
-      () => {
-        expect(() => {
-          eciesFormat.deserialize(
-            '00af91330fe78ccde898f10a39d6088568e24275a6cfbe9e80f4c2f42a4308f907',
-          );
-        }, 'should throw with an incorrect prefix').to.throw('string is not a serialized string');
-      }
-    );
+    it('should return false if an incorrect format is given to isDeserializableString', () => {
+      // 'should throw with an incorrect prefix'
+      expect(() => {
+        eciesFormat.deserialize(
+          '00af91330fe78ccde898f10a39d6088568e24275a6cfbe9e80f4c2f42a4308f907',
+        );
+      }).toThrowError('string is not a serialized string');
+    });
   });
 });
