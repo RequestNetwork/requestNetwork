0ba044908d500a161f09c31636aa32f9d088cbbc
Yo <56731761+yomarion@users.noreply.github.com>
2020-10-14 16:16:53 +0200

    fix: swap methods for partial ERC20 approvals (#341)
    * Support tokens that partially implement ERC20 approval (ex: USDT) with the swap to pay contract.

--- packages/smart-contracts/artifacts/verification/ERC20SwapToPay.sol/SafeERC20.sol
+++ packages/smart-contracts/artifacts/verification/ERC20SwapToPay.sol/SafeERC20.sol
@@ -48,4 +48,46 @@ library SafeERC20 {
     /* solium-enable security/no-inline-assembly */
     return result;
   }
+  
+  /**
+    * @dev Deprecated. This function has issues similar to the ones found in
+    * {IERC20-approve}, and its usage is discouraged.
+    *
+    * Whenever possible, use {safeIncreaseAllowance} and
+    * {safeDecreaseAllowance} instead.
+    */
+  function safeApprove(IERC20 _token, address _spender, uint256 _amount) internal returns (bool result) {
+    address tokenAddress = address(_token);
+    /* solium-disable security/no-inline-assembly */
+    // check if the address is a contract
+    assembly {
+      if iszero(extcodesize(tokenAddress)) { revert(0, 0) }
+    }
+    
+    // solium-disable-next-line security/no-low-level-calls
+    (bool success, ) = tokenAddress.call(abi.encodeWithSignature(
+      "approve(address,uint256)",
+      _spender,
+      _amount
+    ));
+
+    assembly {
+        switch returndatasize()
+        case 0 { // not a standard erc20
+            result := 1
+        }
+        case 32 { // standard erc20
+            returndatacopy(0, 0, 32)
+            result := mload(0)
+        }
+        default { // anything else, should revert for safety
+            revert(0, 0)
+        }
+    }
+
+    require(success, "approve() has been reverted");
+
+    /* solium-enable security/no-inline-assembly */
+    return result;
+  }
 }
