c875d871000bbc480ec5dd4d2a02760cd86a79a3
vrolland <vincent.rolland.38@gmail.com>
2020-10-16 14:52:09 +0200

    fix (?) decimal and optimize constants

--- packages/smart-contracts/src/contracts/StableForFiatProxy.sol
+++ packages/smart-contracts/src/contracts/StableForFiatProxy.sol
@@ -1,5 +1,7 @@
 pragma solidity ^0.6.0;
 
+import "@openzeppelin/contracts/math/SafeMath.sol"
+
 /**
  * @dev Interface of the ERC20 standard as defined in the EIP.
  */
@@ -78,7 +80,6 @@ interface IERC20 {
     event Approval(address indexed owner, address indexed spender, uint256 value);
 }
 
-//import "https://github.com/smartcontractkit/chainlink/evm-contracts/src/v0.6/interfaces/AggregatorInterface.sol";
 interface AggregatorInterface {
     function latestAnswer() external view returns (int256);
 
@@ -95,10 +96,11 @@ interface AggregatorInterface {
 }
 
 /**
- * @title ERC20Proxy
- * @notice This contract performs an ERC20 token transfer and stores a reference
+ * @title ProxyChangeCryptoFiat
  */
-contract ProxyChangeDAIEUR {
+contract ProxyChangeCryptoFiat {
+    using SafeMath for uint256;
+
     // Event to declare a transfer with a reference
     event TransferWithReference(
         address tokenAddress,
@@ -111,40 +113,84 @@ contract ProxyChangeDAIEUR {
     enum FiatEnum {USD, AUD, CHF, EUR, GBP, JPY}
     enum CryptoEnum {ETH, DAI, USDT, USDC, SUSD}
 
-    // TODO replace mapping by something less gas consumming
-    mapping(FiatEnum => address) aggregatorFiatToUsd;
-    mapping(CryptoEnum => address) aggregatorCryptoToUsd;
-    mapping(CryptoEnum => address) aggregatorCryptoToETH;
-    mapping(CryptoEnum => address) tokenAddresses;
+    function getChainlinkAggregatorCryptoToETH(CryptoEnum cryptoEnum)
+        private
+        view
+        returns (AggregatorInterface)
+    {
+        if (cryptoEnum == CryptoEnum.SUSD) {
+            // SUSD/ETH
+            return AggregatorInterface(0x8e0b7e6062272B5eF4524250bFFF8e5Bd3497757);
+        }
+        if (cryptoEnum == CryptoEnum.USDC) {
+            // USDC/ETH
+            return AggregatorInterface(0x986b5E1e1755e3C2440e960477f25201B0a8bbD4);
+        }
+        if (cryptoEnum == CryptoEnum.USDT) {
+            // USDT/ETH
+            return AggregatorInterface(0xEe9F2375b4bdF6387aa8265dD4FB8F16512A1d46);
+        }
+        revert('crypto not supported');
+    }
 
-    constructor() public {
-        // AUD/USD
-        aggregatorFiatToUsd[FiatEnum.AUD] = 0x77F9710E7d0A19669A13c055F62cd80d313dF022;
-        // CHF/USD
-        aggregatorFiatToUsd[FiatEnum.CHF] = 0x449d117117838fFA61263B61dA6301AA2a88B13A;
-        // EUR/USD
-        aggregatorFiatToUsd[FiatEnum.EUR] = 0xb49f677943BC038e9857d61E7d053CaA2C1734C1;
-        // GBP/USD
-        aggregatorFiatToUsd[FiatEnum.GBP] = 0x5c0Ab2d9b5a7ed9f470386e82BB36A3613cDd4b5;
-        // JPY/USD
-        aggregatorFiatToUsd[FiatEnum.JPY] = 0xBcE206caE7f0ec07b545EddE332A47C2F75bbeb3;
+    function getChainlinkAggregatorCryptoToUsd(CryptoEnum cryptoEnum)
+        private
+        view
+        returns (AggregatorInterface)
+    {
+        if (cryptoEnum == CryptoEnum.ETH) {
+            // ETH/USD
+            return AggregatorInterface(0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419);
+        }
+        if (cryptoEnum == CryptoEnum.DAI) {
+            // DAI/USD
+            return AggregatorInterface(0xAed0c38402a5d19df6E4c03F4E2DceD6e29c1ee9);
+        }
+        revert('crypto not supported');
+    }
 
-        // ETH/USD
-        aggregatorCryptoToUsd[CryptoEnum.ETH] = 0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419;
-        // DAI/USD
-        aggregatorCryptoToUsd[CryptoEnum.DAI] = 0xAed0c38402a5d19df6E4c03F4E2DceD6e29c1ee9;
+    function getChainlinkAggregatorFiatToUsd(FiatEnum fiatEnum)
+        private
+        view
+        returns (AggregatorInterface)
+    {
+        if (fiatEnum == FiatEnum.AUD) {
+            // AUD/USD
+            return AggregatorInterface(0x77F9710E7d0A19669A13c055F62cd80d313dF022);
+        }
+        if (fiatEnum == FiatEnum.CHF) {
+            // CHF/USD
+            return AggregatorInterface(0x449d117117838fFA61263B61dA6301AA2a88B13A);
+        }
+        if (fiatEnum == FiatEnum.EUR) {
+            // EUR/USD
+            return AggregatorInterface(0xb49f677943BC038e9857d61E7d053CaA2C1734C1);
+        }
+        if (fiatEnum == FiatEnum.GBP) {
+            // GBP/USD
+            return AggregatorInterface(0x5c0Ab2d9b5a7ed9f470386e82BB36A3613cDd4b5);
+        }
+        if (fiatEnum == FiatEnum.JPY) {
+            // JPY/USD
+            return AggregatorInterface(0xBcE206caE7f0ec07b545EddE332A47C2F75bbeb3);
+        }
+        revert('fiat not supported');
+    }
 
-        // SUSD/ETH
-        aggregatorCryptoToETH[CryptoEnum.SUSD] = 0x8e0b7e6062272B5eF4524250bFFF8e5Bd3497757;
-        // USDC/ETH
-        aggregatorCryptoToETH[CryptoEnum.USDC] = 0x986b5E1e1755e3C2440e960477f25201B0a8bbD4;
-        // USDT/ETH
-        aggregatorCryptoToETH[CryptoEnum.USDT] = 0xEe9F2375b4bdF6387aa8265dD4FB8F16512A1d46;
-
-        tokenAddresses[CryptoEnum.DAI] = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
-        tokenAddresses[CryptoEnum.USDT] = 0xdAC17F958D2ee523a2206206994597C13D831ec7;
-        tokenAddresses[CryptoEnum.USDC] = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
-        tokenAddresses[CryptoEnum.SUSD] = 0x57Ab1ec28D129707052df4dF418D58a2D46d5f51;
+    function getTokenAddress(CryptoEnum cryptoEnum) private view returns (address) {
+        if (cryptoEnum == CryptoEnum.DAI) {
+            return 0x6B175474E89094C44Da98b954EedeAC495271d0F;
+        }
+        if (cryptoEnum == CryptoEnum.USDT) {
+            return 0xdAC17F958D2ee523a2206206994597C13D831ec7;
+        }
+        if (cryptoEnum == CryptoEnum.USDC) {
+            return 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+        }
+        if (cryptoEnum == CryptoEnum.SUSD) {
+            return 0x57Ab1ec28D129707052df4dF418D58a2D46d5f51;
+        }
+        revert('Crypton not supported');
     }
 
     function computeConversion(
@@ -156,35 +202,34 @@ contract ProxyChangeDAIEUR {
         if (_currencyFiat == FiatEnum.USD) {
             amountInUSD = _amountFiat;
         } else {
-            AggregatorInterface priceFeed = AggregatorInterface(aggregatorFiatToUsd[_currencyFiat]);
-
-            amountInUSD = _amountFiat * uint256(priceFeed.latestAnswer());
+            // amount * rate / decimale
+            amountInUSD = _amountFiat.mul(uint256(getChainlinkAggregatorFiatToUsd(_currencyFiat).latestAnswer())).div(8);
         }
 
         uint256 finalAmount;
         if (_currencyCrypto == CryptoEnum.ETH || _currencyCrypto == CryptoEnum.DAI) {
-            AggregatorInterface priceFeed2 = AggregatorInterface(
-                aggregatorCryptoToUsd[_currencyCrypto]
-            );
-            // TODO: Decimal ?!
-            finalAmount = amountInUSD / uint256(priceFeed2.latestAnswer());
+            // amount * decimale / rate
+            finalAmount = amountInUSD.mul(8).div(uint256(getChainlinkAggregatorCryptoToUsd(_currencyCrypto).latestAnswer()));
         } else {
-            AggregatorInterface priceFeed3 = AggregatorInterface(
-                aggregatorCryptoToUsd[CryptoEnum.ETH]
-            );
-            // TODO: Decimal ?!
-            uint256 amountInETH = amountInUSD / uint256(priceFeed3.latestAnswer());
+            // amount * decimale / rate
+            uint256 amountInETH = amountInUSD.mul(8).div(uint256(getChainlinkAggregatorCryptoToUsd(_currencyCrypto).latestAnswer()));
 
-            AggregatorInterface priceFeed4 = AggregatorInterface(
-                aggregatorCryptoToETH[_currencyCrypto]
-            );
-            // TODO: Decimal ?!
-            finalAmount = amountInETH / uint256(priceFeed4.latestAnswer());
+            // amount * decimale / rate
+            finalAmount = amountInETH.mul(18).div(uint256(getChainlinkAggregatorCryptoToETH(_currencyCrypto).latestAnswer()));
         }
 
         return finalAmount;
     }
 
+    /**
+     * @notice Performs a ERC20 token transfer with a reference computing the amount regarding a fiat amount
+     * @param _to Transfer recipient
+     * @param _amountFiat Amount reference to transfer in fiat (8 decimals)
+     * @param _currencyFiat currency of the amount reference
+     * @param _currencyCrypto currency of the crypto to pay with
+     * @param _paymentReference Reference of the payment related
+     * @param _maxCryptoToSpend amount max that we can spend on the behalf of the user
+     */
     function transferWithReference(
         address _to,
         uint256 _amountFiat,
@@ -199,7 +244,7 @@ contract ProxyChangeDAIEUR {
         uint256 amountToPay = computeConversion(_amountFiat, _currencyFiat, _currencyCrypto);
         require(amountToPay <= _maxCryptoToSpend, 'Amount to pay over the user limit');
 
-        IERC20 erc20 = IERC20(tokenAddresses[_currencyCrypto]);
+        IERC20 erc20 = IERC20(getTokenAddress(_currencyCrypto));
         require(
             erc20.allowance(msg.sender, address(this)) >= amountToPay,
             'allowance needed for the token transfer'
