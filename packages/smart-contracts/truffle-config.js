489f7dcd27adfe8cc7efa78d70f0f1a5b0cbf252
vrolland <vincent.rolland.38@gmail.com>
2020-11-23 14:46:20 +0100

    fix truffle config

--- packages/smart-contracts/truffle-config.js
+++ packages/smart-contracts/truffle-config.js
@@ -1,10 +1,10 @@
 module.exports = {
   networks: {
-    // development: {
-    //   host: process.env.TRUFFLE_GANACHE_HOST || 'localhost',
-    //   port: process.env.TRUFFLE_GANACHE_PORT || 8545,
-    //   network_id: '*', // Match any network id
-    // },
+    development: {
+      host: process.env.TRUFFLE_GANACHE_HOST || 'localhost',
+      port: process.env.TRUFFLE_GANACHE_PORT || 8545,
+      network_id: '*', // Match any network id
+    },
     rinkeby: {
       network_id: 4,
       gasPrice: '5000000000', // 5 Gwei
