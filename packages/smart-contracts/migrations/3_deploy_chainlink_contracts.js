a2f30a84689eaea0994e72944c417718c7aad20e
Vincent <4611986+vrolland@users.noreply.github.com>
2021-02-18 18:21:06 +0100

    feat: payment network any to erc20 smartcontracts (#408)

---
+++ packages/smart-contracts/migrations/3_deploy_chainlink_contracts.js
@@ -0,0 +1,36 @@
+const Currency = require('@requestnetwork/currency').default;
+const AggDAI_USD = artifacts.require("AggDaiUsd.sol");
+const AggETH_USD = artifacts.require("AggEthUsd");
+const AggEUR_USD = artifacts.require("AggEurUsd");
+const AggUSDT_ETH = artifacts.require("AggUsdtEth");
+const USDT_fake = artifacts.require("UsdtFake");
+
+const ERC20FeeProxy = artifacts.require('./ERC20FeeProxy.sol');
+const ChainlinkConversionPath = artifacts.require("ChainlinkConversionPath");
+const ProxyChainlinkConversionPath = artifacts.require("ProxyChainlinkConversionPath");
+
+
+// Deploys, set up the contracts
+module.exports = async function (deployer) {
+  await deployer.deploy(AggDAI_USD);
+  await deployer.deploy(AggETH_USD);
+  await deployer.deploy(AggEUR_USD);
+  await deployer.deploy(AggUSDT_ETH);
+  await deployer.deploy(USDT_fake);
+
+  // all these addresses are for test purposes
+  const ETH_address = Currency.getCurrencyHash({ type: 'ETH', value: 'ETH' });
+  const USD_address = Currency.getCurrencyHash({ type: 'ISO4217', value: 'USD' });
+  const EUR_address = Currency.getCurrencyHash({ type: 'ISO4217', value: 'EUR' });
+  const DAI_address = Currency.getCurrencyHash({ type: 'ERC20', value: '0x38cF23C52Bb4B13F051Aec09580a2dE845a7FA35' });
+  const USDT_address = USDT_fake.address;
+
+  const conversionPathInstance = await deployer.deploy(ChainlinkConversionPath);
+
+  // all these aggregators are for test purposes
+  await conversionPathInstance.updateAggregatorsList([DAI_address, EUR_address, ETH_address, USDT_address],
+    [USD_address, USD_address, USD_address, ETH_address],
+    [AggDAI_USD.address, AggEUR_USD.address, AggETH_USD.address, AggUSDT_ETH.address]);
+
+  await deployer.deploy(ProxyChainlinkConversionPath, ERC20FeeProxy.address, ChainlinkConversionPath.address);
+};
