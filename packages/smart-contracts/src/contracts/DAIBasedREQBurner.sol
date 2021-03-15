c45b1d28899fd26bad9ae23b31f1cc6d1e4caec3
yomarion <yoann.marion@gmail.com>
2021-03-15 12:18:39 +0100

    chore: code cleaning & mainnet addresses

--- packages/smart-contracts/src/contracts/DAIBasedREQBurner.sol
+++ packages/smart-contracts/src/contracts/DAIBasedREQBurner.sol
@@ -1,75 +1,76 @@
 pragma solidity ^0.5.17;
 import "./lib/Ownable.sol";
 import "./interface/IUniswapV2Router02.sol";
-/// @title Contract for  allowing  removal of global supply of locked mintable erc20 tokens by converted from DAI using uniswap v2 router contract 
-/// @author Yoann , Request Network
-/// @notice Currently used the contract only for experimental purposes and tokens only .
-/// @dev some function calls have to be optimised (swapexactTokensforTokens for instance and further audit checks ).
-///  pre-requisites: are to mint tokens from faucet : http://central.request.network/ , and then there will be exchange with the FAU.
+
+/**
+ * @title Contract for  allowing  removal of global supply of locked mintable erc20 tokens by converted from DAI using uniswap v2 router contract
+ * @dev some function calls have to be optimised (swapexactTokensforTokens for instance and further audit checks ).
+ * @author Request Network
+ */ 
+
+// example tokens taken from rinkleby  
+//address constant CTBK_ADDRESS = 0x995d6A8C21F24be1Dd04E105DD0d83758343E258;     // Instead of DAI
+//address constant FAU_ADDRESS_FIX = 0xFab46E002BbF0b4509813474841E0716E6730136;  // (Burnable) Instead of REQ
+
+address constant DAI_ADDRESS = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
+address constant REQ_ADDRESS = 0x8f8221aFbB33998d8584A2B05749bA73c37a938a;
+
+
 interface IERC20 {
-    function totalSupply() external view returns (uint supply);
     function balanceOf(address _owner) external view returns (uint balance);
-    function transfer(address _to, uint _value) external returns (bool success);
-    function transferFrom(address _from, address _to, uint _value) external returns (bool success);
     function approve(address _spender, uint _value) external returns (bool success);
-    function allowance(address _owner, address _spender) external view returns (uint remaining);
-    function decimals() external view returns(uint digits);
     event Approval(address indexed _owner, address indexed _spender, uint _value);
 }
 
 /*interface*/ contract IBurnableErc20 is IERC20 {
     function burn(uint value) external;
 }
-/// @title DAITestBurner
-/// @notice A contract to burn test  ERC20 tokens from DAI.
-/// @dev Sends the DAI to the uniswap router factory smart contract which then tries to mint the destination token using the specified token LP.
-///  The converted ERC20 is then burned.
-/// @param _swapRouterAddress address of the uniswap token router (which follow the same method signature ).
+/** @title DaiBasedREQBurner
+ * @notice A contract to burn REQ tokens from DAI.
+ * @dev All DAIs sent to this contract can only be exchanged for REQs that are then burnt, using Uniswap.
+ * @param _swapRouterAddress address of the uniswap token router (which follow the same method signature ).
+ */
 contract DaiBasedREQBurner is Ownable {
-    // example tokens taken from rinkleby  which are erc20 burnable    
-    address constant CTBK_ADDRESS = 0x995d6A8C21F24be1Dd04E105DD0d83758343E258;
-    address constant FAU_ADDRESS = 0xFab46E002BbF0b4509813474841E0716E6730136;
-   // defining the swap router project in order to approve the specific LP trading based on the given address.
+
+    address constant LOCKED_TOKEN_ADDRESS = DAI_ADDRESS;
+    address constant BURNABLE_TOKEN_ADDRESS = REQ_ADDRESS;
+    // swap router used to convert LOCKED into BURNABLE tokens
     IUniswapV2Router02 public swapRouter;
+
     constructor(address _swapRouterAddress) public {
         require(_swapRouterAddress != address(0), "The swap router address should not be 0");
-        /// TODO:  generally this addresses needs to be whitelisted (for mainnet deployment ) 
-        /// @dev can there be possiblity to attach an malicious LP providing contract
-
         swapRouter = IUniswapV2Router02(_swapRouterAddress);
     }
 
-  /// @dev gives the permission to uniswap to accept the swapping of the DAI token 
-  /// and setting up the max limit to avoid the attacks .
-  function approveRouterToSpend() public {
-    uint256 max = 2**256 - 1;
-    IERC20 dai = IERC20(CTBK_ADDRESS);
-    dai.approve(address(swapRouter), max);
-  }
+    /// @dev gives the permission to uniswap to accept the swapping of the BURNABLE token 
+    function approveRouterToSpend() public {
+        uint256 max = 2**256 - 1;
+        IERC20 dai = IERC20(LOCKED_TOKEN_ADDRESS);
+        dai.approve(address(swapRouter), max);
+    }
 
 
-  ///@dev the main function to be executed 
-  ///@param _minReqBurnt  REQ token needed to be burned.
-  ///@param _deadline  time for the 
+    ///@dev the main function to be executed 
+    ///@param _minReqBurnt  REQ token needed to be burned.
+    ///@param _deadline  maximum timestamp to accept the trade from the router
     function burn(uint _minReqBurnt, uint256 _deadline)
         external
         returns(uint)
-    {   // here we have defined the hardcoded version of the erc20 contracts.
-        IERC20 dai = IERC20(CTBK_ADDRESS);
-        IBurnableErc20 req = IBurnableErc20(FAU_ADDRESS);
+    {
+        IERC20 dai = IERC20(LOCKED_TOKEN_ADDRESS);
+        IBurnableErc20 req = IBurnableErc20(BURNABLE_TOKEN_ADDRESS);
         uint daiToConvert = dai.balanceOf(address(this));
-        /// @dev  for uniswapV2Router.swapExactTokensForTokens , the parameter _deadline  insures that particular order to be completed in given fixed time.
+
         if (_deadline == 0) {
             _deadline = block.timestamp + 1000;
         }
 
-            address[] memory path = new address[](2);
-        /// @notice these are the intermediate paths hardcoded  currently and will work only when sufficient CTBK-FAU LP is created  on https://rinkeby.etherscan.io/address/0x995d6a8c21f24be1dd04e105dd0d83758343e258#writeContract
-        /// @dev also to allow the swapRouter and pay contracts like : https://rinkeby.etherscan.io/address/0x68ed5472dd97892e188983f361a2ca55635d400b#writeContract .
-        path[0] = CTBK_ADDRESS;
-        path[1] = FAU_ADDRESS;
-        /// @dev here we need to be specific that fixed amount of the tokens are to be locked 
-        //for the burning to have transparency in the req conversion . 
+        // 1 step swapping path (only works if there is a sufficient liquidity behind the router)
+        address[] memory path = new address[](2);
+        path[0] = LOCKED_TOKEN_ADDRESS;
+        path[1] = BURNABLE_TOKEN_ADDRESS;
+
+        // Do the swap and get the amount of REQ purchased
         uint reqToBurn = swapRouter.swapExactTokensForTokens(
           daiToConvert,
           _minReqBurnt,
@@ -77,7 +78,8 @@ contract DaiBasedREQBurner is Ownable {
           address(this),
           _deadline
         )[1];
-        // Burn all the remaining converted erc20 burnable  token
+
+        // Burn all the purchased REQ and return this amount
         req.burn(reqToBurn);
         return reqToBurn;
     }
