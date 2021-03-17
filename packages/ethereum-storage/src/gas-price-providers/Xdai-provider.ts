d63d96c9683a6e07a3c13385b1accf652a507c5b
Dhruv <dhruv.malik@polytechnique.edu>
2021-03-17 18:17:41 +0100

    temp fixes

--- packages/ethereum-storage/src/gas-price-providers/Xdai-provider.ts
+++ packages/ethereum-storage/src/gas-price-providers/Xdai-provider.ts
@@ -1,9 +1,9 @@
-import EthereumUtils from '../ethereum-utils';
+//import EthereumUtils from '../ethereum-utils';
 
 import { StorageTypes } from '@requestnetwork/types';
-import Utils from '@requestnetwork/utils';
+//import Utils from '@requestnetwork/utils';
 
-import fetch from 'node-fetch';
+//import fetch from 'node-fetch';
 
 const bigNumber: any = require('bn.js');
 
@@ -19,28 +19,33 @@ const API_MULTIPLIER: number = 1000000000;
  * to be verified by YMA , VRO.
  */
 
-export default class XdaiProvider  {
+export default class XdaiProvider implements StorageTypes.IGasPriceProvider {
+
+
 /// in  current context , the cost will not be dependent on the API , but on the fixed price .
-
-
-public async getGasPrice(type: StorageTypes.GasPriceType):Promise<typeof bigNumber | null> {
+public async getGasPrice(type: StorageTypes.GasPriceType):Promise<typeof bigNumber> {
 
 // there are no API endpoints for finding the dynamic price , there price is relatively constant 
 // and slightly depends on the rate of transaction as  told in https://challenge.xdaichain.com/cost-estimates
-
+// current 
 //let FastGasPrice: Number = 20
 //let ProposeGasPrice: Number = 10;
 //let SafeGasPrice: Number = 5;
 
-let GasPrice: Number[] = [20,10,5];
+let GasPrice: any = [20,10,5];
 
 
-const fixedGasPrice = new bigNumber(
+const fixedGasPrice  = new bigNumber(
+    parseInt(
+    {
+        [StorageTypes.GasPriceType.FAST] : GasPrice[0] ,
+        [StorageTypes.GasPriceType.STANDARD] : GasPrice[1] ,
+        [StorageTypes.GasPriceType.SAFELOW] : GasPrice[2] ,
+    }[type], 
+    10,
+    ) *(API_MULTIPLIER) ,
     
-    StorageTypes.GasPriceType.FAST,GasPrice[0] ,
-    StorageTypes.GasPriceType.STANDARD, GasPrice[1] ,
-    StorageTypes.GasPriceType.SAFELOW , GasPrice[2]
-    )[type].mul(API_MULTIPLIER);
+);
 // getting pricing  from gwei to wei
 
 
