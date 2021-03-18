8352d7bf702d248cd61fd5e8496bd50e07d2cb41
Dhruv <dhruv.malik@polytechnique.edu>
2021-03-18 12:29:40 +0100

    removing comments , spaces and  simplifying the code to add hardcoded value for xdai fees based on the txn type

--- packages/ethereum-storage/src/gas-price-providers/Xdai-provider.ts
+++ packages/ethereum-storage/src/gas-price-providers/Xdai-provider.ts
@@ -1,55 +1,50 @@
-//import EthereumUtils from '../ethereum-utils';
 
 import { StorageTypes } from '@requestnetwork/types';
-//import Utils from '@requestnetwork/utils';
-
-//import fetch from 'node-fetch';
-
 const bigNumber: any = require('bn.js');
 
 
 // Multiplier to use to convert the gas price in wei
-const API_MULTIPLIER: number = 1000000000;
-
+const API_MULTIPLIER: number = 10000000000;
 
 /**
- * 
- * assigns the gas price based on the following : https://challenge.xdaichain.com/cost-estimates
- * no querying from the RPC endpoint regarding the gas pricing .
- * to be verified by YMA , VRO.
+ *  Assigns the xDai transactions gas price statically.
+ * Cf. https://challenge.xdaichain.com/cost-estimates
  */
+export default class XdaiGasPriceProvider implements StorageTypes.IGasPriceProvider {
 
-export default class XdaiProvider implements StorageTypes.IGasPriceProvider {
+    // @param  type : based on the type selected in the given context , it will assign the gas to the GasDefinerFunction.
+    public async getGasPrice(type: StorageTypes.GasPriceType): Promise<typeof bigNumber | null> {
+        var fixedGasPrice: typeof bigNumber;
 
-
-/// in  current context , the cost will not be dependent on the API , but on the fixed price .
-public async getGasPrice(type: StorageTypes.GasPriceType):Promise<typeof bigNumber> {
-
-// there are no API endpoints for finding the dynamic price , there price is relatively constant 
-// and slightly depends on the rate of transaction as  told in https://challenge.xdaichain.com/cost-estimates
-// current 
-//let FastGasPrice: Number = 20
-//let ProposeGasPrice: Number = 10;
-//let SafeGasPrice: Number = 5;
-
-let GasPrice: any = [20,10,5];
+        switch (type) {
+            case StorageTypes.GasPriceType.FAST:
+                {
+                    fixedGasPrice = new bigNumber(10, 10);
+                    break;
+                }
 
 
-const fixedGasPrice  = new bigNumber(
-    parseInt(
-    {
-        [StorageTypes.GasPriceType.FAST] : GasPrice[0] ,
-        [StorageTypes.GasPriceType.STANDARD] : GasPrice[1] ,
-        [StorageTypes.GasPriceType.SAFELOW] : GasPrice[2] ,
-    }[type], 
-    10,
-    ) *(API_MULTIPLIER) ,
-    
-);
-// getting pricing  from gwei to wei
+            case StorageTypes.GasPriceType.STANDARD:
+                {
+                    fixedGasPrice = new bigNumber(5, 10);
+                    break;
+
+                }
+
+            case StorageTypes.GasPriceType.SAFELOW:
+                {
+                    fixedGasPrice = new bigNumber(1, 10);
+                    break;
+
+                }
 
 
-return fixedGasPrice;
-}
+        }
+
+
+
+
+        return (fixedGasPrice * API_MULTIPLIER);
+    }
 
 }
