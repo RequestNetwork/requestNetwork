fb5d8570cb735ecd1b969ea28af709f9f192fa44
vrolland <vrolland@protonmail.com>
2021-03-30 15:53:59 +0200

    fix stack too deep

--- packages/smart-contracts/src/contracts/ERC20SwapToPayWithConversion.sol
+++ packages/smart-contracts/src/contracts/ERC20SwapToPayWithConversion.sol
@@ -68,28 +68,22 @@ contract ERC20SwapToPayWithConversion is Ownable {
     address[] calldata _chainlinkPath, // from requestCurrency to spentToken on chainlink
     bytes calldata _paymentReference,
     uint256 _requestFeeAmount, // requestCurrency
-    address _feeAddress,
-    
+    address _feeAddress,    
     uint256 _uniswapDeadline,
     uint256 _chainlinkMaxRateTimespan
   )
     external
   {
-    IERC20 spentToken = IERC20(_uniswapPath[0]);
-    IERC20 paymentNetworkToken = IERC20(_uniswapPath[_uniswapPath.length-1]);
-    require(address(paymentNetworkToken) == _chainlinkPath[_chainlinkPath.length-1], "the requested token on uniswap must be the payment currency");
+    require(_uniswapPath[_uniswapPath.length-1] == _chainlinkPath[_chainlinkPath.length-1], "the requested token on uniswap must be the payment currency");
 
     // Get the amount to pay in paymentNetworkToken
-    // (uint256 paymentNetworkTotalAmount, ) = chainlinkConversionPath.getConversion(_requestAmount.add(_requestFeeAmount), _chainlinkPath);
     uint256 paymentNetworkTotalAmount = getConversion(_chainlinkPath, _requestAmount, _requestFeeAmount);
 
-    // TODO remove? b/c it's gas costly.
-    // require(spentToken.allowance(msg.sender, address(this)) > _amountInMax, "Not sufficient allowance for swap to pay.");
-    require(spentToken.safeTransferFrom(msg.sender, address(this), _amountInMax), "Could not transfer payment token from swapper-payer");
+    require(IERC20(_uniswapPath[0]).safeTransferFrom(msg.sender, address(this), _amountInMax), "Could not transfer payment token from swapper-payer");
 
     // Allow the router to spend all this contract's spentToken
-    if (spentToken.allowance(address(this),address(swapRouter)) < _amountInMax) {
-      approveRouterToSpend(address(spentToken));
+    if (IERC20(_uniswapPath[0]).allowance(address(this),address(swapRouter)) < _amountInMax) {
+      approveRouterToSpend(_uniswapPath[0]);
     }
 
     swapRouter.swapTokensForExactTokens(
@@ -101,8 +95,8 @@ contract ERC20SwapToPayWithConversion is Ownable {
     );
 
     // Allow the payment network to spend all this contract's paymentNetworkToken
-    if (paymentNetworkToken.allowance(address(this),address(paymentProxy)) < paymentNetworkTotalAmount) {
-      approvePaymentProxyToSpend(address(paymentNetworkToken));
+    if (IERC20(_uniswapPath[_uniswapPath.length-1]).allowance(address(this),address(paymentProxy)) < paymentNetworkTotalAmount) {
+      approvePaymentProxyToSpend(_uniswapPath[_uniswapPath.length-1]);
     }
 
     // Pay the request and fees
@@ -117,13 +111,13 @@ contract ERC20SwapToPayWithConversion is Ownable {
       _chainlinkMaxRateTimespan
     );
 
-    // // Give the change back to the payer, in both currencies (only spent token should remain)
-    // if (spentToken.balanceOf(address(this)) > 0) {
-    //   spentToken.safeTransfer(msg.sender, spentToken.balanceOf(address(this)));
-    // }
-    // if (paymentNetworkToken.balanceOf(address(this)) > 0) {
-    //   paymentNetworkToken.safeTransfer(msg.sender, paymentNetworkToken.balanceOf(address(this)));
-    // }
+    // Give the change back to the payer, in both currencies (only spent token should remain)
+    if (IERC20(_uniswapPath[0]).balanceOf(address(this)) > 0) {
+      IERC20(_uniswapPath[0]).safeTransfer(msg.sender, IERC20(_uniswapPath[0]).balanceOf(address(this)));
+    }
+    if (IERC20(_uniswapPath[_uniswapPath.length-1]).balanceOf(address(this)) > 0) {
+      IERC20(_uniswapPath[0]).safeTransfer(msg.sender, IERC20(_uniswapPath[0]).balanceOf(address(this)));
+    }
   }
 
   /*
@@ -138,7 +132,10 @@ contract ERC20SwapToPayWithConversion is Ownable {
     swapRouter = IUniswapV2Router02(_newSwapRouterAddress);
   }
 
-  function getConversion(address[] memory _path, uint256 _requestAmount, uint256 _requestFeeAmount) internal returns (uint256 conversion) {
-    (uint256 conversion, ) = chainlinkConversionPath.getConversion(_requestAmount.add(_requestFeeAmount), _path);
+  /*
+  * Internal functions to reduce the stack in swapTransferWithReference()
+  */
+  function getConversion(address[] memory _path, uint256 _requestAmount, uint256 _requestFeeAmount) view internal returns (uint256 conversion) {
+    (conversion, ) = chainlinkConversionPath.getConversion(_requestAmount.add(_requestFeeAmount), _path);
   }
 }
