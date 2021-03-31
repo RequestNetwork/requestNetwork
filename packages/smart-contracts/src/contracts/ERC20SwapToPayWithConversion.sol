a2dc078172e535b074d17a3018bf06b9010043b0
vrolland <vrolland@protonmail.com>
2021-03-31 16:10:21 +0200

    fix lint

--- packages/smart-contracts/src/contracts/ERC20SwapToPayWithConversion.sol
+++ packages/smart-contracts/src/contracts/ERC20SwapToPayWithConversion.sol
@@ -37,29 +37,9 @@ contract ERC20SwapToPayWithConversion is Ownable {
     chainlinkConversionPath = ChainlinkConversionPath(paymentProxy.chainlinkConversionPath());
   }
 
- /**
-  * @notice Authorizes the proxy to spend a new request currency (ERC20).
-  * @param _erc20Address Address of an ERC20 used as a request currency
-  */
-  function approvePaymentProxyToSpend(address _erc20Address) public {
-    IERC20 erc20 = IERC20(_erc20Address);
-    uint256 max = 2**256 - 1;
-    erc20.safeApprove(address(paymentProxy), max);
-  }
-
- /**
-  * @notice Authorizes the swap router to spend a new payment currency (ERC20).
-  * @param _erc20Address Address of an ERC20 used for payment
-  */
-  function approveRouterToSpend(address _erc20Address) public {
-    IERC20 erc20 = IERC20(_erc20Address);
-    uint256 max = 2**256 - 1;
-    erc20.safeApprove(address(swapRouter), max);
-  }
-
   /**
   * @notice Performs a token swap between a payment currency and a payment network currency after
-  *         a conversion from a request currency and then calls a payment proxy to pay 
+  *         a conversion from a request currency and then calls a payment proxy to pay
   *         the request, including fees.
   * @param _to Transfer recipient = request issuer
   * @param _requestAmount Amount to transfer in request currency
@@ -85,18 +65,22 @@ contract ERC20SwapToPayWithConversion is Ownable {
     address[] calldata _chainlinkPath, // from requestCurrency to spentToken on chainlink
     bytes calldata _paymentReference,
     uint256 _requestFeeAmount, // requestCurrency
-    address _feeAddress,    
+    address _feeAddress,
     uint256 _uniswapDeadline,
     uint256 _chainlinkMaxRateTimespan
   )
     external
   {
-    require(_uniswapPath[_uniswapPath.length-1] == _chainlinkPath[_chainlinkPath.length-1], "the requested token on uniswap must be the payment currency");
+    require(
+      _uniswapPath[_uniswapPath.length-1] == _chainlinkPath[_chainlinkPath.length-1],
+      "the requested token on uniswap must be the payment currency");
 
     // Get the amount to pay in paymentNetworkToken
     uint256 paymentNetworkTotalAmount = getConversion(_chainlinkPath, _requestAmount, _requestFeeAmount);
 
-    require(IERC20(_uniswapPath[0]).safeTransferFrom(msg.sender, address(this), _amountInMax), "Could not transfer payment token from swapper-payer");
+    require(
+      IERC20(_uniswapPath[0]).safeTransferFrom(msg.sender, address(this), _amountInMax),
+      "Could not transfer payment token from swapper-payer");
 
     // Allow the router to spend all this contract's spentToken
     if (IERC20(_uniswapPath[0]).allowance(address(this),address(swapRouter)) < _amountInMax) {
@@ -137,6 +121,26 @@ contract ERC20SwapToPayWithConversion is Ownable {
     }
   }
 
+ /**
+  * @notice Authorizes the proxy to spend a new request currency (ERC20).
+  * @param _erc20Address Address of an ERC20 used as a request currency
+  */
+  function approvePaymentProxyToSpend(address _erc20Address) public {
+    IERC20 erc20 = IERC20(_erc20Address);
+    uint256 max = 2**256 - 1;
+    erc20.safeApprove(address(paymentProxy), max);
+  }
+
+ /**
+  * @notice Authorizes the swap router to spend a new payment currency (ERC20).
+  * @param _erc20Address Address of an ERC20 used for payment
+  */
+  function approveRouterToSpend(address _erc20Address) public {
+    IERC20 erc20 = IERC20(_erc20Address);
+    uint256 max = 2**256 - 1;
+    erc20.safeApprove(address(swapRouter), max);
+  }
+
   /*
   * Admin functions to edit the admin, router address or proxy address
   */
@@ -152,7 +156,7 @@ contract ERC20SwapToPayWithConversion is Ownable {
   /*
   * Internal functions to reduce the stack in swapTransferWithReference()
   */
-  function getConversion(address[] memory _path, uint256 _requestAmount, uint256 _requestFeeAmount) view internal returns (uint256 conversion) {
+  function getConversion(address[] memory _path, uint256 _requestAmount, uint256 _requestFeeAmount) internal view returns (uint256 conversion) {
     (conversion, ) = chainlinkConversionPath.getConversion(_requestAmount.add(_requestFeeAmount), _path);
   }
 }
