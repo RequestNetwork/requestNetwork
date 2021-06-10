bb066ba09740a35221b370e534a97bab1b0b17ce
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2021-06-10 14:22:56 +0200

    support private network

--- packages/payment-detection/src/thegraph/index.ts
+++ packages/payment-detection/src/thegraph/index.ts
@@ -1,16 +1,25 @@
 import { GraphQLClient } from 'graphql-request';
 import { getSdk } from './generated/graphql';
 
-// Note: it is also possible to use the IPFS hash of the subgraph
-//  eg. /subgraphs/id/QmcCaSkefrmhe4xQj6Y6BBbHiFkbrn6UGDEBUWER7nt399
-//  which is a better security but would require an update of the
-//  library each time the subgraph is updated, which isn't ideal
-//  for early testing.
-const BASE_URL = `https://api.thegraph.com/subgraphs`;
+const BASE_URL = `https://api.thegraph.com`;
 
+// NB: the GraphQL client is automatically generated based on files present in ./queries,
+// using graphql-codegen.
+// To generate types, run `yarn codegen`, then open the generated files so that the code editor picks up the changes.
+/**
+ * A GraphQL client to query Request's subgraph.
+ */
 export type TheGraphClient = ReturnType<typeof getSdk>;
 export const getTheGraphClient = (network: string, baseUrl = BASE_URL): TheGraphClient => {
-  const url = `${baseUrl}/name/requestnetwork/request-payments-${network}`;
+  if (network === 'private') {
+    baseUrl = 'http://localhost:8000';
+  }
+  // Note: it is also possible to use the IPFS hash of the subgraph
+  //  eg. /subgraphs/id/QmcCaSkefrmhe4xQj6Y6BBbHiFkbrn6UGDEBUWER7nt399
+  //  which is a better security but would require an update of the
+  //  library each time the subgraph is updated, which isn't ideal
+  //  for early testing.
+  const url = `${baseUrl}/subgraphs/name/requestnetwork/request-payments-${network}`;
   return getSdk(new GraphQLClient(url, {}));
 };
 
