53010e6b6481273a4e03f77bcac98c6f04dfe8f6
vrolland <vrolland@protonmail.com>
2021-07-07 16:51:53 +0200

    feat: add delegate in request client

--- packages/request-client.js/src/api/request.ts
+++ packages/request-client.js/src/api/request.ts
@@ -657,6 +657,65 @@ export default class Request {
   }
 
   /**
+   * Declare a refund is received for the declarative payment network
+   *
+   * @param amount Amount received
+   * @param note Note from payer about the received refund
+   * @param signerIdentity Identity of the signer. The identity type must be supported by the signature provider.
+   * @returns The updated request
+   */
+  public async declareDeclarativeDelegate(
+    delegate: IdentityTypes.IIdentity,
+    signerIdentity: IdentityTypes.IIdentity,
+  ): Promise<Types.IRequestDataWithEvents> {
+    const extensionsData: any[] = [];
+
+    if (!this.paymentNetwork) {
+      throw new Error('Cannot declare delegate without payment network');
+    }
+
+    // We need to cast the object since IPaymentNetwork doesn't implement createExtensionsDataForDeclareReceivedRefund
+    const declarativePaymentNetwork: PaymentNetworkDeclarative = this
+      .paymentNetwork as PaymentNetworkDeclarative;
+
+    if (!declarativePaymentNetwork.createExtensionsDataForAddDelegate) {
+      throw new Error('Cannot declare delegate without declarative payment network');
+    }
+
+    extensionsData.push(
+      declarativePaymentNetwork.createExtensionsDataForAddDelegate({
+        delegate,
+      }),
+    );
+
+    const parameters: RequestLogicTypes.IAddExtensionsDataParameters = {
+      extensionsData,
+      requestId: this.requestId,
+    };
+
+    const addExtensionResult = await this.requestLogic.addExtensionsDataRequest(
+      parameters,
+      signerIdentity,
+      true,
+    );
+
+    // refresh the local request data
+    const requestData = await this.refresh();
+
+    if (!this.disableEvents) {
+      addExtensionResult
+        .on('confirmed', async () => {
+          requestData.emit('confirmed', await this.refresh());
+        })
+        .on('error', (error) => {
+          this.emitter.emit('error', error);
+        });
+    }
+
+    return requestData;
+  }
+
+  /**
    * Gets the request data
    *
    * @returns The updated request data
