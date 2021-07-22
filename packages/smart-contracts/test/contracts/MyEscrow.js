341ea6c7bfd532f2dbe134322b0b4c44a2cc3a77
dadogg80 <ivo.garofalo@gmail.com>
2021-07-22 16:42:51 +0200

    Tests

--- packages/smart-contracts/test/contracts/MyEscrow.js
+++ packages/smart-contracts/test/contracts/MyEscrow.js
@@ -1,25 +1,43 @@
-const ethers = require('ethers');
+const { assert } = require("console");
 
-const { expect } = require('chai');
-const { expectEvent, expectRevert } = require('@openzeppelin/test-helpers');
-
-const Contract = artifacts.require("MyEscrow");
+const ERC20FeeProxy = artifacts.require('./ERC20FeeProxy.sol');
+const Contract = artifacts.require("./MyEscrow.sol");
+const TestERC20 = artifacts.require("./TestERC20.sol");
 
 
 // Traditional Truffle test
-contract("MyEscrow", (accounts) => {
-  it("Should deploy normally", async function () {
-    const instance = await Contract.new();
-    const payer = accounts[1];
-    const payee = accounts[2];
-    const feeAmount = 2;
-    const feeAddress = '0xF4255c5e53a08f72b0573D1b8905C5a50aA9c2De';
-    const paymentReference = '0xaaaa';
+contract("MyEscrow", async (accounts) => {
+  const DAI_address = '0x38cF23C52Bb4B13F051Aec09580a2dE845a7FA35';
   
-    console.log(`MyEscrow is deployed to address: ${instance.address}`);
-  })
-});
+  const Owner = accounts[0];
+  const payer = accounts[1];
+  const payee = accounts[2];
+  const feeAddress = accounts[3];
+  const amount = 10;
+  const feeAmount = 2;
+  const paymentReferece = '0xaaaa';
+  const paymentToken = DAI_address;
 
+  let instance;
+
+  before(async () => {
+   instance = await Contract.deployed(ERC20FeeProxy, {from: Owner});
+  });
+
+  describe("Deployment tests", () => {
+    it("Should deploy as normal and print out the contract address.", async () => {
+      console.log(`MyEscrow is deployed to address: ${instance.address}`);
+    });
+  });
+
+  describe("Deposits to Escrow", () => {
+    it("Should Init and deposit ERC20 token to MyEscrow contract", async () => {
+        await instance.initAndDeposit(paymentToken, amount, payee, paymentReferece, feeAmount, feeAddress, {from: Owner,});
+        let result = await instance.getInvoice(paymentReferece);
+        aassert(result.amount.toNumber() ===  10, "MyEscrow contract is not funded properly");
+    });
+  });
+});
 ///  init and deposit
 /// widthdraw as payer
 /// withdraw from payee or other address - Revert
@@ -36,5 +54,3 @@ contract("MyEscrow", (accounts) => {
 
 /// wednesday presentation for Yoann
 /// Julien, chris thursday?
-
-¨
