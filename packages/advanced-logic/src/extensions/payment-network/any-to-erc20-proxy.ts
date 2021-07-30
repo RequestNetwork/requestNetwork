e02edbe5df4b728880ba733d715c4a0235f95087
yomarion <yoann.marion@gmail.com>
2021-07-30 18:15:31 +0200

    feat: matic aggregators DAI USDC USDT

--- packages/advanced-logic/src/extensions/payment-network/any-to-erc20-proxy.ts
+++ packages/advanced-logic/src/extensions/payment-network/any-to-erc20-proxy.ts
@@ -42,6 +42,16 @@ const supportedCurrencies: Record<string, Record<RequestLogicTypes.CURRENCY, str
     [RequestLogicTypes.CURRENCY.ETH]: ['ETH'],
     [RequestLogicTypes.CURRENCY.BTC]: [],
   },
+  matic: {
+    ISO4217: ['USD'],
+    ERC20: [
+      '0x6b175474e89094c44da98b954eedeac495271d0f',
+      '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
+      '0xdac17f958d2ee523a2206206994597c13d831ec7',
+    ],
+    ETH: ['ETH'],
+    BTC: [],
+  },
 };
 
 export default class AnyToErc20ProxyPaymentNetwork extends Erc20FeeProxyPaymentNetwork {
