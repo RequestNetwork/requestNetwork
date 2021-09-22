b32c85de82cf6aea6ac7f406e339180f7e2d9218
Vincent <4611986+vrolland@users.noreply.github.com>
2021-09-22 11:07:28 +0200

    feat: payment network ethereum fee proxy in advanced logic (#582)

--- packages/advanced-logic/src/extensions/payment-network/ethereum/fee-proxy-contract.ts
+++ packages/advanced-logic/src/extensions/payment-network/ethereum/fee-proxy-contract.ts
@@ -1,32 +1,21 @@
 import { ExtensionTypes, RequestLogicTypes } from '@requestnetwork/types';
 import FeeReferenceBasedPaymentNetwork from '../fee-reference-based';
-
 import * as walletAddressValidator from 'wallet-address-validator';
 
 const CURRENT_VERSION = '0.2.0';
 
 /**
- * Implementation of the payment network to pay in ERC20, including third-party fees payment, based on a reference provided to a proxy contract.
+ * Implementation of the payment network to pay in Ethereum, including third-party fees payment, based on a reference provided to a proxy contract.
  */
-export default class Erc20FeeProxyPaymentNetwork<
+export default class EthereumFeeProxyPaymentNetwork<
   TCreationParameters extends ExtensionTypes.PnFeeReferenceBased.ICreationParameters = ExtensionTypes.PnFeeReferenceBased.ICreationParameters
 > extends FeeReferenceBasedPaymentNetwork<TCreationParameters> {
   public constructor(
-    extensionId: ExtensionTypes.ID = ExtensionTypes.ID.PAYMENT_NETWORK_ERC20_FEE_PROXY_CONTRACT,
+    extensionId: ExtensionTypes.ID = ExtensionTypes.ID.PAYMENT_NETWORK_ETH_FEE_PROXY_CONTRACT,
     currentVersion: string = CURRENT_VERSION,
-    public supportedNetworks: string[] = [
-      'mainnet',
-      'rinkeby',
-      'private',
-      'matic',
-      'mumbai',
-      'celo',
-      'alfajores',
-      'fuse',
-    ],
-    public supportedCurrencyType: string = RequestLogicTypes.CURRENCY.ERC20,
+    public supportedNetworks: string[] = ['mainnet', 'rinkeby', 'private'],
   ) {
-    super(extensionId, currentVersion, supportedNetworks, supportedCurrencyType);
+    super(extensionId, currentVersion, supportedNetworks, RequestLogicTypes.CURRENCY.ETH);
   }
 
   /**
