4ddc5e9f2ef86c82147baf21059a4c23602c2360
Alexandre ABRIOUX <alexandre-abrioux@users.noreply.github.com>
2021-10-04 13:19:10 +0200

    add tests for address based

---
+++ packages/advanced-logic/test/extensions/payment-network/address-based.test.ts
@@ -0,0 +1,39 @@
+import { ExtensionTypes, RequestLogicTypes } from '@requestnetwork/types';
+import AddressBasedPaymentNetwork from '../../../src/extensions/payment-network/address-based';
+
+describe('extensions/payment-network/address-based', () => {
+  it('address validation should throw when using unsupported currency type', () => {
+    class TestAddressBasedPaymentNetwork extends AddressBasedPaymentNetwork {
+      public testIsValidAddress() {
+        this.isValidAddress('test', 'test');
+      }
+    }
+    expect(() => {
+      const testAddressBasedPaymentNetwork = new TestAddressBasedPaymentNetwork(
+        ExtensionTypes.ID.PAYMENT_NETWORK_ERC20_ADDRESS_BASED,
+        'test',
+        [],
+        RequestLogicTypes.CURRENCY.ISO4217,
+      );
+      testAddressBasedPaymentNetwork.testIsValidAddress();
+    }).toThrowError(
+      'Default implementation of isValidAddressForNetwork() does not support currency type ISO4217. Please override this method if needed.',
+    );
+  });
+  it('address validation should throw when using unsupported currency', () => {
+    class TestAddressBasedPaymentNetwork extends AddressBasedPaymentNetwork {
+      public testIsValidAddress() {
+        this.isValidAddressForSymbolAndNetwork('test', 'test', 'test');
+      }
+    }
+    expect(() => {
+      const testAddressBasedPaymentNetwork = new TestAddressBasedPaymentNetwork(
+        ExtensionTypes.ID.PAYMENT_NETWORK_ERC20_ADDRESS_BASED,
+        'test',
+        [],
+        RequestLogicTypes.CURRENCY.ERC20,
+      );
+      testAddressBasedPaymentNetwork.testIsValidAddress();
+    }).toThrowError('Currency not found in default manager: test / test');
+  });
+});
