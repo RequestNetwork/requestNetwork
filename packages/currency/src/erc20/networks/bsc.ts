c6746d44ca7623b1bb1ba41be7751482382c52e1
Bertrand Juglas <b@juglas.name>
2021-10-06 17:56:29 +0200

    fixing checksum

--- packages/currency/src/erc20/networks/bsc.ts
+++ packages/currency/src/erc20/networks/bsc.ts
@@ -2,7 +2,7 @@ import { TokenMap } from './types';
 
 // List of the supported bsc network tokens
 export const supportedBSCERC20: TokenMap = {
-  '0x1af3f329e8be154074d8769d1ffa4ee058b1dbc3': {
+  '0x1AF3F329e8BE154074D8769D1FFa4eE058B1DBc3': {
     name: 'Binance-Peg Dai Token',
     symbol: 'DAI',
     decimals: 18,
