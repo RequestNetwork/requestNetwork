a2aef61bf6972f751aee5801eb91465518c6befb
Kevin Dave <kevindavee1505@gmail.com>
2021-10-08 12:49:43 +0700

    fix: wrong interface inheritance + delete duplicate

--- packages/types/src/extensions/pn-any-to-erc20-types.ts
+++ packages/types/src/extensions/pn-any-to-erc20-types.ts
@@ -3,7 +3,7 @@ import * as PnAnyToAnyConversion from './pn-any-to-any-conversion-types';
 
 /** Any to ERC20 reference-based payment network extension interface */
 export interface IAnyToERC20 extends PnAnyToAnyConversion.IConversionReferenceBased {
-  createCreationAction: (creationParameters: ICreationParameters) => Extension.IAction;
+  createCreationAction: (parameters?: ICreationParameters) => Extension.IAction;
 }
 
 /** Parameters for the creation action */
