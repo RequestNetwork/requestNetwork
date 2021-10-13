bee7e9f23352e19c288598e391326672ef523a78
Darko Kolev <kolevdarko@gmail.com>
2021-10-13 19:36:36 +0200

    feat: adding usdc token on xdai (#628)

---
+++ packages/currency/src/erc20/networks/xdai.ts
@@ -0,0 +1,10 @@
+import { TokenMap } from './types';
+
+// List of the supported bsc network tokens
+export const supportedXDAIERC20: TokenMap = {
+  '0xDDAfbb505ad214D7b80b1f830fcCc89B60fb7A83': {
+    name: 'USDC token on xDai',
+    symbol: 'USDC',
+    decimals: 6,
+  },
+};
