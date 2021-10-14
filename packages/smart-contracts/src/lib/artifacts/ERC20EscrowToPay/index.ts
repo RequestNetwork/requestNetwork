e18a45e01dcd19b60f8229cc2f6694d330f1137c
Ivo Garofalo <ivo.garofalo@gmail.com>
2021-10-14 16:55:46 +0200

    Add erc20EscrowToPayArtifact to src/lib/artifacts

---
+++ packages/smart-contracts/src/lib/artifacts/ERC20EscrowToPay/index.ts
@@ -0,0 +1,27 @@
+import { ContractArtifact } from '../../ContractArtifact';
+
+import { abi as ABI_0_1_0 } from './0.1.0.json';
+import { ERC20EscrowToPayV1 } from '../../../types/ERC20EscrowToPayV1';
+
+export const erc20EscrowToPayArtifact = new ContractArtifact<ERC20EscrowToPayV1>(
+  {
+    '0.1.0': {
+      abi: ABI_0_1_0,
+      deployment: {
+        private: {
+          address: '0xdDA6327139485221633A1FcD65f4aC932E60A2e1',
+          creationBlockNumber: 0,
+        },
+        mainnet: {
+          address: '',
+          creationBlockNumber: 10774767,
+        },
+        rinkeby: {
+          address: '',
+          creationBlockNumber: 7118080,
+        },
+      },
+    },
+  },
+  '0.1.0',
+);
