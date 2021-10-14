3537f3175df346345bc448a1eb708a52ddbdbc62
Ivo Garofalo <ivo.garofalo@gmail.com>
2021-10-14 16:57:47 +0200

    Add ./ERC20EscrowToPay to scr/lib/artifacts/index

--- packages/smart-contracts/src/lib/artifacts/index.ts
+++ packages/smart-contracts/src/lib/artifacts/index.ts
@@ -6,6 +6,7 @@ export * from './Erc20ConversionProxy';
 export * from './ERC20FeeProxy';
 export * from './ERC20Proxy';
 export * from './ERC20SwapToPay';
+export * from './ERC20EscrowToPay';
 export * from './Erc20SwapConversion';
 export * from './EthereumProxy';
 export * from './EthereumFeeProxy';
