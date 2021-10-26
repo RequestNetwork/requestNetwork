87c7e7cb9cf5219de48a63d1acfdcdbf5e9e6af8
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2021-10-26 15:34:36 +0200

    fix type import

--- packages/payment-detection/src/reference-based-detector.ts
+++ packages/payment-detection/src/reference-based-detector.ts
@@ -11,7 +11,6 @@ import PaymentReferenceCalculator from './payment-reference-calculator';
 
 import { BigNumber } from 'ethers';
 import DeclarativePaymentNetwork from './declarative';
-import { IDeclarativePaymentEventParameters } from 'types/src/payment-types';
 
 /**
  * Abstract class to extend to get the payment balance of reference based requests
@@ -22,7 +21,9 @@ export abstract class ReferenceBasedDetector<
   >
   extends DeclarativePaymentNetwork<ExtensionType>
   implements
-    PaymentTypes.IPaymentNetwork<TPaymentEventParameters | IDeclarativePaymentEventParameters> {
+    PaymentTypes.IPaymentNetwork<
+      TPaymentEventParameters | PaymentTypes.IDeclarativePaymentEventParameters
+    > {
   /**
    * @param extension The advanced logic payment network extension, reference based
    * @param extensionType Example : ExtensionTypes.ID.PAYMENT_NETWORK_ETH_INPUT_DATA
@@ -100,7 +101,9 @@ export abstract class ReferenceBasedDetector<
   public async getBalance(
     request: RequestLogicTypes.IRequest,
   ): Promise<
-    PaymentTypes.IBalanceWithEvents<TPaymentEventParameters | IDeclarativePaymentEventParameters>
+    PaymentTypes.IBalanceWithEvents<
+      TPaymentEventParameters | PaymentTypes.IDeclarativePaymentEventParameters
+    >
   > {
     const paymentNetwork = request.extensions[this.extensionType];
     const paymentChain = this.getPaymentChain(request.currency, paymentNetwork);
@@ -146,14 +149,14 @@ export abstract class ReferenceBasedDetector<
         .toString();
 
       const events: PaymentTypes.IPaymentNetworkEvent<
-        TPaymentEventParameters | IDeclarativePaymentEventParameters
+        TPaymentEventParameters | PaymentTypes.IDeclarativePaymentEventParameters
       >[] = [...declaredBalance.events, ...payments.events, ...refunds.events].sort(
         (
           a: PaymentTypes.IPaymentNetworkEvent<
-            TPaymentEventParameters | IDeclarativePaymentEventParameters
+            TPaymentEventParameters | PaymentTypes.IDeclarativePaymentEventParameters
           >,
           b: PaymentTypes.IPaymentNetworkEvent<
-            TPaymentEventParameters | IDeclarativePaymentEventParameters
+            TPaymentEventParameters | PaymentTypes.IDeclarativePaymentEventParameters
           >,
         ) => (a.timestamp || 0) - (b.timestamp || 0),
       );
