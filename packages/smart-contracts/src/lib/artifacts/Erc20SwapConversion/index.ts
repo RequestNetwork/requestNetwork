06e92b459c8822ea20989068bcd9212f48171db3
Yo <56731761+yomarion@users.noreply.github.com>
2021-10-26 17:24:49 +0200

    feat: Fantom deployments (#654)
    * feat: deployment information on Fantom
    
    * chore: deployment env and README

--- packages/smart-contracts/src/lib/artifacts/Erc20SwapConversion/index.ts
+++ packages/smart-contracts/src/lib/artifacts/Erc20SwapConversion/index.ts
@@ -25,6 +25,10 @@ export const erc20SwapConversionArtifact = new ContractArtifact<ERC20SwapToConve
           address: '0x1d6B06C6f7adFd9314BD4C58a6D306261113a1D4',
           creationBlockNumber: 12759710,
         },
+        fantom: {
+          address: '0x1d6B06C6f7adFd9314BD4C58a6D306261113a1D4',
+          creationBlockNumber: 20066424,
+        },
       },
     },
   },
--- packages/smart-contracts/src/lib/artifacts/EthereumProxy/index.ts
+++ packages/smart-contracts/src/lib/artifacts/EthereumProxy/index.ts
@@ -29,6 +29,10 @@ export const ethereumProxyArtifact = new ContractArtifact<EthereumProxy>(
           address: '0x27c60BE17e853c47A9F1d280B05365f483c2dFAF',
           creationBlockNumber: 11540170,
         },
+        fantom: {
+          address: '0x27c60BE17e853c47A9F1d280B05365f483c2dFAF',
+          creationBlockNumber: 20060175,
+        },
         // FIXME: Temporarily disable this artifact to fix ETH payment detection
         //  because Matic doesn't support eth_getLogs for many blocks.
         //  The fix will be to use TheGraph for Ethereum Proxy
