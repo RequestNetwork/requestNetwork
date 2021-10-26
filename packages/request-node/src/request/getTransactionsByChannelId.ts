61a52bb8f8a165284957519ab8e18cdf6d598d52
Alexandre ABRIOUX <alexandre-abrioux@users.noreply.github.com>
2021-10-26 13:35:08 +0000

    undo test changes

--- packages/request-node/src/request/getTransactionsByChannelId.ts
+++ packages/request-node/src/request/getTransactionsByChannelId.ts
@@ -17,11 +17,45 @@ export default class GetTransactionsByChannelIdHandler {
    * @param serverResponse http server response object
    */
   async handler(clientRequest: Request, serverResponse: Response): Promise<void> {
-    void clientRequest;
-    void this.logger;
-    void this.dataAccess;
-    void GET_TRANSACTIONS_TIMEOUT;
-    serverResponse.status(StatusCodes.INTERNAL_SERVER_ERROR).send('aaa');
-    return;
+    // Retrieves data access layer
+    let transactions;
+
+    // Used to compute request time
+    const requestStartTime = Date.now();
+
+    // As the Node doesn't implement a cache, all transactions have to be retrieved directly on IPFS
+    // This operation can take a long time and then the timeout of the request should be increase
+    // PROT-187: Decrease or remove this value
+    clientRequest.setTimeout(GET_TRANSACTIONS_TIMEOUT);
+
+    const { channelId, timestampBoundaries } = clientRequest.query;
+
+    // Verifies if data sent from get request are correct
+    // clientRequest.query is expected to contain the channelId of the transactions to search for
+    if (!channelId || typeof channelId !== 'string') {
+      serverResponse.status(StatusCodes.UNPROCESSABLE_ENTITY).send('Incorrect data');
+      return;
+    }
+    try {
+      transactions = await this.dataAccess.getTransactionsByChannelId(
+        channelId,
+        timestampBoundaries && typeof timestampBoundaries === 'string'
+          ? JSON.parse(timestampBoundaries)
+          : undefined,
+      );
+
+      // Log the request time
+      const requestEndTime = Date.now();
+      this.logger.debug(
+        `getTransactionsByChannelId latency: ${requestEndTime - requestStartTime}ms`,
+        ['metric', 'latency'],
+      );
+
+      serverResponse.status(StatusCodes.OK).send(transactions);
+    } catch (e) {
+      this.logger.error(`getTransactionsByChannelId error: ${e}`);
+
+      serverResponse.status(StatusCodes.INTERNAL_SERVER_ERROR).send(e);
+    }
   }
 }
