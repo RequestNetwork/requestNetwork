640084c76bd980980317d35cc1eae079179dff04
Ivo G <41997352+Dadogg80@users.noreply.github.com>
2021-10-27 09:55:43 +0200

    Update packages/smart-contracts/hardhat.config.ts
    fix: Set compiler to v.0.8.4
    
    Co-authored-by: Vincent <4611986+vrolland@users.noreply.github.com>

--- packages/smart-contracts/hardhat.config.ts
+++ packages/smart-contracts/hardhat.config.ts
@@ -25,7 +25,7 @@ if (accounts && process.env.ADMIN_PRIVATE_KEY) {
 }
 
 export default {
-  solidity: '0.8.0',
+  solidity: '0.8.4',
   paths: {
     sources: 'src/contracts',
     tests: 'test/contracts',
