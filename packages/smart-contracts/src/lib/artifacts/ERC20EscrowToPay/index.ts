2345651ffa52cd686dea748983fd093458630d21
Ivo Garofalo <ivo.garofalo@gmail.com>
2021-10-29 17:14:10 +0200

    fix arifact

--- packages/smart-contracts/src/lib/artifacts/ERC20EscrowToPay/index.ts
+++ packages/smart-contracts/src/lib/artifacts/ERC20EscrowToPay/index.ts
@@ -1,28 +1,28 @@
 import { ContractArtifact } from '../../ContractArtifact';
 
 import { abi as ABI_0_1_0 } from './0.1.0.json';
-import type { ERC20EscrowToPay } from 'smart-contracts/src/types/';
-
+// @ts-ignore Cannot find module
+import type { ERC20EscrowToPay } from '../../../types/ERC2EscrowToPay';
 
 export const erc20EscrowToPayArtifact = new ContractArtifact<ERC20EscrowToPay>(
     {
-        '0.1.0': {
-            abi: ABI_0_1_0,
-            deployment: {
-                private: {
-                    address: '0x3b863735261925EB860C7F2729e07612ff8326e8',
-                    creationBlockNumber: 0,
-                },
-                mainnet: {
-                    address: 'TODO',
-                    creationBlockNumber: 10774767,
-                },
-                rinkeby: {
-                    address: 'TODO',
-                    creationBlockNumber: 7118080,
-                },
+    '0.1.0': {
+        abi: ABI_0_1_0,
+        deployment: {
+            private: {
+                address: '0x3b863735261925EB860C7F2729e07612ff8326e8',
+                creationBlockNumber: 0,
+            },
+            mainnet: {
+                address: '',
+                creationBlockNumber: 0,
+            },
+            rinkeby: {
+                address: '',
+                creationBlockNumber: 0,
             },
         },
+    },
     },
     '0.1.0',
 );
