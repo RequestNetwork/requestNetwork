749166b27432ee295376c2e36972cb9623a69c9d
darko <kolevdarko@gmail.com>
2021-11-10 14:47:25 +0100

    added new fantom erc20 tokens

--- packages/currency/src/erc20/networks/fantom.ts
+++ packages/currency/src/erc20/networks/fantom.ts
@@ -6,4 +6,54 @@ export const supportedFantomTokens: TokenMap = {
     symbol: 'YEL',
     decimals: 18,
   },
+  '0x6a07A792ab2965C72a5B8088d3a069A7aC3a993B': {
+    name: 'AAVE Token',
+    symbol: 'AAVE',
+    decimals: 18,
+  },
+  '0xe1146b9ac456fcbb60644c36fd3f868a9072fc6e': {
+    name: 'fBTC Token',
+    symbol: 'BTC',
+    decimals: 18,
+  },
+  '0x657A1861c15A3deD9AF0B6799a195a249ebdCbc6': {
+    name: 'CREAM token',
+    symbol: 'CREAM',
+    decimals: 18,
+  },
+  '0x8d11ec38a3eb5e956b052f67da8bdc9bef8abf3e': {
+    name: 'DAI token',
+    symbol: 'DAI',
+    decimals: 18,
+  },
+  '0x658b0c7613e890ee50b8c4bc6a3f41ef411208ad': {
+    name: 'fETH token',
+    symbol: 'ETH',
+    decimals: 18,
+  },
+  '0xb3654dc3d10ea7645f8319668e8f54d2574fbdc8': {
+    name: 'LINK token',
+    symbol: 'LINK',
+    decimals: 18,
+  },
+  '0x56ee926bD8c72B2d5fa1aF4d9E4Cbb515a1E3Adc': {
+    name: 'SNX token',
+    symbol: 'SNX',
+    decimals: 18,
+  },
+  '0xae75A438b2E0cB8Bb01Ec1E1e376De11D44477CC': {
+    name: 'SUSHI token',
+    symbol: 'SUSHI',
+    decimals: 18,
+  },
+  '0x940f41f0ec9ba1a34cf001cc03347ac092f5f6b5': {
+    name: 'gfUSDT token',
+    symbol: 'USDT',
+    decimals: 18,
+  },
+  '0x04068da6c83afcfa0e13ba15a6696662335d5b75': {
+    name: 'USDC token',
+    symbol: 'USDC',
+    decimals: 18,
+  },
 };
