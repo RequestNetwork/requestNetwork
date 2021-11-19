7ca0de06fe7fa3d2ec41bb4b9f2fdd463baec5c9
Ivo Garofalo <ivo.garofalo@gmail.com>
2021-11-19 14:35:57 +0100

    add Rinkeby address

--- packages/smart-contracts/src/lib/artifacts/ERC20EscrowToPay/index.ts
+++ packages/smart-contracts/src/lib/artifacts/ERC20EscrowToPay/index.ts
@@ -5,24 +5,24 @@ import { abi as ABI_0_1_0 } from './0.1.0.json';
 import type { ERC20EscrowToPay } from '../../../types/ERC2EscrowToPay';
 
 export const erc20EscrowToPayArtifact = new ContractArtifact<ERC20EscrowToPay>(
-    {
+  {
     '0.1.0': {
-        abi: ABI_0_1_0,
-        deployment: {
-            private: {
-                address: '0xF08dF3eFDD854FEDE77Ed3b2E515090EEe765154',
-                creationBlockNumber: 0,
-            },
-            mainnet: {
-                address: '',
-                creationBlockNumber: 0,
-            },
-            rinkeby: {
-                address: '0xC34C8a06D1ffC645b01cda45886A46a5b188ba29',
-                creationBlockNumber: 0,
-            },
+      abi: ABI_0_1_0,
+      deployment: {
+        private: {
+          address: '0xF08dF3eFDD854FEDE77Ed3b2E515090EEe765154',
+          creationBlockNumber: 0,
         },
+        mainnet: {
+          address: '',
+          creationBlockNumber: 0,
+        },
+        rinkeby: {
+          address: '0x8230e703B1c4467A4543422b2cC3284133B9AB5e',
+          creationBlockNumber: 9669613,
+        },
+      },
     },
-    },
-    '0.1.0',
+  },
+  '0.1.0',
 );
