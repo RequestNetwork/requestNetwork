3711ed24669f96ddf04079273175c913e158b96b
Ivo Garofalo <ivo.garofalo@gmail.com>
2021-12-10 12:56:42 +0100

    remove comments and non-unused code

--- packages/payment-detection/test/erc20/escrow-info-retriever.test.ts
+++ packages/payment-detection/test/erc20/escrow-info-retriever.test.ts
@@ -25,13 +25,14 @@ describe('api/erc20/escrow-info-retriever', () => {
     let infoRetriever: EscrowERC20InfoRetriever;
 
     const mockedGetLogs = (filter: ethers.EventFilter) => {
-      const emergencyClaimTopic =
+      // Assigns the correct topic for initEmergencyClaim.
+      const initEmergencyClaimTopic =
         '0x37b4fae7fd90ce3674204f79d686d40c4069a66c402976717d4f30817c0c0939';
       const frozenRequestTopic =
         '0x5e14b9b7c3d9675ce5ecb24ee8181d371561709a08aa9c412acb36627386dba8';
       const revertedEmergencyTopic =
         '0xcc401323a0bd24a2e6e1564e168c52332731fff1a2937d998ee25462588ba0fa';
-      if (filter.topics?.includes(emergencyClaimTopic)) {
+      if (filter.topics?.includes(initEmergencyClaimTopic)) {
         return Promise.resolve([initEmergencyLog]);
       }
       if (filter.topics?.includes(frozenRequestTopic)) {
@@ -116,7 +117,6 @@ describe('api/erc20/escrow-info-retriever', () => {
           timestamp: 69,
         };
       };
-      console.log('Step one done');
     });
 
     it('can get the events of an address out of mocked logs', async () => {
@@ -129,119 +129,3 @@ describe('api/erc20/escrow-info-retriever', () => {
     });
   });
 });
-//expect(events[0].name).toBe(PaymentTypes.EVENTS_NAMES.INITIATED_EMERGENCY_CLAIM);
-
-/*
-      // inject mock provider.getLogs()
-      infoRetriever.provider.getLogs = (filter: ethers.EventFilter): any => {
-        if (
-          !filter.topics?.includes(
-            // InitiatedEmergencyClaim
-            '0x37b4fae7fd90ce3674204f79d686d40c4069a66c402976717d4f30817c0c0939',
-          )
-        ) {
-          return [];
-        }
-        return [
-          {
-            address: escrowContractAddress,
-            blockHash: '0xe8693d0fb897eac7c350696834f4cf36be3b7d86f746d6764d9903a06dc5fe44',
-            blockNumber: 115,
-            data: '0x0797560800000000000000000000000000000000000000000000000000000000000000200000000000000000000000000000000000000000000000000000000000000002aaaa000000000000000000000000000000000000000000000000000000000000',
-            logIndex: 2,
-            topics: [
-              '0x37b4fae7fd90ce3674204f79d686d40c4069a66c402976717d4f30817c0c0939',
-              '0x6330b989705733cc5c1f7285b8a5b892e08be86ed6fbe9d254713a4277bc5bd2',
-            ],
-            transactionHash: '0x66e20126e917ede3543c79357c71ddeda2af68e27d684fd5aa6a813bb6946ce4',
-            transactionIndex: 0,
-          },
-        ];
-      };
-      */
-
-/*
-    it('can get the localhost fees of an address', async () => {
-      const infoRetriever = new ProxyERC20InfoRetriever(
-        'b7182613b46c5e92',
-        feeProxyContractAddress,
-        0,
-        erc20LocalhostContractAddress,
-        '0x627306090abab3a6e1400e9345bc60c78a8bef57',
-        PaymentTypes.EVENTS_NAMES.PAYMENT,
-        'private',
-      );
-
-      // inject mock provider.getLogs()
-      infoRetriever.provider.getLogs = (filter: ethers.EventFilter): any => {
-        if (
-          !filter.topics?.includes(
-            '0x9f16cbcc523c67a60c450e5ffe4f3b7b6dbe772e7abcadb2686ce029a9a0a2b6',
-          )
-        ) {
-          return [];
-        }
-        return [
-          {
-            blockNumber: 28,
-            blockHash: '0x40496f2205f0c8d819c2cab683a5a7e0b20b49d3d891c8943780138670f184c7',
-            transactionIndex: 0,
-            address: feeProxyContractAddress,
-            data:
-              '0x0000000000000000000000009fbda871d559710256a2502a2517b794b482db40000000000000000000000000627306090abab3a6e1400e9345bc60c78a8bef57000000000000000000000000000000000000000000000000000000000000000a0000000000000000000000000000000000000000000000000000000000000001000000000000000000000000c5fdf4076b8f3a5357c5e395ab970b5b54098fef',
-            topics: [
-              '0x9f16cbcc523c67a60c450e5ffe4f3b7b6dbe772e7abcadb2686ce029a9a0a2b6',
-              '0xa1801d1208f939d16ff239f43c66983c01b1f107994ff695f6a195be4137c796',
-            ],
-            transactionHash: '0xa4ccc5094096fb6b2e744cb602ade7f37d0c78d8847e58471d6de786fc9c5283',
-            logIndex: 4,
-          },
-        ];
-      };
-
-      // inject mock provider.getBlock()
-      infoRetriever.provider.getBlock = (): any => {
-        return {
-          timestamp: 10,
-        };
-      };
-
-      const events = await infoRetriever.getTransferEvents();
-
-      // if this assert fails it means this address received another transaction
-      expect(events).toHaveLength(1);
-
-      const event = events[0];
-      expect(event.name).toBe(PaymentTypes.EVENTS_NAMES.PAYMENT);
-      expect(event.amount).toBe('10');
-      expect(typeof event.timestamp).toBe('number');
-
-      const parameters: PaymentTypes.IERC20FeePaymentEventParameters = event.parameters!;
-
-      expect(parameters.to).toBe('0x627306090abab3a6e1400e9345bc60c78a8bef57');
-      expect(typeof parameters.block).toBe('number');
-      expect(typeof parameters.txHash).toBe('string');
-      expect(parameters.feeAddress).toBe('0xC5fdf4076b8F3A5357c5E395ab970B5B54098Fef');
-      expect(parameters.feeAmount).toBe('1');
-    });
-
-    it('gets an empty list of events for an address without ERC20 on localhost', async () => {
-      const infoRetriever = new ProxyERC20InfoRetriever(
-        paymentReferenceMock,
-        proxyContractAddress,
-        0,
-        erc20LocalhostContractAddress,
-        escrowContractAddress,
-        PaymentTypes.EVENTS_NAMES.PAYMENT,
-        'private',
-      );
-
-      // inject mock provider.getLogs()
-      infoRetriever.provider.getLogs = (): any => {
-        return [];
-      };
-
-      const events = await infoRetriever.getTransferEvents();
-      expect(Object.keys(events)).toHaveLength(0);
-    });
-    */
