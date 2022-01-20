f4ad4f0d7e1a2b8b4c8d617135eccad66cd3bee1
Yo <56731761+yomarion@users.noreply.github.com>
2022-01-20 16:13:27 +0100

    feat(artifacts): ETHConversionProxy v2 on Fantom (#745)

--- packages/smart-contracts/src/lib/artifacts/EthConversionProxy/index.ts
+++ packages/smart-contracts/src/lib/artifacts/EthConversionProxy/index.ts
@@ -47,6 +47,23 @@ export const ethConversionArtifact = new ContractArtifact<EthConversionProxy>(
           address: '0x7Ebf48a26253810629C191b56C3212Fd0D211c26',
           creationBlockNumber: 10023415,
         },
+        fantom: {
+          address: '0x7Ebf48a26253810629C191b56C3212Fd0D211c26',
+          creationBlockNumber: 28552915,
+        },
+        /* All the contracts below have not been updated with 0.2.0 yet */
+        mainnet: {
+          address: '0xCa3353a15fCb5C83a1Ff64BFf055781aC5c4d2F4',
+          creationBlockNumber: 13765042,
+        },
+        matic: {
+          address: '0xCa3353a15fCb5C83a1Ff64BFf055781aC5c4d2F4',
+          creationBlockNumber: 20670503,
+        },
+        celo: {
+          address: '0xCa3353a15fCb5C83a1Ff64BFf055781aC5c4d2F4',
+          creationBlockNumber: 10141038,
+        },
       },
     },
   },
