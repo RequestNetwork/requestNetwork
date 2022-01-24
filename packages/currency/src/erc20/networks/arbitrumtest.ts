1f71dcb813fd5d774fcc5b9c83e15e972ec39164
Leo Sollier <leo.sollier@request.network>
2022-01-24 23:15:40 +0100

    fix: checksum

--- packages/currency/src/erc20/networks/arbitrumtest.ts
+++ packages/currency/src/erc20/networks/arbitrumtest.ts
@@ -3,7 +3,7 @@ import { TokenMap } from './types';
 // List of the supported arbitrum testnet ERC20 tokens
 export const supportedArbitrumTestERC20: TokenMap = {
   // Faucet Token on arbitrum testnet network. Easy to use on tests.
-  '0x7b91473d1ed6ad359132150b9cbf70452221e5e7': {
+  '0x7B91473d1Ed6Ad359132150B9CBF70452221e5E7': {
     decimals: 18,
     name: 'Arbitrum Faucet Token',
     symbol: 'ARFAU',
