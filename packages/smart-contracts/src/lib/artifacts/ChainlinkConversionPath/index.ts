b59806d00cf88714fae6bd1fd6201554ba8ef0d9
leoslr <50319677+leoslr@users.noreply.github.com>
2022-01-25 15:26:01 +0100

    fix: naming (#750)

--- packages/smart-contracts/src/lib/artifacts/ChainlinkConversionPath/index.ts
+++ packages/smart-contracts/src/lib/artifacts/ChainlinkConversionPath/index.ts
@@ -46,7 +46,7 @@ export const chainlinkConversionPath = new ContractArtifact<ChainlinkConversionP
           address: '0xEEc4790306C43DC00cebbE4D0c36Fadf8634B533',
           creationBlockNumber: 10141032,
         },
-        'arbitrum-testnet': {
+        'arbitrum-rinkeby': {
           address: '0xEEc4790306C43DC00cebbE4D0c36Fadf8634B533',
           creationBlockNumber: 8403926,
         },
