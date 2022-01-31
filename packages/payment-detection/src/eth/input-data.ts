a7b9267e480281315a9fbc05bbe37a777022ddf7
Darko Kolev <kolevdarko@gmail.com>
2022-01-31 12:14:12 +0100

    handling optional proxyContract for input data

--- packages/payment-detection/src/eth/input-data.ts
+++ packages/payment-detection/src/eth/input-data.ts
@@ -12,6 +12,7 @@ import { ReferenceBasedDetector } from '../reference-based-detector';
 import { makeGetDeploymentInformation } from '../utils';
 import { networkSupportsTheGraphForNativePayments } from '../thegraph';
 import { TheGraphInfoRetriever } from '../erc20/thegraph-info-retriever';
+import { DeploymentInformation } from '@requestnetwork/smart-contracts';
 
 // interface of the object indexing the proxy contract version
 interface IProxyContractVersion {
@@ -23,6 +24,8 @@ const PROXY_CONTRACT_ADDRESS_MAP: IProxyContractVersion = {
   ['0.1.0']: '0.1.0',
   ['0.2.0']: '0.1.0',
 };
+
+class MissingProxyContract extends Error {}
 
 /**
  * Handle payment networks with ETH input data extension
@@ -79,12 +82,12 @@ export class EthInputDataPaymentDetector extends ReferenceBasedDetector<
       this.explorerApiKeys[paymentChain],
     );
     const events = await infoRetriever.getTransferEvents();
-    const proxyContractArtifact = this.getProxyDeploymentInformation(
-      paymentChain,
-      paymentNetwork.version,
-    );
+    try {
+      const proxyContractArtifact = this.getProxyDeploymentInformation(
+        paymentChain,
+        paymentNetwork.version,
+      );
 
-    if (proxyContractArtifact) {
       const proxyInfoRetriever = networkSupportsTheGraphForNativePayments(paymentChain)
         ? new TheGraphInfoRetriever(
             paymentReference,
@@ -105,12 +108,29 @@ export class EthInputDataPaymentDetector extends ReferenceBasedDetector<
 
       const proxyEvents = await proxyInfoRetriever.getTransferEvents();
       events.push(...proxyEvents);
+    } catch (e) {
+      if (e instanceof MissingProxyContract) {
+        console.log('Missing proxy contract for input data');
+      } else {
+        throw e;
+      }
     }
     return events;
   }
 
-  protected getProxyDeploymentInformation(networkName: string, version: string) {
-    return EthInputDataPaymentDetector.getDeploymentInformation(networkName, version);
+  protected getProxyDeploymentInformation(
+    networkName: string,
+    version: string,
+  ): DeploymentInformation {
+    const proxyContractArticlard = EthInputDataPaymentDetector.getDeploymentInformation(
+      networkName,
+      version,
+    );
+    if (proxyContractArticlard) {
+      return proxyContractArticlard;
+    } else {
+      throw new MissingProxyContract();
+    }
   }
 
   /*
