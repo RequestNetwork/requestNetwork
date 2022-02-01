b1e21523d8312d320553d764ce27c99738560d9c
Leo Sollier <leo.sollier@request.network>
2022-02-01 15:45:52 +0100

    style: apply prettier

--- packages/smart-contracts/scripts/3_deploy_chainlink_contract.ts
+++ packages/smart-contracts/scripts/3_deploy_chainlink_contract.ts
@@ -62,17 +62,15 @@ export default async function deploy(
     )
   )?.address;
   const localSwapRouterAddress = '0x4E72770760c011647D4873f60A3CF6cDeA896CD8';
-  const {
-    address: erc20SwapConversionAddress,
-    instance: erc20SwapConversion,
-  } = await deploySwapConversion(
-    {
-      ...args,
-      conversionProxyAddress: erc20ConversionAddress,
-      swapProxyAddress: localSwapRouterAddress,
-    },
-    hre,
-  );
+  const { address: erc20SwapConversionAddress, instance: erc20SwapConversion } =
+    await deploySwapConversion(
+      {
+        ...args,
+        conversionProxyAddress: erc20ConversionAddress,
+        swapProxyAddress: localSwapRouterAddress,
+      },
+      hre,
+    );
   if (!erc20SwapConversion) {
     console.error('Deployment for erc20SwapConversion failed.');
     return;
