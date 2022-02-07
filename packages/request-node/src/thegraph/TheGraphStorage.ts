c621373050e508f32dccb2c72f35f93d544c81b8
Alexandre ABRIOUX <alexandre-abrioux@users.noreply.github.com>
2022-02-07 16:55:40 +0100

    chore(request-node): eip-1559 for TheGraphDataAccess (#760)

--- packages/request-node/src/thegraph/TheGraphStorage.ts
+++ packages/request-node/src/thegraph/TheGraphStorage.ts
@@ -1,10 +1,13 @@
 import { EventEmitter } from 'events';
-import { utils, Signer, ContractReceipt } from 'ethers';
+import { BigNumber, ContractReceipt, PayableOverrides, providers, Signer, utils } from 'ethers';
 import TypedEmitter from 'typed-emitter';
 import Utils from '@requestnetwork/utils';
 import { LogTypes, StorageTypes } from '@requestnetwork/types';
 import { requestHashSubmitterArtifact } from '@requestnetwork/smart-contracts';
 import { RequestOpenHashSubmitter } from '@requestnetwork/smart-contracts/types';
+import { suggestFees } from 'eip1559-fee-suggestions-ethers';
+import { GasPriceDefiner } from '@requestnetwork/ethereum-storage';
+import assert from 'assert';
 
 type TheGraphStorageProps = {
   network: string;
@@ -19,15 +22,22 @@ export type TheGraphStorageEventEmitter = TypedEmitter<{
 }>;
 
 export class TheGraphStorage {
-  private logger: LogTypes.ILogger;
-  private ipfsStorage: StorageTypes.IIpfsStorage;
-  private hashSubmitter: RequestOpenHashSubmitter;
-  private network: string;
+  private readonly logger: LogTypes.ILogger;
+  private readonly ipfsStorage: StorageTypes.IIpfsStorage;
+  private readonly hashSubmitter: RequestOpenHashSubmitter;
+  private readonly network: string;
+  private readonly provider: providers.JsonRpcProvider;
+  private enableEip1559 = true;
 
   constructor({ network, signer, ipfsStorage, logger }: TheGraphStorageProps) {
     this.logger = logger || new Utils.SimpleLogger();
     this.ipfsStorage = ipfsStorage;
     this.network = network;
+    assert(
+      signer.provider instanceof providers.JsonRpcProvider,
+      'TheGraphStorage provider must be a JsonRpcProvider',
+    );
+    this.provider = signer.provider;
     this.hashSubmitter = requestHashSubmitterArtifact.connect(
       network,
       signer,
@@ -36,6 +46,14 @@ export class TheGraphStorage {
 
   async initialize(): Promise<void> {
     await this.ipfsStorage.initialize();
+    try {
+      await this.provider.send('eth_feeHistory', []);
+    } catch (e) {
+      this.logger.warn(
+        'This RPC provider does not support the "eth_feeHistory" method: switching to legacy gas price',
+      );
+      this.enableEip1559 = false;
+    }
     this.logger.debug('TheGraph storage initialized');
   }
 
@@ -43,10 +61,27 @@ export class TheGraphStorage {
     const { ipfsHash, ipfsSize } = await this.ipfsStorage.ipfsAdd(content);
 
     const fee = await this.hashSubmitter.getFeesAmount(ipfsSize);
+    const overrides: PayableOverrides = { value: fee };
+    if (this.enableEip1559) {
+      const suggestedFee = await suggestFees(
+        this.hashSubmitter.provider as providers.JsonRpcProvider,
+      );
+      overrides.maxFeePerGas = BigNumber.from(suggestedFee.baseFeeSuggestion);
+      overrides.maxPriorityFeePerGas = BigNumber.from(
+        suggestedFee.maxPriorityFeeSuggestions.urgent,
+      );
+    } else {
+      // retro-compatibility for networks where the eth_feeHistory RPC method is not available (pre EIP-1559)
+      const gasPriceDefiner = new GasPriceDefiner();
+      overrides.gasPrice = await gasPriceDefiner.getGasPrice(
+        StorageTypes.GasPriceType.FAST,
+        this.network,
+      );
+    }
     const tx = await this.hashSubmitter.submitHash(
       ipfsHash,
       utils.hexZeroPad(utils.hexlify(ipfsSize), 32),
-      { value: fee },
+      overrides,
     );
 
     const eventEmitter = new EventEmitter() as TheGraphStorageEventEmitter;
@@ -66,13 +101,13 @@ export class TheGraphStorage {
 
     void tx
       .wait()
-      .then((receipt) => {
+      .then((receipt: providers.TransactionReceipt) => {
         this.logger.debug(
           `TX ${receipt.transactionHash} confirmed at block ${receipt.blockNumber}`,
         );
         eventEmitter.emit('confirmed', receipt);
       })
-      .catch((e) => eventEmitter.emit('error', e));
+      .catch((e: Error) => eventEmitter.emit('error', e));
 
     return Object.assign(eventEmitter, result);
   }
