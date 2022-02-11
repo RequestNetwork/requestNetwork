9dec8ce510bfb20fa177536baac13d8dfff5e969
Bertrand Juglas <b@juglas.name>
2022-02-11 20:08:51 +0100

    fix: update mock and test

--- packages/integration-test/test/scheduled/mocks.ts
+++ packages/integration-test/test/scheduled/mocks.ts
@@ -1,17 +1,40 @@
 import { AdvancedLogicTypes } from '@requestnetwork/types';
 
+const createAddPaymentAddressAction = jest.fn();
+const createAddRefundAddressAction = jest.fn();
+const createCreationAction = jest.fn();
+const createAddFeeAction = jest.fn();
+const createAddPaymentInstructionAction = jest.fn();
+const createAddRefundInstructionAction = jest.fn();
+
 export const mockAdvancedLogic: AdvancedLogicTypes.IAdvancedLogic = {
-  applyActionToExtensions: jest.fn(),
+  applyActionToExtensions(): any {
+    return;
+  },
   extensions: {
     addressBasedErc20: {
-      createAddPaymentAddressAction: jest.fn(),
-      createAddRefundAddressAction: jest.fn(),
-      createCreationAction: jest.fn(),
+      createAddPaymentAddressAction,
+      createAddRefundAddressAction,
+      createCreationAction,
     },
-    streamErc777: {
-      createAddPaymentAddressAction: jest.fn(),
-      createAddRefundAddressAction: jest.fn(),
-      createCreationAction: jest.fn(),
+    feeProxyContractErc20: {
+      supportedNetworks: ['mainnet', 'rinkeby'],
+      createAddPaymentAddressAction,
+      createAddRefundAddressAction,
+      createCreationAction,
+      createAddFeeAction,
+      // inherited from declarative
+      createAddPaymentInstructionAction,
+      createAddRefundInstructionAction,
+    },
+    proxyContractErc20: {
+      supportedNetworks: ['rinkeby'],
+      createAddPaymentAddressAction,
+      createAddRefundAddressAction,
+      createCreationAction,
+      // inheritance from declarative
+      createAddPaymentInstructionAction,
+      createAddRefundInstructionAction,
     },
   },
 };
