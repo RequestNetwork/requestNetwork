ad14e090d9d821b17f5d5b58bb2575bb37b6c18d
Bertrand Juglas <b@juglas.name>
2022-02-17 18:28:55 +0100

    fix: remove unneeded deployment information

--- packages/payment-detection/src/erc777/superfluid-detector.ts
+++ packages/payment-detection/src/erc777/superfluid-detector.ts
@@ -4,15 +4,8 @@ import {
   PaymentTypes,
   RequestLogicTypes,
 } from '@requestnetwork/types';
-import { erc20ProxyArtifact } from '@requestnetwork/smart-contracts';
-import { NetworkNotSupported, VersionNotSupported } from '../balance-error';
 import { SuperFluidInfoRetriever } from './superfluid-retriever';
-import { makeGetDeploymentInformation } from '../utils';
 import { ReferenceBasedDetector } from '../reference-based-detector';
-
-const SF_CONTRACT_ADDRESS_MAP = {
-  ['0.1.0']: '0.1.0',
-};
 
 /**
  * Handle payment networks with ERC777 Superfluid streaming extension
@@ -47,28 +40,9 @@ export class SuperFluidPaymentDetector extends ReferenceBasedDetector<
     paymentReference: string,
     requestCurrency: RequestLogicTypes.ICurrency,
     paymentChain: string,
-    paymentNetwork: ExtensionTypes.IState<ExtensionTypes.PnReferenceBased.ICreationParameters>,
   ): Promise<PaymentTypes.IPaymentNetworkEvent<PaymentTypes.IERC20PaymentEventParameters>[]> {
     if (!address) {
       return [];
-    }
-
-    try {
-      SuperFluidPaymentDetector.getDeploymentInformation(paymentChain, paymentNetwork.version);
-    } catch (e) {
-      const errMessage = (e as Error)?.message || '';
-      if (errMessage.startsWith('No deployment for network')) {
-        throw new NetworkNotSupported(
-          `Network not supported for this payment network: ${paymentChain}`,
-        );
-      }
-      if (
-        errMessage.startsWith('No contract matches payment network version') ||
-        errMessage.startsWith('No deployment for version')
-      ) {
-        throw new VersionNotSupported(errMessage);
-      }
-      throw e;
     }
 
     const infoRetriever = new SuperFluidInfoRetriever(
@@ -81,12 +55,4 @@ export class SuperFluidPaymentDetector extends ReferenceBasedDetector<
 
     return infoRetriever.getTransferEvents();
   }
-
-  /*
-   * Returns deployment information for the underlying smart contract for a given payment network version
-   */
-  public static getDeploymentInformation = makeGetDeploymentInformation(
-    erc20ProxyArtifact,
-    SF_CONTRACT_ADDRESS_MAP,
-  );
 }
