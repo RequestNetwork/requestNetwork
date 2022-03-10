013ec2bee6264fb58124945a987f5486c97dcc07
Leo Sollier <leo.sollier@request.network>
2022-03-10 19:14:28 +0100

    method to deploy one contract through create2

---
+++ packages/smart-contracts/scripts-create2/deploy-one.ts
@@ -0,0 +1,43 @@
+import { HardhatRuntimeEnvironmentExtended, IDeploymentParams, isContractDeployed } from './utils';
+
+// Deploys, set up the contracts
+export async function deployOneWithCreate2(
+  deploymentParams: IDeploymentParams,
+  hre: HardhatRuntimeEnvironmentExtended,
+): Promise<string> {
+  try {
+    if (!hre.config.xdeploy.networks || hre.config.xdeploy.networks.length === 0) {
+      throw new Error('Invalid networks');
+    }
+    // Deploy the contract on several network through xdeployer
+    const deploymentResult = await hre.run('xdeploy', deploymentParams);
+    for (let i = 0; i < hre.config.xdeploy.networks.length; i++) {
+      if (deploymentResult[i].deployed) {
+        console.log(`${deploymentParams.contract} succesffuly deployed:`);
+        console.log(`         On network:        ${hre.config.xdeploy.networks[i]}`);
+        console.log(`               At address:        ${deploymentResult[i].address}`);
+        console.log(`               At block:          ${deploymentResult[i].blockNumber}`);
+      } else {
+        if (
+          isContractDeployed(
+            deploymentParams.contract,
+            hre.config.xdeploy.networks[i],
+            deploymentResult[i].address,
+          )
+        ) {
+          console.log(`${deploymentParams.contract} already deployed:`);
+          console.log(`         On network:        ${hre.config.xdeploy.networks[i]}`);
+          console.log(`               At address:        ${deploymentResult[i].address}`);
+        } else {
+          console.log(`${deploymentParams.contract} has not been deployed:`);
+          console.log(`         On network:        ${hre.config.xdeploy.networks[i]}`);
+          console.log(`               Error:             ${deploymentResult[i].error.reason}`);
+          console.log(`               Hint:              Check that your artefacts are up to date`);
+        }
+      }
+    }
+    return deploymentResult[0].address;
+  } catch (e) {
+    throw new Error(e.toString());
+  }
+}
