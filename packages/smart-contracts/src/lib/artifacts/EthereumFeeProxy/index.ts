1ea6eea383325678d18fd82456e181a25fc41a3a
Leo Sollier <leo.sollier@request.network>
2022-03-15 19:35:34 +0100

    update artefacts with proxies latest version

--- packages/smart-contracts/src/lib/artifacts/EthereumFeeProxy/index.ts
+++ packages/smart-contracts/src/lib/artifacts/EthereumFeeProxy/index.ts
@@ -47,6 +47,47 @@ export const ethereumFeeProxyArtifact = new ContractArtifact<EthereumFeeProxy>(
         },
       },
     },
+    '0.2.0': {
+      abi: ABI_0_1_0,
+      deployment: {
+        rinkeby: {
+          address: '0xfCFBcfc4f5A421089e3Df45455F7f4985FE2D6a8',
+          creationBlockNumber: 10307582,
+        },
+        fantom: {
+          address: '0xfCFBcfc4f5A421089e3Df45455F7f4985FE2D6a8',
+          creationBlockNumber: 33495801,
+        },
+        avalanche: {
+          address: '0xfCFBcfc4f5A421089e3Df45455F7f4985FE2D6a8',
+          creationBlockNumber: 12144016,
+        },
+        fuse: {
+          address: '0xfCFBcfc4f5A421089e3Df45455F7f4985FE2D6a8',
+          creationBlockNumber: 15992819,
+        },
+        matic: {
+          address: '0xfCFBcfc4f5A421089e3Df45455F7f4985FE2D6a8',
+          creationBlockNumber: 25972999,
+        },
+        xdai: {
+          address: '0xfCFBcfc4f5A421089e3Df45455F7f4985FE2D6a8',
+          creationBlockNumber: 21123997,
+        },
+        'arbitrum-one': {
+          address: '0xfCFBcfc4f5A421089e3Df45455F7f4985FE2D6a8',
+          creationBlockNumber: 7943516,
+        },
+        bsc: {
+          address: '0xfCFBcfc4f5A421089e3Df45455F7f4985FE2D6a8',
+          creationBlockNumber: 16084639,
+        },
+        mainnet: {
+          address: '0xfCFBcfc4f5A421089e3Df45455F7f4985FE2D6a8',
+          creationBlockNumber: 14392218,
+        },
+      },
+    },
   },
-  '0.1.0',
+  '0.2.0',
 );
