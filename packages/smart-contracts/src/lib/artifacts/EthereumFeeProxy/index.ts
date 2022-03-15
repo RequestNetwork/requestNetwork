a9b2fddb4de734fa963eb67773bbdad67f53d988
Leo Sollier <leo.sollier@request.network>
2022-03-15 20:15:35 +0100

    add private network to EthereumFeeProxy

--- packages/smart-contracts/src/lib/artifacts/EthereumFeeProxy/index.ts
+++ packages/smart-contracts/src/lib/artifacts/EthereumFeeProxy/index.ts
@@ -50,6 +50,10 @@ export const ethereumFeeProxyArtifact = new ContractArtifact<EthereumFeeProxy>(
     '0.2.0': {
       abi: ABI_0_1_0,
       deployment: {
+        private: {
+          address: '0x3d49d1eF2adE060a33c6E6Aa213513A7EE9a6241',
+          creationBlockNumber: 0,
+        },
         rinkeby: {
           address: '0xfCFBcfc4f5A421089e3Df45455F7f4985FE2D6a8',
           creationBlockNumber: 10307582,
