5111b2ab31c31f286e7012a29b9986b2fdf7875c
Leo Sollier <leo.sollier@request.network>
2022-03-15 18:32:44 +0100

    update hardhat config

--- packages/smart-contracts/hardhat.config.ts
+++ packages/smart-contracts/hardhat.config.ts
@@ -3,18 +3,24 @@ import '@nomiclabs/hardhat-waffle';
 import '@nomiclabs/hardhat-ganache';
 import '@nomiclabs/hardhat-etherscan';
 import '@nomiclabs/hardhat-ethers';
-import { task } from 'hardhat/config';
+import '@requestnetwork/xdeployer';
+import { subtask, task } from 'hardhat/config';
 import { config } from 'dotenv';
 import deployAllContracts from './scripts/5_deploy-all';
 import { deployAllPaymentContracts } from './scripts/deploy-payments';
 import { preparePayments } from './scripts/prepare-payments';
-
-import { HardhatRuntimeEnvironment } from 'hardhat/types';
+import { checkCreate2Deployer } from './scripts-create2/check-deployer';
+import { deployWithCreate2FromList } from './scripts-create2/deploy-from-list';
+import deployDeployer from './scripts-create2/deploy-request-deployer';
+import { computeCreate2DeploymentAddressesFromList } from './scripts-create2/compute-from-list';
+import VerifyCreate2FromList from './scripts-create2/verify-from-list';
 
 config();
 
 const accounts = process.env.DEPLOYMENT_PRIVATE_KEY
   ? [process.env.DEPLOYMENT_PRIVATE_KEY]
+  : process.env.DEPLOYER_MASTER_KEY
+  ? [process.env.DEPLOYER_MASTER_KEY]
   : process.env.ADMIN_PRIVATE_KEY
   ? [process.env.ADMIN_PRIVATE_KEY]
   : undefined;
@@ -23,8 +29,23 @@ if (accounts && process.env.ADMIN_PRIVATE_KEY) {
   accounts.push(process.env.ADMIN_PRIVATE_KEY);
 }
 
+const DEPLOYER_KEY_GUARD = 'DEPLOYER_KEY_GUARD';
+
+// Request deployer address on local blockchain
+const LOCAL_DEPLOYER_ADDRESS = '0x8CdaF0CD259887258Bc13a92C0a6dA92698644C0';
+
+// Request deployer address on live blockchains
+const LIVE_DEPLOYER_ADDRESS = '0xE99Ab70a5FAE59551544FA326fA048f7B95A24B2';
+
+// Arbitrary data used to deploy our contracts at predefined addresses
+const REQUEST_SALT = '0x0679724da7211bc62502a39f41cbf818fc7132c266e7c819fc2b06fad9593655';
+
+const requestDeployer = process.env.REQUEST_DEPLOYER_LIVE
+  ? LIVE_DEPLOYER_ADDRESS
+  : LOCAL_DEPLOYER_ADDRESS;
+
 export default {
-  solidity: '0.8.4',
+  solidity: '0.8.9',
   paths: {
     sources: 'src/contracts',
     tests: 'test/contracts',
@@ -36,12 +57,16 @@ export default {
       accounts: undefined,
     },
     mainnet: {
-      url: process.env.WEB3_PROVIDER_URL || 'https://mainnet.infura.io/v3/YOUR_API_KEY',
+      url:
+        process.env.WEB3_PROVIDER_URL ||
+        'https://mainnet.infura.io/v3/9524d42a47cc4b8b86282744e20fc03b',
       chainId: 1,
       accounts,
     },
     rinkeby: {
-      url: process.env.WEB3_PROVIDER_URL || 'https://rinkeby.infura.io/v3/YOUR_API_KEY',
+      url:
+        process.env.WEB3_PROVIDER_URL ||
+        'https://rinkeby.infura.io/v3/9524d42a47cc4b8b86282744e20fc03b',
       chainId: 4,
       accounts,
     },
@@ -97,8 +122,18 @@ export default {
     },
   },
   etherscan: {
-    // Can be overridden according to the network (set-explorer-api-key)
-    apiKey: process.env.ETHERSCAN_API_KEY,
+    apiKey: {
+      mainnet: process.env.ETHERSCAN_API_KEY,
+      rinkeby: process.env.ETHERSCAN_API_KEY,
+      bsc: process.env.BSCSCAN_API_KEY,
+      bscTestnet: process.env.BSCSCAN_API_KEY,
+      opera: process.env.FTMSCAN_API_KEY,
+      polygon: process.env.POLYGONSCAN_API_KEY,
+      arbitrumOne: process.env.ARBISCAN_API_KEY,
+      arbitrumTestnet: process.env.ARBISCAN_API_KEY,
+      avalanche: process.env.SNOWTRACE_API_KEY,
+      xdai: 'api-key',
+    },
   },
   typechain: {
     outDir: 'src/types',
@@ -108,39 +143,20 @@ export default {
   mocha: {
     timeout: 60000, // Usefull on test networks
   },
-};
-
-// Override the default API key for non-Etherscan explorers
-const setExplorerApiKey = (hre: HardhatRuntimeEnvironment) => {
-  switch (hre.network.name) {
-    case 'bsc':
-    case 'bsctestnet': {
-      hre.config.etherscan.apiKey = process.env.BSCSCAN_API_KEY;
-      return;
-    }
-    case 'matic':
-    case 'mumbai': {
-      hre.config.etherscan.apiKey = process.env.POLYGONSCAN_API_KEY;
-      return;
-    }
-    case 'fantom': {
-      hre.config.etherscan.apiKey = process.env.FTMSCAN_API_KEY;
-      return;
-    }
-    case 'arbitrum-one': {
-      hre.config.etherscan.apiKey = process.env.ARBISCAN_API_KEY;
-      return;
-    }
-    case 'avalanche': {
-      hre.config.etherscan.apiKey = process.env.SNOWTRACE_API_KEY;
-      return;
-    }
-  }
+  xdeploy: {
+    salt: REQUEST_SALT,
+    signer: process.env.ADMIN_PRIVATE_KEY,
+    networks: ['mainnet'],
+    rpcUrls: ['https://mainnet.infura.io/v3/9524d42a47cc4b8b86282744e20fc03b'],
+    gasLimit: undefined,
+    deployerAddress: requestDeployer,
+  },
 };
 
 // FIXME: use deployAllPaymentContracts instead to test with the same deployments
 task('deploy-local-env', 'Deploy a local environment').setAction(async (args, hre) => {
   args.force = true;
+  await hre.run(DEPLOYER_KEY_GUARD);
   await deployAllContracts(args, hre);
   console.log('All contracts (re)deployed locally');
 });
@@ -155,7 +171,7 @@ task(
     args.force = args.force ?? false;
     args.dryRun = args.dryRun ?? false;
     args.simulate = args.dryRun;
-    setExplorerApiKey(hre);
+    await hre.run(DEPLOYER_KEY_GUARD);
     await deployAllPaymentContracts(args, hre);
   });
 
@@ -163,5 +179,42 @@ task(
   'prepare-live-payments',
   'Run ERC20 approval transactions for Swap Conversion, with the second signer (FIXME with missing tasks).',
 ).setAction(async (_args, hre) => {
+  await hre.run(DEPLOYER_KEY_GUARD);
   await preparePayments(hre);
+});
+
+// Tasks inherent to the CREATE2 deployment scheme
+task(
+  'deploy-deployer-contract',
+  'Deploy request deployer contract on the specified network',
+).setAction(async (_args, hre) => {
+  await deployDeployer(hre);
+});
+
+task(
+  'compute-contract-addresses',
+  'Compute the contract addresses from the Create2DeploymentList using the create2 scheme',
+).setAction(async (_args, hre) => {
+  await computeCreate2DeploymentAddressesFromList(hre);
+});
+
+task(
+  'deploy-contract-through-deployer',
+  'Deploy the contracts from the Create2DeploymentList using the create2 scheme',
+).setAction(async (_args, hre) => {
+  await checkCreate2Deployer(hre);
+  await deployWithCreate2FromList(hre);
+});
+
+task(
+  'verify-contract-from-deployer',
+  'Verify the contracts from the Create2DeploymentList for a specific network',
+).setAction(async (_args, hre) => {
+  await VerifyCreate2FromList(hre);
+});
+
+subtask(DEPLOYER_KEY_GUARD, 'prevent usage of the deployer master key').setAction(async () => {
+  if (accounts && accounts[0] === process.env.DEPLOYER_MASTER_KEY) {
+    throw new Error('The deployer master key should not be used for this action');
+  }
 });
