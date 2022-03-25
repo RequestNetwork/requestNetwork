b5cabb4645dd70145682696799ceb24b2c2d1ae0
yomarion <yoann.marion@gmail.com>
2022-03-25 14:39:11 +0100

    chore: deployment of ERC20ConversionProxy 0.1.2 on BSC

--- packages/smart-contracts/scripts-create2/constructor-args.ts
+++ packages/smart-contracts/scripts-create2/constructor-args.ts
@@ -1,13 +1,13 @@
 export const getConstructorArgs = (contract: string) => {
   switch (contract) {
     case 'Erc20ConversionProxy': {
-      if (!process.env.ADMIN_PRIVATE_KEY) {
-        throw new Error(`ADMIN_PRIVATE_KEY missing to get constructor args for: ${contract}`);
+      if (!process.env.ADMIN_WALLET_ADDRESS) {
+        throw new Error(`ADMIN_WALLET_ADDRESS missing to get constructor args for: ${contract}`);
       }
       return [
         '0x0000000000000000000000000000000000000000',
         '0x0000000000000000000000000000000000000000',
-        process.env.ADMIN_PRIVATE_KEY,
+        process.env.ADMIN_WALLET_ADDRESS,
       ];
     }
     default:
--- packages/smart-contracts/scripts-create2/deploy-one.ts
+++ packages/smart-contracts/scripts-create2/deploy-one.ts
@@ -9,41 +9,39 @@ export const deployOneWithCreate2 = async (
   deploymentParams: IDeploymentParams,
   hre: HardhatRuntimeEnvironmentExtended,
 ): Promise<string> => {
-  try {
-    if (!hre.config.xdeploy.networks || hre.config.xdeploy.networks.length === 0) {
-      throw new Error('Invalid networks');
-    }
-    // Deploy the contract on several network through xdeployer
-    const deploymentResult = await xdeploy(deploymentParams, hre);
-    for (let i = 0; i < hre.config.xdeploy.networks.length; i++) {
-      if (deploymentResult[i].deployed) {
-        console.log(`${deploymentParams.contract} succesffuly deployed:`);
+  if (!hre.config.xdeploy.networks || hre.config.xdeploy.networks.length === 0) {
+    throw new Error('Invalid networks');
+  }
+  // Deploy the contract on several network through xdeployer
+  const deploymentResult = await xdeploy(deploymentParams, hre);
+  for (let i = 0; i < hre.config.xdeploy.networks.length; i++) {
+    if (deploymentResult[i].deployed) {
+      console.log(`${deploymentParams.contract} succesffuly deployed:`);
+      console.log(`         On network:        ${hre.config.xdeploy.networks[i]}`);
+      console.log(`         At address:        ${deploymentResult[i].address}`);
+      console.log(`         At block:          ${deploymentResult[i].receipt.blockNumber}`);
+    } else {
+      if (
+        isContractDeployed(
+          deploymentParams.contract,
+          hre.config.xdeploy.networks[i],
+          deploymentResult[i].address,
+        )
+      ) {
+        console.log(`${deploymentParams.contract} already deployed:`);
         console.log(`         On network:        ${hre.config.xdeploy.networks[i]}`);
         console.log(`         At address:        ${deploymentResult[i].address}`);
-        console.log(`         At block:          ${deploymentResult[i].receipt.blockNumber}`);
       } else {
-        if (
-          isContractDeployed(
-            deploymentParams.contract,
-            hre.config.xdeploy.networks[i],
-            deploymentResult[i].address,
-          )
-        ) {
-          console.log(`${deploymentParams.contract} already deployed:`);
-          console.log(`         On network:        ${hre.config.xdeploy.networks[i]}`);
-          console.log(`         At address:        ${deploymentResult[i].address}`);
-        } else {
-          console.log(`${deploymentParams.contract} has not been deployed:`);
-          console.log(`         On network:        ${hre.config.xdeploy.networks[i]}`);
-          console.log(`         Error:             ${deploymentResult[i].error?.message}`);
-          console.log(`         Hint:              Check that your artefacts are up to date`);
-        }
+        console.log(`${deploymentParams.contract} has not been deployed:`);
+        console.log(`         On network:        ${hre.config.xdeploy.networks[i]}`);
+        console.log(`         Error:             ${deploymentResult[i].error}`);
+        console.log(
+          `         Hint:              Check admin wallet balance and that your artifacts are up to date`,
+        );
       }
     }
-    return deploymentResult[0].address;
-  } catch (e) {
-    throw new Error(e.toString());
   }
+  return deploymentResult[0].address;
 };
 
 export const deployWithCreate2FromList = async (
