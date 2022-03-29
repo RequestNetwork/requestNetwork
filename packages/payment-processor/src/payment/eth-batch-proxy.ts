2d5d0d34d957ddc80ca04b078119157029f3bea1
olivier7delf <55892112+olivier7delf@users.noreply.github.com>
2022-03-29 10:37:31 +0200

    batch fee in table

--- packages/payment-processor/src/payment/eth-batch-proxy.ts
+++ packages/payment-processor/src/payment/eth-batch-proxy.ts
@@ -16,6 +16,9 @@ import {
 } from './utils';
 import { IPreparedTransaction } from './prepared-transaction';
 
+// TODO: define archi to get this info
+const BATCH_FEE = 0.01; // 0% <= BATCH_FEE < 100%
+
 /**
  * Processes a batch transaction to pay many ETH Requests with fees.
  * @param requests List of request
@@ -33,31 +36,64 @@ export async function payEthBatchProxyRequest(
 }
 
 /**
- * Encodes the call to pay a batch of requests through the ETH bacth proxy contract, can be used with a Multisig contract.
- * @param requests request to pay
+ * Get eth batch arguments
+ * @param requests
+ * @returns
  */
-export function encodePayEthBatchRequest(requests: ClientTypes.IRequestData[]): string {
+function GetEthBatchArgs(
+  requests: ClientTypes.IRequestData[],
+): [Array<string>, Array<BigNumber>, Array<string>, Array<BigNumber>, string] {
   const paymentReferences: Array<string> = [];
   const paymentAddresses: Array<string> = [];
-  const feesToPay: Array<BigNumber> = [];
-  const amountsToPay: Array<BigNumber> = [];
-
-  // const feeAddresses: Array<string> = [];
   let feeAddressUsed = constants.AddressZero;
   // TODO: define a proper strategy to choose a feeAddress
 
+  const feesToPay: Array<BigNumber> = [];
+  const amountsToPay: Array<BigNumber> = [];
+  const batchFeesToPay: Array<BigNumber> = [];
+
+  const network = requests[0].currencyInfo.network;
+
   for (let i = 0; i < requests.length; i++) {
     validateEthFeeProxyRequest(requests[i]);
+    if (network !== requests[i].currencyInfo.network) {
+      throw new Error(`Every payment network currency must be identical`);
+    }
+
     const { paymentReference, paymentAddress, feeAddress, feeAmount } = getRequestPaymentValues(
       requests[i],
     );
 
     paymentReferences.push(`0x${paymentReference}`);
     paymentAddresses.push(paymentAddress);
-    feeAddressUsed = feeAddress || constants.AddressZero;
     feesToPay.push(BigNumber.from(feeAmount || 0));
-    amountsToPay.push(getAmountToPay(requests[i]));
+    const amountToPay = getAmountToPay(requests[i]);
+    amountsToPay.push(amountToPay);
+    batchFeesToPay.push(
+      BigNumber.from(amountToPay)
+        .mul(BATCH_FEE * 100)
+        .div(100),
+    );
+
+    feeAddressUsed = feeAddress || constants.AddressZero;
   }
+
+  const feesAndBatchFeesToPay = feesToPay.concat(batchFeesToPay);
+
+  return [paymentAddresses, amountsToPay, paymentReferences, feesAndBatchFeesToPay, feeAddressUsed];
+}
+/**
+ * Encodes the call to pay a batch of requests through the ETH batch proxy contract, can be used with a Multisig contract.
+ * @param requests request to pay
+ */
+export function encodePayEthBatchRequest(requests: ClientTypes.IRequestData[]): string {
+  const [
+    paymentAddresses,
+    amountsToPay,
+    paymentReferences,
+    feesAndBatchFeesToPay,
+    feeAddressUsed,
+  ] = GetEthBatchArgs(requests);
 
   const proxyContract = BatchPayments__factory.createInterface();
 
@@ -65,7 +101,7 @@ export function encodePayEthBatchRequest(requests: ClientTypes.IRequestData[]): 
     paymentAddresses,
     amountsToPay,
     paymentReferences,
-    feesToPay,
+    feesAndBatchFeesToPay,
     feeAddressUsed,
   ]);
 }
@@ -77,30 +113,31 @@ export function encodePayEthBatchRequest(requests: ClientTypes.IRequestData[]): 
 export function prepareEthBatchPaymentTransaction(
   requests: ClientTypes.IRequestData[],
 ): IPreparedTransaction {
-  const feesToPay: Array<BigNumber> = [];
-  const amountsToPay: Array<BigNumber> = [];
+  const [
+    _paymentAddresses,
+    amountsToPay,
+    _paymentReferences,
+    feesAndBatchFeesToPay,
+    _feeAddressUsed,
+  ] = GetEthBatchArgs(requests);
 
-  const network = requests[0].currencyInfo.network;
-
-  for (let i = 0; i < requests.length; i++) {
-    validateEthFeeProxyRequest(requests[i]);
-    if (network !== requests[i].currencyInfo.network) {
-      throw new Error(`Every payment network currency must be identical`);
-    }
-    const { feeAmount } = getRequestPaymentValues(requests[i]);
-    feesToPay.push(BigNumber.from(feeAmount || 0));
-    amountsToPay.push(getAmountToPay(requests[i]));
-  }
+  _paymentReferences;
+  _paymentAddresses;
+  _feeAddressUsed; // unused
 
   const encodedTx = encodePayEthBatchRequest(requests);
 
+  const network = requests[0].currencyInfo.network;
   if (!network) {
     throw new Error(`payment network must be set`);
   }
   const proxyAddress = batchPaymentsArtifact.getAddress(network);
 
   const amountToPay = amountsToPay.reduce((sum, current) => sum.add(current), BigNumber.from(0));
-  const feeToPay = feesToPay.reduce((sum, current) => sum.add(current), BigNumber.from(0));
+  const feeToPay = feesAndBatchFeesToPay.reduce(
+    (sum, current) => sum.add(current),
+    BigNumber.from(0),
+  );
 
   return {
     data: encodedTx,
