eb28a76064dd874c812ec97dd5dbb53a845af5b2
olivier7delf <55892112+olivier7delf@users.noreply.github.com>
2022-03-31 12:34:05 +0200

    batch payments and fees squashed (#805)

--- packages/smart-contracts/src/lib/artifacts/index.ts
+++ packages/smart-contracts/src/lib/artifacts/index.ts
@@ -11,6 +11,7 @@ export * from './EthereumProxy';
 export * from './EthereumFeeProxy';
 export * from './EthConversionProxy';
 export * from './ERC20EscrowToPay';
+export * from './BatchPayments';
 /**
  * Request Storage
  */
