76dff210c6c3c6fdba038843993e72dd1314a1db
Darko Kolev <kolevdarko@gmail.com>
2022-03-31 11:53:00 +0200

    added abi and 0.2.0 version of escrow on rinkeby

--- packages/smart-contracts/src/lib/artifacts/ERC20EscrowToPay/index.ts
+++ packages/smart-contracts/src/lib/artifacts/ERC20EscrowToPay/index.ts
@@ -1,6 +1,7 @@
 import { ContractArtifact } from '../../ContractArtifact';
 
 import { abi as ABI_0_1_0 } from './0.1.0.json';
+import { abi as ABI_0_2_0 } from './0.2.0.json';
 // @ts-ignore Cannot find module
 import type { ERC20EscrowToPay } from '../../../types/ERC2EscrowToPay';
 
@@ -20,6 +21,23 @@ export const erc20EscrowToPayArtifact = new ContractArtifact<ERC20EscrowToPay>(
         rinkeby: {
           address: '0x8230e703B1c4467A4543422b2cC3284133B9AB5e',
           creationBlockNumber: 9669613,
+        },
+      },
+    },
+    '0.2.0': {
+      abi: ABI_0_2_0,
+      deployment: {
+        private: {
+          address: '',
+          creationBlockNumber: 0,
+        },
+        mainnet: {
+          address: '',
+          creationBlockNumber: 0,
+        },
+        rinkeby: {
+          address: '0xff1CAe28E5a5B199CCBaae5257B118372095Aa26',
+          creationBlockNumber: 10417067,
         },
       },
     },
