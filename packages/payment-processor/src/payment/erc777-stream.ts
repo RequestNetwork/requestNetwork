f7f368b89d6c2a5744f41e9c69e1bd0d1c014c06
Bertrand Juglas <b@juglas.name>
2022-04-04 21:24:36 +0200

    feat: create ERC777 processing

---
+++ packages/payment-processor/src/payment/erc777-stream.ts
@@ -0,0 +1,42 @@
+import { ContractTransaction, Signer } from 'ethers';
+
+import { ClientTypes, ExtensionTypes } from '@requestnetwork/types';
+
+import {
+  getPaymentNetworkExtension,
+  getProvider,
+  getRequestPaymentValues,
+  // validateRequest,
+} from './utils';
+import { Framework } from '@superfluid-finance/sdk-core';
+
+/**
+ * Processes a transaction to pay an ERC777 stream Request.
+ * @param request
+ * @param signerOrProvider the Web3 provider, or signer. Defaults to window.ethereum.
+ * @param _overrides optionally, override default transaction values, like gas.
+ */
+export async function payErc777StreamRequest(
+  request: ClientTypes.IRequestData,
+  signer: Signer,
+): Promise<ContractTransaction> {
+  const id = getPaymentNetworkExtension(request)?.id;
+  if (id !== ExtensionTypes.ID.PAYMENT_NETWORK_ERC777_STREAM) {
+    throw new Error('Not a supported ERC777 payment network request');
+  }
+  const sf = await Framework.create({
+    networkName: request.currencyInfo.network,
+    provider: signer.provider ? signer.provider : getProvider(),
+  });
+  const superToken = await sf.loadSuperToken(request.currencyInfo.value);
+  console.log('superToken.address:', superToken.address);
+  const { paymentReference, paymentAddress } = getRequestPaymentValues(request);
+  const streamPayOp = sf.cfaV1.createFlow({
+    flowRate: '385802469135800',
+    receiver: paymentAddress,
+    superToken: superToken.address,
+    userData: `0x${paymentReference}`,
+  });
+  const batchCall = sf.batchCall([streamPayOp]);
+  return batchCall.exec(signer);
+}
