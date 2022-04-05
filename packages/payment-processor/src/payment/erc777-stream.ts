c7a0ad2b8c0de9f2ba73bdc520f7531ad121b980
Bertrand Juglas <b@juglas.name>
2022-04-05 18:07:05 +0200

    feat: add stream values and use flowRate

--- packages/payment-processor/src/payment/erc777-stream.ts
+++ packages/payment-processor/src/payment/erc777-stream.ts
@@ -1,11 +1,12 @@
 import { ContractTransaction, Signer } from 'ethers';
 
-import { ClientTypes, ExtensionTypes } from '@requestnetwork/types';
+import { ClientTypes, ExtensionTypes, PaymentTypes } from '@requestnetwork/types';
 
 import {
   getPaymentNetworkExtension,
   getProvider,
   getRequestPaymentValues,
+  validateRequest,
   // validateRequest,
 } from './utils';
 import { Framework } from '@superfluid-finance/sdk-core';
@@ -24,15 +25,17 @@ export async function payErc777StreamRequest(
   if (id !== ExtensionTypes.ID.PAYMENT_NETWORK_ERC777_STREAM) {
     throw new Error('Not a supported ERC777 payment network request');
   }
+  validateRequest(request, PaymentTypes.PAYMENT_NETWORK_ID.ERC777_STREAM);
   const sf = await Framework.create({
     networkName: request.currencyInfo.network,
     provider: signer.provider ? signer.provider : getProvider(),
   });
   const superToken = await sf.loadSuperToken(request.currencyInfo.value);
   console.log('superToken.address:', superToken.address);
-  const { paymentReference, paymentAddress } = getRequestPaymentValues(request);
+  // TODO: use expectedStartDate to compute offset between start of invoicing and start of payment
+  const { paymentReference, paymentAddress, expectedFlowRate } = getRequestPaymentValues(request);
   const streamPayOp = sf.cfaV1.createFlow({
-    flowRate: '385802469135800',
+    flowRate: expectedFlowRate ? expectedFlowRate : '0',
     receiver: paymentAddress,
     superToken: superToken.address,
     userData: `0x${paymentReference}`,
