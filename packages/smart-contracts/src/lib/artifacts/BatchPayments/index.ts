adbe70fc3dcc5c86b61bb12aebd05a6ddba555dc
olivier7delf <55892112+olivier7delf@users.noreply.github.com>
2022-04-14 13:17:12 +0200

    batch payment on rinkeby - deployed with the new Requestdeployer

--- packages/smart-contracts/src/lib/artifacts/BatchPayments/index.ts
+++ packages/smart-contracts/src/lib/artifacts/BatchPayments/index.ts
@@ -13,6 +13,10 @@ export const batchPaymentsArtifact = new ContractArtifact<BatchPayments>(
           address: '0x74e3FC764c2474f25369B9d021b7F92e8441A2Dc',
           creationBlockNumber: 0,
         },
+        rinkeby: {
+          address: '0x305164cC3846E6e631A77D4FD632219643A8767E',
+          creationBlockNumber: 10492237,
+        },
       },
     },
   },
