9c51703d32a4a4b8a3e3d109a086ec52d7b0a42b
Bertrand Juglas <b@juglas.name>
2022-05-03 17:04:32 +0200

    fix: add ERC777 support to getHash (#839)

--- packages/currency/src/getHash.ts
+++ packages/currency/src/getHash.ts
@@ -2,7 +2,8 @@ import { RequestLogicTypes } from '@requestnetwork/types';
 import Utils from '@requestnetwork/utils';
 
 export const getHash = (curr: RequestLogicTypes.ICurrency): string => {
-  return curr.type === RequestLogicTypes.CURRENCY.ERC20
+  return curr.type === RequestLogicTypes.CURRENCY.ERC20 ||
+    curr.type === RequestLogicTypes.CURRENCY.ERC777
     ? curr.value
     : Utils.crypto.last20bytesOfNormalizedKeccak256Hash({
         type: curr.type,
