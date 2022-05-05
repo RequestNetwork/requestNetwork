4ad98c2b0ed3e613776fc274a3b7c44b686c0499
Leo Sollier <leo.sollier@request.network>
2022-05-05 13:11:04 +0200

    feat any-to-native any-to-near extensions

---
+++ packages/advanced-logic/src/extensions/payment-network/any-to-native.ts
@@ -0,0 +1,51 @@
+import { FeeReferenceBasedPaymentNetwork } from './fee-reference-based';
+import { ExtensionTypes, RequestLogicTypes } from '@requestnetwork/types';
+import { InvalidPaymentAddressError } from './address-based';
+
+export default abstract class AnyToNativeTokenPaymentNetwork extends FeeReferenceBasedPaymentNetwork {
+  public constructor(
+    extensionId: ExtensionTypes.ID,
+    currentVersion: string,
+    supportedNetworks: string[],
+  ) {
+    super(extensionId, currentVersion, supportedNetworks, RequestLogicTypes.CURRENCY.ETH);
+  }
+
+  public createCreationAction(
+    creationParameters: ExtensionTypes.PnAnyToAnyConversion.ICreationParameters,
+  ): ExtensionTypes.IAction<ExtensionTypes.PnAnyToAnyConversion.ICreationParameters> {
+    const network = creationParameters.network;
+    if (!network) {
+      throw Error('network is required');
+    }
+    if (!this.supportedNetworks.includes(network)) {
+      throw Error(`network ${network} not supported`);
+    }
+    if (
+      creationParameters.paymentAddress &&
+      !this.isValidAddress(creationParameters.paymentAddress, network)
+    ) {
+      throw new InvalidPaymentAddressError(creationParameters.paymentAddress);
+    }
+    if (
+      creationParameters.refundAddress &&
+      !this.isValidAddress(creationParameters.refundAddress, network)
+    ) {
+      throw new InvalidPaymentAddressError(creationParameters.refundAddress, 'refundAddress');
+    }
+    if (
+      creationParameters.feeAddress &&
+      !this.isValidAddress(creationParameters.feeAddress, network)
+    ) {
+      throw new InvalidPaymentAddressError(creationParameters.feeAddress, 'feeAddress');
+    }
+    return super.createCreationAction(creationParameters);
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  protected isValidAddress(_address: string, _networkName?: string): boolean {
+    throw new Error(
+      `Default implementation of isValidAddress() does not support native tokens. Please override this method.`,
+    );
+  }
+}
---
+++ packages/advanced-logic/src/extensions/payment-network/any-to-near.ts
@@ -0,0 +1,166 @@
+import { ICurrencyManager, UnsupportedCurrencyError } from '@requestnetwork/currency';
+import { ExtensionTypes, IdentityTypes, RequestLogicTypes } from '@requestnetwork/types';
+import { UnsupportedNetworkError } from './address-based';
+import AnyToNativeTokenPaymentNetwork from './any-to-native';
+
+const CURRENT_VERSION = '0.2.0';
+const supportedNetworks = ['aurora', 'aurora-testnet'];
+
+export default class AnyToNearPaymentNetwork extends AnyToNativeTokenPaymentNetwork {
+  public constructor(
+    private currencyManager: ICurrencyManager,
+    extensionId: ExtensionTypes.ID = ExtensionTypes.ID.PAYMENT_NETWORK_ANY_TO_NATIVE_TOKEN,
+    currentVersion: string = CURRENT_VERSION,
+  ) {
+    super(extensionId, currentVersion, supportedNetworks);
+  }
+
+  /**
+   * Creates the extensionsData to add a fee address
+   *
+   * @param addFeeParameters extensions parameters to create
+   *
+   * @returns IAction the extensionsData to be stored in the request
+   */
+  public createAddFeeAction(
+    addFeeParameters: ExtensionTypes.PnFeeReferenceBased.IAddFeeParameters,
+  ): ExtensionTypes.IAction {
+    return super.createAddFeeAction(addFeeParameters);
+  }
+
+  /**
+   * Check if a near address is valid
+   *
+   * @param {string} address address to check
+   * @returns {boolean} true if address is valid
+   */
+  protected isValidAddress(address: string, networkName?: string): boolean {
+    switch (networkName) {
+      case 'aurora':
+        return this.isValidMainnetAddress(address);
+      case 'aurora-testnet':
+        return this.isValidTestnetAddress(address);
+      case undefined:
+        return this.isValidMainnetAddress(address) || this.isValidTestnetAddress(address);
+      default:
+        throw new UnsupportedNetworkError(networkName, this.supportedNetworks);
+    }
+  }
+
+  private isValidMainnetAddress(address: string): boolean {
+    return this.isValidAddressForSymbolAndNetwork(address, 'NEAR', 'aurora');
+  }
+
+  private isValidTestnetAddress(address: string): boolean {
+    return this.isValidAddressForSymbolAndNetwork(address, 'NEAR-testnet', 'aurora-testnet');
+  }
+
+  /**
+   * Applies a creation extension action
+   *
+   * @param extensionAction action to apply
+   * @param timestamp action timestamp
+   *
+   * @returns state of the extension created
+   */
+  protected applyCreation(
+    extensionAction: ExtensionTypes.IAction,
+    timestamp: number,
+  ): ExtensionTypes.IState {
+    if (!extensionAction.parameters.network || extensionAction.parameters.network.length === 0) {
+      throw Error('network is required');
+    }
+
+    const feePNCreationAction = super.applyCreation(extensionAction, timestamp);
+
+    return {
+      ...feePNCreationAction,
+      events: [
+        {
+          name: 'create',
+          parameters: {
+            feeAddress: extensionAction.parameters.feeAddress,
+            feeAmount: extensionAction.parameters.feeAmount,
+            paymentAddress: extensionAction.parameters.paymentAddress,
+            refundAddress: extensionAction.parameters.refundAddress,
+            salt: extensionAction.parameters.salt,
+            network: extensionAction.parameters.network,
+            maxRateTimespan: extensionAction.parameters.maxRateTimespan,
+          },
+          timestamp,
+        },
+      ],
+      values: {
+        ...feePNCreationAction.values,
+        network: extensionAction.parameters.network,
+        maxRateTimespan: extensionAction.parameters.maxRateTimespan,
+      },
+    };
+  }
+
+  /**
+   * Applies add payment address
+   *
+   * @param extensionState previous state of the extension
+   * @param extensionAction action to apply
+   * @param requestState request state read-only
+   * @param actionSigner identity of the signer
+   *
+   * @returns state of the extension updated
+   */
+  protected applyAddPaymentAddress(
+    extensionState: ExtensionTypes.IState,
+    extensionAction: ExtensionTypes.IAction,
+    requestState: RequestLogicTypes.IRequest,
+    actionSigner: IdentityTypes.IIdentity,
+    timestamp: number,
+  ): ExtensionTypes.IState {
+    const paymentAddress = extensionAction.parameters.paymentAddress;
+    if (!this.isValidAddress(paymentAddress, extensionState.values.network)) {
+      throw new Error(`paymentAddress '${paymentAddress}' is not a valid address`);
+    }
+    return super.applyAddPaymentAddress(
+      extensionState,
+      extensionAction,
+      requestState,
+      actionSigner,
+      timestamp,
+    );
+  }
+
+  /**
+   * Validate the extension action regarding the currency and network
+   * It must throw in case of error
+   *
+   * @param request
+   */
+  protected validate(
+    request: RequestLogicTypes.IRequest,
+    extensionAction: ExtensionTypes.IAction,
+  ): void {
+    const network =
+      extensionAction.action === ExtensionTypes.PnFeeReferenceBased.ACTION.CREATE
+        ? extensionAction.parameters.network
+        : request.extensions[this.extensionId]?.values.network;
+
+    if (!network) {
+      throw new Error(
+        `The network must be provided from the creation action or from the extension state`,
+      );
+    }
+
+    if (!supportedNetworks.includes(network)) {
+      throw new Error(`The network (${network}) is not supported for this payment network.`);
+    }
+
+    const currency = this.currencyManager.fromStorageCurrency(request.currency);
+    if (!currency) {
+      throw new UnsupportedCurrencyError(request.currency);
+    }
+    if (!this.currencyManager.supportsConversion(currency, network) && currency.id !== 'USD') {
+      throw new Error(
+        `The currency (${request.currency.value}) of the request is not supported for this payment network.`,
+      );
+    }
+  }
+}
