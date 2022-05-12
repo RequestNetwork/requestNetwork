59bfaef33b0cb7d5d97924c79dd4b5b728bf69f7
Darko Kolev <kolevdarko@gmail.com>
2022-05-12 11:15:50 +0200

    fixing more tests

--- packages/request-client.js/test/index.test.ts
+++ packages/request-client.js/test/index.test.ts
@@ -1629,7 +1629,7 @@ describe('index', () => {
           data.extensionsData[0].parameters.salt,
           data.extensionsData[0].parameters.paymentAddress,
         ),
-      ).toBe('efce79375b2db9f7');
+      ).toBe('a0ebc16890a6483f');
 
       jest.advanceTimersByTime(150);
       const dataAfterRefresh = await request.refresh();
@@ -1695,7 +1695,7 @@ describe('index', () => {
           data.extensionsData[0].parameters.salt,
           data.extensionsData[0].parameters.paymentAddress,
         ),
-      ).toBe('0xa0ebc16890a6483f');
+      ).toBe('a0ebc16890a6483f');
 
       jest.advanceTimersByTime(150);
       let dataAfterRefresh = await request.refresh();
