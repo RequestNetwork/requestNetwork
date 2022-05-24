40e8f991ca72ce9e40650d77caa220159c5d79bf
Darko Kolev <kolevdarko@gmail.com>
2022-05-24 17:41:10 +0200

    using deployed instead of hardcoded Erc20FeeProxy address in Escrow deployment

--- packages/smart-contracts/scripts/test-deploy-all.ts
+++ packages/smart-contracts/scripts/test-deploy-all.ts
@@ -11,7 +11,7 @@ export default async function deploy(_args: any, hre: HardhatRuntimeEnvironment)
   await deployRequest(_args, hre);
   const mainPaymentAddresses = await deployPayment(_args, hre);
   await deployConversion(_args, hre, mainPaymentAddresses);
-  await deployEscrow(hre);
+  await deployEscrow(hre, mainPaymentAddresses);
   await deployBatchPayment(_args, hre);
   await deploySuperFluid(hre);
 }
--- packages/smart-contracts/scripts/test-deploy-escrow-deployment.ts
+++ packages/smart-contracts/scripts/test-deploy-escrow-deployment.ts
@@ -4,15 +4,17 @@ import { HardhatRuntimeEnvironment } from 'hardhat/types';
 import { deployOne } from '../scripts/deploy-one';
 
 // Deploys, set up the contracts
-export async function deployEscrow(hre: HardhatRuntimeEnvironment): Promise<void> {
-  const erc20FeeProxyAddress = '0x75c35C980C0d37ef46DF04d31A140b65503c0eEd';
+export async function deployEscrow(
+  hre: HardhatRuntimeEnvironment,
+  mainPaymentAddresses: any,
+): Promise<void> {
   let deployer: Signer;
   try {
     [deployer] = await hre.ethers.getSigners();
     const deployerAddress = await deployer.getAddress();
     // Deploy Escrow contract
     const { address: erc20EscrowToPayAddress } = await deployOne({}, hre, 'ERC20EscrowToPay', {
-      constructorArguments: [erc20FeeProxyAddress, deployerAddress],
+      constructorArguments: [mainPaymentAddresses.ERC20FeeProxyAddress, deployerAddress],
     });
 
     // ----------------------------------
