4a301ba34be540a447a7e1ea31c03e0bba40143e
Darko Kolev <kolevdarko@gmail.com>
2022-06-21 10:38:36 +0200

    deployed escrow to bsc and matic'

--- packages/smart-contracts/src/lib/artifacts/ERC20EscrowToPay/index.ts
+++ packages/smart-contracts/src/lib/artifacts/ERC20EscrowToPay/index.ts
@@ -21,6 +21,10 @@ export const erc20EscrowToPayArtifact = new ContractArtifact<ERC20EscrowToPay>(
           address: '0xEbe28A2B7336670Ba752bfEad4a121D2c4FF2464',
           creationBlockNumber: 10461945,
         },
+        matic: {
+          address: '0xc7f471F5A8f8b33F131049b1e9A43941CbE31792',
+          creationBlockNumber: 29821569,
+        },
         celo: {
           address: '0xa015c141C02996EcE6410646DA3D07d70091c577',
           creationBlockNumber: 13299808,
@@ -40,6 +44,10 @@ export const erc20EscrowToPayArtifact = new ContractArtifact<ERC20EscrowToPay>(
         fantom: {
           address: '0xa015c141C02996EcE6410646DA3D07d70091c577',
           creationBlockNumber: 39534777,
+        },
+        bsc: {
+          address: '0xc7f471F5A8f8b33F131049b1e9A43941CbE31792',
+          creationBlockNumber: 18877277,
         },
       },
     },
