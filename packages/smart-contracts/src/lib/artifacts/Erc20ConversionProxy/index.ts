5f4044f28cf6f36dbd9515793a67c4d1e4625109
Romain <45540622+rom1trt@users.noreply.github.com>
2022-07-04 17:16:27 +0200

    Update index.ts

--- packages/smart-contracts/src/lib/artifacts/Erc20ConversionProxy/index.ts
+++ packages/smart-contracts/src/lib/artifacts/Erc20ConversionProxy/index.ts
@@ -50,7 +50,6 @@ export const erc20ConversionProxy = new ContractArtifact<Erc20ConversionProxy>(
           address: '0xf0f49873C50765239F6f9534Ba13c4fe16eD5f2E',
           creationBlockNumber: 8403930,
         },
-        // FIXME: add goerli version
         /**
          * FIXME: The contract was deployed on networks below with ABI 0.1.1
          * The ABI for payments is the same, only administration tasks change.
