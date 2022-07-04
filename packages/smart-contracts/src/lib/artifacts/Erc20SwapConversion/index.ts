691f2a76c0d1194b4b34312107e8c5b1cff68f62
Romain <45540622+rom1trt@users.noreply.github.com>
2022-07-04 17:15:47 +0200

    Update index.ts

--- packages/smart-contracts/src/lib/artifacts/Erc20SwapConversion/index.ts
+++ packages/smart-contracts/src/lib/artifacts/Erc20SwapConversion/index.ts
@@ -18,7 +18,7 @@ export const erc20SwapConversionArtifact = new ContractArtifact<ERC20SwapToConve
           address: '0x1d6B06C6f7adFd9314BD4C58a6D306261113a1D4',
           creationBlockNumber: 9447192,
         },
-        // FIXME: add goerli version
+        // FIXME: add goerli 
         bsc: {
           address: '0x1d6B06C6f7adFd9314BD4C58a6D306261113a1D4',
           creationBlockNumber: 16165023,
@@ -64,7 +64,6 @@ export const erc20SwapConversionArtifact = new ContractArtifact<ERC20SwapToConve
           address: '0x3b4837C9F4A606b71e61FD56Db6241781194df92',
           creationBlockNumber: 0,
         },
-        // FIXME: add goerli version
         goerli: {
           address: '0x3b4837C9F4A606b71e61FD56Db6241781194df92',
           creationBlockNumber: 7091388,
