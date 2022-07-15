bbe2a4a96344d9f15f8bfebd3e148235dd9ac784
Romain TREFAULT <romain.trefault@epita.fr>
2022-07-15 14:00:10 +0200

    refactor: move goerli to the newest versions

--- packages/smart-contracts/src/lib/artifacts/ERC20SwapToPay/index.ts
+++ packages/smart-contracts/src/lib/artifacts/ERC20SwapToPay/index.ts
@@ -21,10 +21,6 @@ export const erc20SwapToPayArtifact = new ContractArtifact<ERC20SwapToPay>(
           address: '0xb674e3d228e631594D8fd4BF947E1811288bf836',
           creationBlockNumber: 7363204,
         },
-        goerli: {
-          address: '0x0Ef49176A87Adcc88bD5125126C6a6c23a28303C',
-          creationBlockNumber: 7109102,
-        },
       },
     },
     '0.3.0': {
@@ -41,6 +37,10 @@ export const erc20SwapToPayArtifact = new ContractArtifact<ERC20SwapToPay>(
         rinkeby: {
           address: '0x1B5077Ca852d39CDDeDaF45FAF1235841854420b',
           creationBlockNumber: 7408086,
+        },
+        goerli: {
+          address: '0x0Ef49176A87Adcc88bD5125126C6a6c23a28303C',
+          creationBlockNumber: 7109102,
         },
         bsc: {
           address: '0x75740D9b5cA3BCCb356CA7f0D0dB71aBE427a835',
