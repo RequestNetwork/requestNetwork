c621ebe7d3fc2e471cb384437646b9041c55803b
Romain TREFAULT <romain.trefault@epita.fr>
2022-07-25 15:40:39 +0200

    fix: typo

--- packages/smart-contracts/scripts-create2/contract-setup/setups.ts
+++ packages/smart-contracts/scripts-create2/contract-setup/setups.ts
@@ -47,7 +47,7 @@ const setupBatchPayments = async (
       ]);
     }),
   );
-  console.log('Setup for setupBatchPayment successfull');
+  console.log('Setup for setupBatchPayment successful');
 };
 
 /**
@@ -124,7 +124,12 @@ const setupEthConversionProxy = async (
       // start from the adminNonce, increase gasPrice if needed
       await Promise.all([
         updatePaymentErc20FeeProxy(EthConversionProxyConnected, network, adminNonce, gasPrice),
-        updateChainlinkConversionPath(EthConversionProxyConnected, network, adminNonce+1, gasPrice),
+        updateChainlinkConversionPath(
+          EthConversionProxyConnected,
+          network,
+          adminNonce + 1,
+          gasPrice,
+        ),
       ]);
     }),
   );
