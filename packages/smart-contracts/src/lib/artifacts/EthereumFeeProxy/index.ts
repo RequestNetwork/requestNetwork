c70c173249769a7077d43361eb65ceb8f3f086e5
Darko Kolev <kolevdarko@gmail.com>
2022-07-28 10:05:11 +0200

    refactor: contract setup compression fix (#888)

--- packages/smart-contracts/src/lib/artifacts/EthereumFeeProxy/index.ts
+++ packages/smart-contracts/src/lib/artifacts/EthereumFeeProxy/index.ts
@@ -17,6 +17,10 @@ export const ethereumFeeProxyArtifact = new ContractArtifact<EthereumFeeProxy>(
           address: '0xC6E23a20C0a1933ACC8E30247B5D1e2215796C1F',
           creationBlockNumber: 9447193,
         },
+        goerli: {
+          address: '0xe11BF2fDA23bF0A98365e1A4c04A87C9339e8687',
+          creationBlockNumber: 7091386,
+        },
         fantom: {
           address: '0xC6E23a20C0a1933ACC8E30247B5D1e2215796C1F',
           creationBlockNumber: 20066431,
