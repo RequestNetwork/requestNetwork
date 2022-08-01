1ede263c1586a2e5a4fb2d1d4e869cdb450bbd77
Romain TREFAULT <romtref@gmail.com>
2022-08-01 09:36:33 +0200

    refactor: remove some goerli tests

--- packages/payment-detection/test/erc20/thegraph-info-retriever.test.ts
+++ packages/payment-detection/test/erc20/thegraph-info-retriever.test.ts
@@ -51,7 +51,7 @@ describe('api/erc20/thegraph-info-retriever', () => {
       expect(transferEvents[0].parameters?.feeAmount).toEqual(paymentData.feeAmount);
     });
 
-    it('should get payment event from ethFeeConversionProxy via subgraph (Rinkeby)', async () => {
+    it('should get payment event from ethFeeConversionProxy via subgraph', async () => {
       const paymentData = {
         reference: '0x6c93723bc5f82e6fbb2ea994bf0fb572fa19f7a2a3146065e21752b95668efe5',
         txHash: '0x2f7b4752aa259166c038cd9073056c5979760cf0eea55d093fca2095c229313b',
@@ -83,92 +83,6 @@ describe('api/erc20/thegraph-info-retriever', () => {
       const allNetworkEvents = await graphRetriever.getTransferEvents();
       const transferEvents = allNetworkEvents.paymentEvents;
       expect(transferEvents).toHaveLength(1);
-      expect(transferEvents[0].amount).toEqual(paymentData.amount);
-      expect(transferEvents[0].parameters?.to).toEqual(paymentData.to);
-      expect(transferEvents[0].parameters?.txHash).toEqual(paymentData.txHash);
-      expect(transferEvents[0].parameters?.block).toEqual(paymentData.block);
-    });
-  });
-
-  describe('on goerli', () => {
-    const GOERLI_ETH_FEE_PROXY_CONTRACT = '0xe11BF2fDA23bF0A98365e1A4c04A87C9339e8687';
-    const GOERLI_ETH_CONVERSION_PROXY_CONTRACT = '0xED250D9219EB93098Bb67aEbc992963172B9c8DA';
-
-    it('should get payment event from ethFeeProxy via subgraph', async () => {
-      const paymentData = {
-        reference: '0x6c93723bc5f82e6fbb2ea994bf0fb572fa19f7a2a3146065e21752b95668efe5',
-        txHash: '0x29a55b7f978a902275746f6eb930abf608dc77c0592fce9cf6c326128fcf7493',
-        from: '0xBc9643B7Bd7c8621F4a68280433796484b9E525d',
-        to: '0x5000ee9fb9c96a2a09d8efb695ac21d6c429ff11',
-        network: 'goerli',
-        salt: '0ee84db293a752c6',
-        amount: '600000000000000',
-        requestId: '0188791633ff0ec72a7dbdefb886d2db6cccfa98287320839c2f173c7a4e3ce7e1',
-        block: 7230236,
-        feeAddress: '0x5000EE9FB9c96A2A09D8efB695aC21D6C429fF11',
-        feeAmount: '0',
-      };
-      const paymentReference = PaymentReferenceCalculator.calculate(
-        paymentData.requestId,
-        paymentData.salt,
-        paymentData.to,
-      );
-      const onChainReference = utils.keccak256(`0x${paymentReference}`);
-      expect(onChainReference).toEqual(paymentData.reference);
-
-      const graphRetriever = new TheGraphInfoRetriever(
-        paymentReference,
-        GOERLI_ETH_FEE_PROXY_CONTRACT,
-        null,
-        paymentData.to,
-        PaymentTypes.EVENTS_NAMES.PAYMENT,
-        paymentData.network,
-      );
-      const allNetworkEvents = await graphRetriever.getTransferEvents();
-      const transferEvents = allNetworkEvents.paymentEvents;
-      // expect(transferEvents).toHaveLength(1);
-      expect(transferEvents[0].amount).toEqual('600000000000000');
-      expect(transferEvents[0].name).toEqual('payment');
-      expect(transferEvents[0].parameters?.to).toEqual(paymentData.to);
-      expect(transferEvents[0].parameters?.txHash).toEqual(paymentData.txHash);
-      expect(transferEvents[0].parameters?.block).toEqual(paymentData.block);
-      expect(transferEvents[0].parameters?.feeAddress).toEqual(paymentData.feeAddress);
-      expect(transferEvents[0].parameters?.feeAmount).toEqual(paymentData.feeAmount);
-    });
-
-    // doesn't work because chainlink doesn't have oracles on goerli
-    it.skip('should get payment event from ethFeeConversionProxy via subgraph', async () => {
-      const paymentData = {
-        reference: '0x6c93723bc5f82e6fbb2ea994bf0fb572fa19f7a2a3146065e21752b95668efe5',
-        txHash: '0x2f7b4752aa259166c038cd9073056c5979760cf0eea55d093fca2095c229313b',
-        from: '0x186e7fe6c34ea0eca7f9c2fd29651fc0443e3f29',
-        to: '0x5000ee9fb9c96a2a09d8efb695ac21d6c429ff11',
-        network: 'goerli',
-        salt: '0ee84db293a752c6',
-        amount: '7000',
-        block: 9610470,
-        requestId: '0188791633ff0ec72a7dbdefb886d2db6cccfa98287320839c2f173c7a4e3ce7e1',
-      };
-
-      const shortReference = PaymentReferenceCalculator.calculate(
-        paymentData.requestId,
-        paymentData.salt,
-        paymentData.to,
-      );
-      const onChainReference = utils.keccak256(`0x${shortReference}`);
-      expect(onChainReference).toEqual(paymentData.reference);
-
-      const graphRetriever = new TheGraphInfoRetriever(
-        shortReference,
-        GOERLI_ETH_CONVERSION_PROXY_CONTRACT,
-        null,
-        paymentData.to,
-        PaymentTypes.EVENTS_NAMES.PAYMENT,
-        paymentData.network,
-      );
-      const allNetworkEvents = await graphRetriever.getTransferEvents();
-      const transferEvents = allNetworkEvents.paymentEvents;
-      // expect(transferEvents).toHaveLength(1);
       expect(transferEvents[0].amount).toEqual(paymentData.amount);
       expect(transferEvents[0].parameters?.to).toEqual(paymentData.to);
       expect(transferEvents[0].parameters?.txHash).toEqual(paymentData.txHash);
