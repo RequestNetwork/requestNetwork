98f320ef18bd7cf77f0b7471cb55bfe73473c726
Bertrand Juglas <b@juglas.name>
2022-08-05 18:34:00 +0200

    feat: add prepare and encode functions for SF

--- packages/payment-processor/src/payment/erc777-stream.ts
+++ packages/payment-processor/src/payment/erc777-stream.ts
@@ -1,4 +1,4 @@
-import { ContractTransaction, Signer, Overrides } from 'ethers';
+import { ContractTransaction, Signer, Overrides, providers } from 'ethers';
 
 import { ClientTypes, ExtensionTypes, PaymentTypes } from '@requestnetwork/types';
 
@@ -9,6 +9,7 @@ import {
   validateRequest,
 } from './utils';
 import { Framework } from '@superfluid-finance/sdk-core';
+import { IPreparedTransaction } from './prepared-transaction';
 
 export const RESOLVER_ADDRESS = '0x913bbCFea2f347a24cfCA441d483E7CBAc8De3Db';
 // Superfluid payments of requests use the generic field `userData` to index payments.
@@ -18,7 +19,7 @@ export const USERDATA_PREFIX = '0xbeefac';
 
 /**
  * Processes a transaction to pay an ERC777 stream Request.
- * @param request
+ * @param request the request to pay
  * @param signer the Web3 signer. Defaults to window.ethereum.
  * @param overrides optionally, override default transaction values, like gas.
  */
@@ -27,25 +28,16 @@ export async function payErc777StreamRequest(
   signer: Signer,
   overrides?: Overrides,
 ): Promise<ContractTransaction> {
-  const id = getPaymentNetworkExtension(request)?.id;
-  if (id !== ExtensionTypes.ID.PAYMENT_NETWORK_ERC777_STREAM) {
-    throw new Error('Not a supported ERC777 payment network request');
-  }
-  validateRequest(request, PaymentTypes.PAYMENT_NETWORK_ID.ERC777_STREAM);
-  const sf = await getSuperFluidFramework(request, signer);
-  // FIXME: according to specs PR https://github.com/RequestNetwork/requestNetwork/pull/688
-  // in file packages/advanced-logic/specs/payment-network-erc777-stream-0.1.0.md
-  // Below are the SF actions to add in the BatchCall:
-  // - use expectedStartDate to compute offset between start of invoicing and start of streaming
-  // - start fee streaming
-  const streamPayOp = await getStartStreamOp(sf, request, overrides);
-  const batchCall = sf.batchCall([streamPayOp]);
-  return batchCall.exec(signer);
+  const { data, to, value } = await prepareErc777StreamPaymentTransaction(
+    request,
+    signer.provider ?? getProvider(),
+  );
+  return signer.sendTransaction({ data, to, value, ...overrides });
 }
 
 /**
  * Processes a transaction to complete an ERC777 stream paying a Request.
- * @param request
+ * @param request the request to pay
  * @param signer the Web3 signer. Defaults to window.ethereum.
  * @param overrides optionally, override default transaction values, like gas.
  */
@@ -59,7 +51,7 @@ export async function completeErc777StreamRequest(
     throw new Error('Not a supported ERC777 payment network request');
   }
   validateRequest(request, PaymentTypes.PAYMENT_NETWORK_ID.ERC777_STREAM);
-  const sf = await getSuperFluidFramework(request, signer);
+  const sf = await getSuperFluidFramework(request, signer.provider ?? getProvider());
   // FIXME: according to specs PR https://github.com/RequestNetwork/requestNetwork/pull/688
   // in file packages/advanced-logic/specs/payment-network-erc777-stream-0.1.0.md
   // Below are the SF actions to add in the BatchCall :
@@ -70,22 +62,32 @@ export async function completeErc777StreamRequest(
   return batchCall.exec(signer);
 }
 
-async function getSuperFluidFramework(request: ClientTypes.IRequestData, signer: Signer) {
+/**
+ * Encodes the call to pay a request through the ERC20 fee proxy contract, can be used with a Multisig contract.
+ * @param request the request to pay
+ * @param provider the Web3 provider. Defaults to window.ethereum.
+ */
+async function getSuperFluidFramework(
+  request: ClientTypes.IRequestData,
+  provider: providers.Provider,
+) {
   const isNetworkPrivate = request.currencyInfo.network === 'private';
   const networkName = isNetworkPrivate ? 'custom' : request.currencyInfo.network;
   return await Framework.create({
     networkName,
-    provider: signer.provider ?? getProvider(),
+    provider: provider,
     dataMode: isNetworkPrivate ? 'WEB3_ONLY' : undefined,
     resolverAddress: isNetworkPrivate ? RESOLVER_ADDRESS : undefined,
     protocolReleaseVersion: isNetworkPrivate ? 'test' : undefined,
   });
 }
-async function getStartStreamOp(
-  sf: Framework,
-  request: ClientTypes.IRequestData,
-  overrides?: Overrides,
-) {
+
+/**
+ * Get from SuperFluid framework the operation to start paying a request.
+ * @param sf the SuperFluid framework to use
+ * @param request the request to pay
+ */
+async function getStartStreamOp(sf: Framework, request: ClientTypes.IRequestData) {
   const superToken = await sf.loadSuperToken(request.currencyInfo.value);
   const { paymentReference, paymentAddress, expectedFlowRate } = getRequestPaymentValues(request);
   return sf.cfaV1.createFlow({
@@ -93,10 +95,16 @@ async function getStartStreamOp(
     receiver: paymentAddress,
     superToken: superToken.address,
     userData: `${USERDATA_PREFIX}${paymentReference}`,
-    overrides: overrides,
   });
 }
 
+/**
+ * Get from SuperFluid framework the operation to stop paying a request.
+ * @param sf the SuperFluid framework to use
+ * @param signer the Web3 signer. Defaults to window.ethereum.
+ * @param request the request to pay
+ * @param overrides optionally, override default transaction values, like gas.
+ */
 async function getStopStreamOp(
   sf: Framework,
   signer: Signer,
@@ -112,4 +120,47 @@ async function getStopStreamOp(
     userData: `${USERDATA_PREFIX}${paymentReference}`,
     overrides: overrides,
   });
+}
+
+/**
+ * Encodes the call to pay a request through the ERC777 SuperFluid stream contract.
+ * @param request the request to pay
+ * @param sf the SuperFluid framework to use
+ */
+export async function encodePayErc777StreamRequest(
+  request: ClientTypes.IRequestData,
+  sf: Framework,
+): Promise<string> {
+  // FIXME: according to specs PR https://github.com/RequestNetwork/requestNetwork/pull/688
+  // in file packages/advanced-logic/specs/payment-network-erc777-stream-0.1.0.md
+  // Below are the SF actions to add in the BatchCall:
+  // - use expectedStartDate to compute offset between start of invoicing and start of streaming
+  // - start fee streaming
+  const streamPayOp = await getStartStreamOp(sf, request);
+  const batchCall = sf.batchCall([streamPayOp]);
+
+  const operationStructArray = await Promise.all(batchCall.getOperationStructArrayPromises);
+  return batchCall.host.hostContract.interface.encodeFunctionData('batchCall', [
+    operationStructArray,
+  ]);
+}
+/**
+ * Prepare the transaction to pay a request through the ERC777 SuperFluid stream contract.
+ * @param request the request to pay
+ * @param provider the Web3 provider. Defaults to window.ethereum.
+ */
+export async function prepareErc777StreamPaymentTransaction(
+  request: ClientTypes.IRequestData,
+  provider: providers.Provider,
+): Promise<IPreparedTransaction> {
+  validateRequest(request, PaymentTypes.PAYMENT_NETWORK_ID.ERC777_STREAM);
+  const sf = await getSuperFluidFramework(request, provider);
+
+  const encodedTx = await encodePayErc777StreamRequest(request, sf);
+
+  return {
+    data: encodedTx,
+    to: sf.host.hostContract.address,
+    value: 0,
+  };
 }
