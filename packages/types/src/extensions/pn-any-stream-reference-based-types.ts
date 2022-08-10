a45733b9084da65b34035462dbda83e60f58d036
Leo Sollier <leo.sollier@request.network>
2022-08-10 18:11:36 +0200

    update types

--- packages/types/src/extensions/pn-any-stream-reference-based-types.ts
+++ packages/types/src/extensions/pn-any-stream-reference-based-types.ts
@@ -6,7 +6,10 @@ export {
 } from './pn-any-reference-based-types';
 
 /** Parameters for the creation action */
-export interface ICreationParameters extends PnReferenceBased.ICreationParameters {
-  expectedFlowRate: string;
-  expectedStartDate: string;
+export interface ICreationParameters extends Partial<PnReferenceBased.ICreationParameters> {
+  expectedFlowRate?: string;
+  expectedStartDate?: string;
+  previousRequestId?: string;
+  masterRequestId?: string;
+  recurrenceNumber?: string;
 }
