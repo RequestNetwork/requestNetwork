51e07cd9ce8e436ba00df272f469df3883d431b1
Leo Sollier <leo.sollier@request.network>
2022-08-12 17:15:21 +0200

    update types and add type guards

--- packages/types/src/extensions/pn-any-stream-reference-based-types.ts
+++ packages/types/src/extensions/pn-any-stream-reference-based-types.ts
@@ -6,10 +6,23 @@ export {
 } from './pn-any-reference-based-types';
 
 /** Parameters for the creation action */
-export interface ICreationParameters extends Partial<PnReferenceBased.ICreationParameters> {
-  expectedFlowRate?: string;
-  expectedStartDate?: string;
-  previousRequestId?: string;
-  masterRequestId?: string;
-  recurrenceNumber?: number;
+export type ICreationParameters =
+  | IMasterRequestCreationParameters
+  | ISubsequentRequestCreationParameters;
+
+/** Parameters for the creation action of the first request of a series */
+export interface IMasterRequestCreationParameters extends PnReferenceBased.ICreationParameters {
+  expectedFlowRate: string;
+  expectedStartDate: string;
+}
+
+/** Parameters for the creation action of other requests of a series */
+export interface ISubsequentRequestCreationParameters
+  extends Partial<PnReferenceBased.ICreationParameters> {
+  /* requestId of the previous request in the series */
+  previousRequestId: string;
+  /* requestId of the first request of the series */
+  masterRequestId: string;
+  /* rank of the request in the series */
+  recurrenceNumber: number;
 }
