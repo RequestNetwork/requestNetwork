8a2faa24f2398198c53780029a9afeeb8a4cb702
Darko Kolev <kolevdarko@gmail.com>
2022-08-16 15:12:05 +0200

    updated stream.ts from master

--- packages/advanced-logic/src/extensions/payment-network/erc777/stream.ts
+++ packages/advanced-logic/src/extensions/payment-network/erc777/stream.ts
@@ -7,7 +7,7 @@ const CURRENT_VERSION = '0.1.0';
  * Implementation of the payment network to pay in ERC777, including third-party fees payment, based on a reference provided to a proxy contract.
  */
 export default class Erc777StreamPaymentNetwork<
-  TCreationParameters extends ExtensionTypes.PnStreamReferenceBased.ICreationParameters = ExtensionTypes.PnStreamReferenceBased.ICreationParameters,
+  TCreationParameters extends ExtensionTypes.PnStreamReferenceBased.ICreationParameters = ExtensionTypes.PnStreamReferenceBased.ICreationParameters
 > extends ReferenceBasedPaymentNetwork<TCreationParameters> {
   public constructor(
     extensionId: ExtensionTypes.ID = ExtensionTypes.ID.PAYMENT_NETWORK_ERC777_STREAM,
