c8c0574ab7d5940eabfadfcf6c5d7b747ca25853
yomarion <yoann.marion@gmail.com>
2022-11-03 07:59:26 +0100

    fix: isPaymentNetworkId

--- packages/types/src/utils.ts
+++ packages/types/src/utils.ts
@@ -5,13 +5,21 @@ import {
   ISubsequentRequestCreationParameters,
 } from './extensions/pn-any-stream-reference-based-types';
 
+/*
+ * Helper to properly type keys of an enum with string values.
+ * See: https://www.petermorlion.com/iterating-a-typescript-enum/
+ */
+function enumKeys<O extends Record<string, unknown>, K extends keyof O = keyof O>(obj: O): K[] {
+  return Object.keys(obj).filter((k) => Number.isNaN(+k)) as K[];
+}
+
 /**
  * Types a value like ExtensionType into a paymentNetworkID enum element if possible
  * @param value Example: ExtensionTypes.PAYMENT_NETWORK_ID.ERC20_PROXY_CONTRACT
  */
 export function isPaymentNetworkId(value: any): value is ExtensionTypes.PAYMENT_NETWORK_ID {
-  for (const pn in ExtensionTypes.PAYMENT_NETWORK_ID) {
-    if (pn === value) {
+  for (const pn of enumKeys(ExtensionTypes.PAYMENT_NETWORK_ID)) {
+    if (ExtensionTypes.PAYMENT_NETWORK_ID[pn] === value) {
       return true;
     }
   }
