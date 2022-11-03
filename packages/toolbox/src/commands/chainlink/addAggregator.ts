abc53c7d679fa3c4101110bb8a5930bc74e3ce45
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2022-11-03 16:18:26 +0100

    feat(toolbox): add aggregators by name (#978)

--- packages/toolbox/src/commands/chainlink/addAggregator.ts
+++ packages/toolbox/src/commands/chainlink/addAggregator.ts
@@ -1,5 +1,7 @@
 import * as yargs from 'yargs';
 import { runUpdate } from './contractUtils';
+import { getAllAggregators, getCurrencyManager } from './aggregatorsUtils';
+import assert from 'assert';
 
 type Options = {
   dryRun: boolean;
@@ -8,7 +10,8 @@ type Options = {
   mnemonic?: string;
   input: string;
   output: string;
-  aggregator: string;
+  aggregator?: string;
+  list?: string;
 };
 
 export const command = 'addAggregator <network>';
@@ -26,14 +29,17 @@ export const builder = (): yargs.Argv<Options> =>
     input: {
       type: 'string',
       demandOption: true,
+      describe: 'The token hash, or symbol, to use as input',
     },
     output: {
       type: 'string',
       demandOption: true,
+      describe: 'The token hash, or symbol, to use as output',
     },
     aggregator: {
       type: 'string',
-      demandOption: true,
+      describe:
+        'The address of the aggregation contract, or its name on ChainLink docs. eg. "USDC / USD". If omitted, will default to "input / output"',
     },
     mnemonic: {
       type: 'string',
@@ -42,9 +48,38 @@ export const builder = (): yargs.Argv<Options> =>
       type: 'string',
       describe: 'Takes precedence over mnemonic',
     },
+    list: {
+      type: 'string',
+      describe:
+        'Required when passing symbols in input or output. The list NAME must be available at https://api.request.network/currency/list/NAME',
+    },
   });
 
 export const handler = async (args: Options): Promise<void> => {
-  const { input, output, aggregator } = args;
+  let { input, output, aggregator } = args;
+  const { network, list } = args;
+
+  const currencyManager = await getCurrencyManager(list);
+  const inputCcy = currencyManager.from(input, network) || currencyManager.from(input);
+  const outputCcy = currencyManager.from(output, network) || currencyManager.from(output);
+
+  if (!input.startsWith('0x')) {
+    assert(inputCcy, `input ${input} not found`);
+    input = inputCcy.hash;
+  }
+  if (!output.startsWith('0x')) {
+    assert(outputCcy, `output ${output} not found`);
+    output = outputCcy.hash;
+  }
+  if (!aggregator) {
+    aggregator = `${inputCcy?.symbol} / ${outputCcy?.symbol}`;
+  }
+  if (!aggregator.startsWith('0x')) {
+    const aggregators = await getAllAggregators(network);
+    const newAggregator = aggregators.find((x) => x.pair === aggregator);
+    assert(newAggregator, `aggregator ${aggregator} not found`);
+    aggregator = newAggregator.proxy;
+  }
+  assert(aggregator);
   await runUpdate('updateAggregator', [input, output, aggregator], args);
 };
