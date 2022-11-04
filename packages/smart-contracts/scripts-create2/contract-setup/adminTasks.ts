645e6b5c63de42877a0c9b404cd288fb4129623a
olivier7delf <55892112+olivier7delf@users.noreply.github.com>
2022-11-04 10:03:38 +0100

    update the batch fee commented value

--- packages/smart-contracts/scripts-create2/contract-setup/adminTasks.ts
+++ packages/smart-contracts/scripts-create2/contract-setup/adminTasks.ts
@@ -5,7 +5,7 @@ import { BigNumber } from 'ethers';
 
 // Fees: 0.5%
 export const REQUEST_SWAP_FEES = 5;
-// Batch fee: temporarily at 0%
+// Batch fee: 0.3%
 const BATCH_FEE = 30;
 // Batch fee amount in USD Limit: 150 * 1e8 ($150)
 const BATCH_FEE_AMOUNT_USD_LIMIT = 150 * 1e8;
