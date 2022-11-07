1f46588faf59d438c28b582d8fc808f725d817e1
Leo Sollier <leo.sollier@request.network>
2022-11-07 21:28:34 +0100

    fix admin tasks conditions

--- packages/smart-contracts/scripts-create2/contract-setup/adminTasks.ts
+++ packages/smart-contracts/scripts-create2/contract-setup/adminTasks.ts
@@ -37,15 +37,16 @@ export const updateSwapRouter = async (
 };
 
 export const updateRequestSwapFees = async (contract: any, gasPrice: BigNumber): Promise<void> => {
-  const currentFees = await contract.requestSwapFees();
-  if (currentFees !== REQUEST_SWAP_FEES) {
+  const currentFees: BigNumber = await contract.requestSwapFees();
+  if (!currentFees.eq(REQUEST_SWAP_FEES)) {
+    console.log(`currentFees: ${currentFees.toString()}, new fees: ${REQUEST_SWAP_FEES}`);
     await contract.updateRequestSwapFees(REQUEST_SWAP_FEES, { gasPrice: gasPrice });
   }
 };
 
 export const updateBatchPaymentFees = async (contract: any, gasPrice: BigNumber): Promise<void> => {
-  const currentFees = await contract.batchFee();
-  if (currentFees !== BATCH_FEE) {
+  const currentFees: BigNumber = await contract.batchFee();
+  if (!currentFees.eq(BATCH_FEE)) {
     // Log is useful to have a direct view on what is being updated
     console.log(`currentFees: ${currentFees.toString()}, new fees: ${BATCH_FEE}`);
     await contract.setBatchFee(BATCH_FEE, { gasPrice: gasPrice });
