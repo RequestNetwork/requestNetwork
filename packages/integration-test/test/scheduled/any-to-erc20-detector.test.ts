6ef8f17d9021aef9966e14e8d756349b0d66b6ae
yomarion <yoann.marion@gmail.com>
2023-03-24 11:56:15 +0100

    fix: test

--- packages/integration-test/test/scheduled/any-to-erc20-detector.test.ts
+++ packages/integration-test/test/scheduled/any-to-erc20-detector.test.ts
@@ -7,21 +7,23 @@ import { createMockConversionErc20Request } from '../utils';
 
 const pnFactory = new PaymentNetworkFactory(mockAdvancedLogic, CurrencyManager.getDefault());
 
+const paidEURRequest = {
+  network: 'matic',
+  requestId: '0117d7a59a48e5031b3c56c92621453149e4a4462dba6eaeb3271a995c4201448b',
+  paymentAddress: '0x4E64C2d06d19D13061e62E291b2C4e9fe5679b93',
+  salt: '5ddb1c1645ac2daf',
+  tokenAddress: '0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063', // DAI on matic
+  feeAddress: '0x35d0e078755cd84d3e0656caab417dee1d7939c7',
+  feeAmount: '0',
+  currency: {
+    type: RequestLogicTypes.CURRENCY.ISO4217,
+    value: 'EUR',
+  },
+};
+
 describe('Any to ERC20 Fee Proxy detection test-suite', () => {
   it('can getBalance on a matic request with TheGraph', async () => {
-    const mockRequest = createMockConversionErc20Request({
-      network: 'matic',
-      requestId: '0117d7a59a48e5031b3c56c92621453149e4a4462dba6eaeb3271a995c4201448b',
-      paymentAddress: '0x4E64C2d06d19D13061e62E291b2C4e9fe5679b93',
-      salt: '5ddb1c1645ac2daf',
-      tokenAddress: '0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063', // DAI on matic
-      feeAddress: '0x35d0e078755cd84d3e0656caab417dee1d7939c7',
-      feeAmount: '0',
-      currency: {
-        type: RequestLogicTypes.CURRENCY.ISO4217,
-        value: 'EUR',
-      },
-    });
+    const mockRequest = createMockConversionErc20Request(paidEURRequest);
     const paymentDetector = pnFactory.getPaymentNetworkFromRequest(mockRequest);
     expect(paymentDetector).toBeDefined();
 
@@ -31,8 +33,6 @@ describe('Any to ERC20 Fee Proxy detection test-suite', () => {
     expect(balance.events).toHaveLength(1);
     expect(balance.events[0].name).toBe('payment');
     const params = balance.events[0].parameters as PaymentTypes.IERC20FeePaymentEventParameters;
-    console.log(balance.events[0]);
-    console.log(params);
     expect(params?.to).toBe('0x4E64C2d06d19D13061e62E291b2C4e9fe5679b93');
     expect(balance.events[0].amount).toBe('10');
     expect(balance.events[0].timestamp).toBe(1679579206);
@@ -40,16 +40,9 @@ describe('Any to ERC20 Fee Proxy detection test-suite', () => {
 
   it('cannot trick getBalance with a payment denominated in the wrong currency', async () => {
     const mockRequest = createMockConversionErc20Request({
-      network: 'matic',
-      requestId: '0117d7a59a48e5031b3c56c92621453149e4a4462dba6eaeb3271a995c4201448b',
-      paymentAddress: '0x4E64C2d06d19D13061e62E291b2C4e9fe5679b93',
-      salt: '5ddb1c1645ac2daf',
-      tokenAddress: '0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063', // DAI on matic
-      feeAddress: '0x35d0e078755cd84d3e0656caab417dee1d7939c7',
-      feeAmount: '0',
+      ...paidEURRequest,
       currency: {
         type: RequestLogicTypes.CURRENCY.ISO4217,
-        // One payment matches everything else but the currency on Matic (cf. test above)
         value: 'JPY',
       },
     });
