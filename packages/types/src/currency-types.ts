5262580968874ac5d08d4d2492b274df9763bc9c
Jerry Laloan <jerrylaloan@hotmail.com>
2023-06-15 14:47:12 +0700

    Added type

--- packages/types/src/currency-types.ts
+++ packages/types/src/currency-types.ts
@@ -30,12 +30,17 @@ export type EvmChainName =
 export type BtcChainName = 'mainnet' | 'testnet';
 
 /**
+ * List of supported Tron chains
+ */
+export type TronChainName = 'tron';
+
+/**
  * List of supported NEAR chains
  * FIXME: get rid of the wrong 'aurora' alias
  */
 export type NearChainName = 'aurora' | 'aurora-testnet' | 'near' | 'near-testnet';
 
-export type ChainName = EvmChainName | BtcChainName | NearChainName;
+export type ChainName = EvmChainName | BtcChainName | NearChainName | TronChainName;
 
 /**
  * Virtual machin chains, where payment proxy contracts can be deployed
