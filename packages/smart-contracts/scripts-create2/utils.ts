5cdbbd7005145ec99aa6a4da9fe30639735a3d0f
MantisClone <david.hunt-mateo@mantisdata.solutions>
2023-07-12 20:27:07 -0400

    Deploy only ERC20TransferableReceivable

--- packages/smart-contracts/scripts-create2/utils.ts
+++ packages/smart-contracts/scripts-create2/utils.ts
@@ -8,16 +8,16 @@ import { EvmChains } from '@requestnetwork/currency';
  * If you want to skip deploying one or more, then comment them out in the list bellow.
  */
 export const create2ContractDeploymentList = [
-  /* 'ChainlinkConversionPath',
-  'EthereumProxy',
-  'EthereumFeeProxy',
-  'EthConversionProxy',
-  'ERC20FeeProxy', */
-  'ERC20SwapToPay',
-  'ERC20SwapToConversion',
-  'BatchConversionPayments',
-  /* 'ERC20EscrowToPay',
-  'ERC20TransferableReceivable', */
+  // 'ChainlinkConversionPath',
+  // 'EthereumProxy',
+  // 'EthereumFeeProxy',
+  // 'EthConversionProxy',
+  // 'ERC20FeeProxy',
+  // 'ERC20SwapToPay',
+  // 'ERC20SwapToConversion',
+  // 'BatchConversionPayments',
+  // 'ERC20EscrowToPay',
+  'ERC20TransferableReceivable',
 ];
 
 /**
