556d0194a54a5cafebacf8c15531e55802b30198
Alexandre ABRIOUX <alexandre-abrioux@users.noreply.github.com>
2023-11-07 16:03:40 +0100

    ci: save test results in CircleCI

--- packages/data-format/jest.config.js
+++ packages/data-format/jest.config.js
@@ -2,4 +2,19 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   collectCoverage: true,
+  reporters: [
+    'default',
+    [
+      'jest-junit',
+      {
+        addFileAttribute: 'true',
+        ancestorSeparator: ' > ',
+        suiteNameTemplate: '{filename}',
+        classNameTemplate: '{classname}',
+        titleTemplate: '{title}',
+        outputDirectory: 'reports',
+        outputName: 'jest-results.xml',
+      },
+    ],
+  ],
 };
--- packages/smart-contracts/jest.config.js
+++ packages/smart-contracts/jest.config.js
@@ -2,5 +2,20 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   collectCoverage: true,
+  reporters: [
+    'default',
+    [
+      'jest-junit',
+      {
+        addFileAttribute: 'true',
+        ancestorSeparator: ' > ',
+        suiteNameTemplate: '{filename}',
+        classNameTemplate: '{classname}',
+        titleTemplate: '{title}',
+        outputDirectory: 'reports',
+        outputName: 'jest-results.xml',
+      },
+    ],
+  ],
   moduleFileExtensions: ['js', 'json', 'jsx', 'ts', 'tsx', 'node', 'd.ts'],
 };
