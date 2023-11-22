38bbb2cf11ccbb7b06b014a935b126c7979700e7
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2023-11-22 13:52:30 +0100

    getTheGraphClient

--- packages/payment-detection/src/thegraph/client.ts
+++ packages/payment-detection/src/thegraph/client.ts
@@ -26,30 +26,40 @@ export type TheGraphClient<TChain extends CurrencyTypes.VMChainName = CurrencyTy
   (TChain extends CurrencyTypes.NearChainName
     ? ReturnType<typeof getNearSdk>
     : ReturnType<typeof getSdk>) & {
-    options?: {
-      /** constraint to select indexers that have at least parsed this block */
-      minIndexedBlock?: number;
-    };
+    options?: TheGraphClientOptions;
   };
 
 export type TheGraphClientOptions = {
   timeout?: number;
+  minIndexedBlock?: number;
 };
 
-export const getTheGraphClient = (url: string, options?: TheGraphClientOptions) =>
-  getSdk(new GraphQLClient(url, options));
+export const getTheGraphEvmClient = (url: string, options?: TheGraphClientOptions) => {
+  const sdk: TheGraphClient<CurrencyTypes.EvmChainName> = getSdk(new GraphQLClient(url, options));
+  sdk.options = options;
+  return sdk;
+};
 
-export const getTheGraphNearClient = (url: string, options?: TheGraphClientOptions) =>
-  getNearSdk(new GraphQLClient(url, options));
+export const getTheGraphNearClient = (url: string, options?: TheGraphClientOptions) => {
+  const sdk: TheGraphClient<CurrencyTypes.NearChainName> = getNearSdk(
+    new GraphQLClient(url, options),
+  );
+  sdk.options = options;
 
-export const defaultGetTheGraphClient = (network: CurrencyTypes.ChainName) => {
+  return sdk;
+};
+
+export const getTheGraphClient = (
+  network: CurrencyTypes.ChainName,
+  options?: TheGraphClientOptions,
+) => {
   return network === 'private'
     ? undefined
     : NearChains.isChainSupported(network)
-    ? getTheGraphNearClient(`${HOSTED_THE_GRAPH_URL}${network.replace('aurora', 'near')}`)
+    ? getTheGraphNearClient(`${HOSTED_THE_GRAPH_URL}${network.replace('aurora', 'near')}`, options)
     : network === 'mantle'
-    ? getTheGraphClient(THE_GRAPH_URL_MANTLE)
+    ? getTheGraphEvmClient(THE_GRAPH_URL_MANTLE, options)
     : network === 'mantle-testnet'
-    ? getTheGraphClient(THE_GRAPH_URL_MANTLE_TESTNET)
-    : getTheGraphClient(`${HOSTED_THE_GRAPH_URL}${network}`);
+    ? getTheGraphEvmClient(THE_GRAPH_URL_MANTLE_TESTNET, options)
+    : getTheGraphEvmClient(`${HOSTED_THE_GRAPH_URL}${network}`, options);
 };
