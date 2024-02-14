4c2070b4e854f6ef6b8ab33dc4244ef27fe2546c
Neil Duffy <skivy71@gmail.com>
2024-02-14 21:45:00 +0000

    changed to relative import

--- packages/currency/src/chains/evm/data/base.ts
+++ packages/currency/src/chains/evm/data/base.ts
@@ -1,9 +1,7 @@
 import { TokenMap } from '../../../types';
-
-import { supportedBaseERC20 } from 'currency/src/erc20/chains/base';
+import { supportedBaseERC20 } from '../../../erc20/chains/base';
 
 export const chainId = 8453;
-
 export const currencies: TokenMap = {
   ...supportedBaseERC20,
 };
