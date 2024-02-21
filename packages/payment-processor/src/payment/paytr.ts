8df51c0e8688a04c2203b2efff9ee98d0a2f41ca
pclaesen <pieterclaesen@gmail.com>
2024-02-21 15:48:47 +0000

    Add basic data

---
+++ packages/payment-processor/src/payment/paytr.ts
@@ -0,0 +1,16 @@
+// import { constants, ContractTransaction, Signer, BigNumberish, providers, BigNumber } from 'ethers';
+
+// import { ClientTypes, ExtensionTypes } from '@requestnetwork/types';
+// import { getPaymentNetworkExtension } from '@requestnetwork/payment-detection';
+// import { EvmChains } from '@requestnetwork/currency';
+
+// import { ITransactionOverrides } from './transaction-overrides';
+// import {
+//   getAmountToPay,
+//   getProvider,
+//   getRequestPaymentValues,
+//   getSigner,
+//   validateErc20FeeProxyRequest,
+//   validateRequest,
+// } from './utils';
+// import { IPreparedTransaction } from './prepared-transaction';
