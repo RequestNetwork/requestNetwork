ebf1d88ac1dab6c53a9ae193941ff8b597aa6506
pclaesen <pieterclaesen@gmail.com>
2024-02-22 08:15:33 +0000

    Add Paytr interface

--- packages/payment-processor/src/payment/paytr.ts
+++ packages/payment-processor/src/payment/paytr.ts
@@ -1,4 +1,4 @@
-// import { constants, ContractTransaction, Signer, BigNumberish, providers, BigNumber } from 'ethers';
+// import { constants, ContractTransaction, Signer, BigNumberish, providers, BigNumber, ethers } from 'ethers';
 
 // import { ClientTypes, ExtensionTypes } from '@requestnetwork/types';
 // import { getPaymentNetworkExtension } from '@requestnetwork/payment-detection';
@@ -15,29 +15,29 @@
 // } from './utils';
 // import { IPreparedTransaction } from './prepared-transaction';
 
-//Paytr custom import
-//import { abi as ABI_0_1_0 } from './../../../smart-contracts/src/lib/artifacts/Paytr/0.1.0.json';
+// //Paytr custom import
+// import { abi as ABI_0_1_0 } from './../../../smart-contracts/src/lib/artifacts/Paytr/0.1.0.json';
 
-export function encodePayErc20FeeRequest(
-  request: ClientTypes.IRequestData,
-  amount?: BigNumberish,
-  feeAmountOverride?: BigNumberish,
-): string {
-  validateErc20FeeProxyRequest(request, amount, feeAmountOverride);
+// export function encodePayErc20FeeRequest(
+//   request: ClientTypes.IRequestData,
+//   amount?: BigNumberish,
+//   feeAmountOverride?: BigNumberish,
+// ): string {
+//   validateErc20FeeProxyRequest(request, amount, feeAmountOverride);
 
-  const tokenAddress = request.currencyInfo.value;
-  const { paymentReference, paymentAddress, feeAddress, feeAmount } =
-    getRequestPaymentValues(request);
-  const amountToPay = getAmountToPay(request, amount);
-  const feeToPay = BigNumber.from(feeAmountOverride || feeAmount || 0);
-  //const proxyContract = ERC20FeeProxy__factory.createInterface();
+//   const tokenAddress = request.currencyInfo.value;
+//   const { paymentReference, paymentAddress, feeAddress, feeAmount } =
+//     getRequestPaymentValues(request);
+//   const amountToPay = getAmountToPay(request, amount);
+//   const feeToPay = BigNumber.from(feeAmountOverride || feeAmount || 0);
+//   const paytrInterface = new ethers.utils.Interface(ABI_0_1_0);
 
-  return proxyContract.encodeFunctionData('transferFromWithReferenceAndFee', [
-    tokenAddress,
-    paymentAddress,
-    amountToPay,
-    `0x${paymentReference}`,
-    feeToPay,
-    feeAddress || constants.AddressZero,
-  ]);
-}
+//   return paytrInterface.encodeFunctionData('payOutERC20Invoice', [
+//     tokenAddress,
+//     paymentAddress,
+//     amountToPay,
+//     `0x${paymentReference}`,
+//     feeToPay,
+//     feeAddress || constants.AddressZero,
+//   ]);
+// }
