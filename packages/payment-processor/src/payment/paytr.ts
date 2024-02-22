0b9755fb47b5e0c519a9e95c0c37693775d463c1
pclaesen <pieterclaesen@gmail.com>
2024-02-22 06:45:22 +0100

    WIP

--- packages/payment-processor/src/payment/paytr.ts
+++ packages/payment-processor/src/payment/paytr.ts
@@ -14,3 +14,30 @@
 //   validateRequest,
 // } from './utils';
 // import { IPreparedTransaction } from './prepared-transaction';
+
+//Paytr custom import
+//import { abi as ABI_0_1_0 } from './../../../smart-contracts/src/lib/artifacts/Paytr/0.1.0.json';
+
+export function encodePayErc20FeeRequest(
+  request: ClientTypes.IRequestData,
+  amount?: BigNumberish,
+  feeAmountOverride?: BigNumberish,
+): string {
+  validateErc20FeeProxyRequest(request, amount, feeAmountOverride);
+
+  const tokenAddress = request.currencyInfo.value;
+  const { paymentReference, paymentAddress, feeAddress, feeAmount } =
+    getRequestPaymentValues(request);
+  const amountToPay = getAmountToPay(request, amount);
+  const feeToPay = BigNumber.from(feeAmountOverride || feeAmount || 0);
+  //const proxyContract = ERC20FeeProxy__factory.createInterface();
+
+  return proxyContract.encodeFunctionData('transferFromWithReferenceAndFee', [
+    tokenAddress,
+    paymentAddress,
+    amountToPay,
+    `0x${paymentReference}`,
+    feeToPay,
+    feeAddress || constants.AddressZero,
+  ]);
+}
