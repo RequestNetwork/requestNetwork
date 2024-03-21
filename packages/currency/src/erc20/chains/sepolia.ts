cb43329d012870c6736a104a49072eba53a970b8
Olivier Delfosse <55892112+olivier7delf@users.noreply.github.com>
2024-03-21 12:36:59 +0100

    add usdc sepolia currency

--- packages/currency/src/erc20/chains/sepolia.ts
+++ packages/currency/src/erc20/chains/sepolia.ts
@@ -13,4 +13,9 @@ export const supportedSepoliaERC20: TokenMap = {
     name: 'FakeUSDT',
     symbol: 'fUSDT',
   },
+  '0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238': {
+    decimals: 18,
+    name: 'FakeUSDC',
+    symbol: 'fUSDC',
+  },
 };
