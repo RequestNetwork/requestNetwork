e440a3eaefaaab5e3ae0a838df9b00e56becafaa
Leo Sollier <leo.sollier@request.network>
2024-06-04 05:28:07 +0200

    fixes

--- packages/types/src/extensions/pn-meta.ts
+++ packages/types/src/extensions/pn-meta.ts
@@ -1,12 +1,6 @@
 // import * as Extension from '../extension-types';
 import { ExtensionTypes } from '..';
-import {
-  PnAnyDeclarative,
-  PnAnyToAnyConversion,
-  PnAnyToErc20,
-  PnAnyToEth,
-  PnFeeReferenceBased,
-} from '../extension-types';
+import { PnAnyDeclarative, PnAnyToErc20, PnAnyToEth } from '../extension-types';
 
 /** Manager of the extension */
 export interface IMeta<TCreationParameters = ICreationParameters>
@@ -21,14 +15,9 @@ export interface IMeta<TCreationParameters = ICreationParameters>
 
 /** Parameters of creation action */
 export interface ICreationParameters extends PnAnyDeclarative.ICreationParameters {
-  [ExtensionTypes.PAYMENT_NETWORK_ID
-    .ERC20_FEE_PROXY_CONTRACT]?: PnFeeReferenceBased.ICreationParameters[];
+  [ExtensionTypes.PAYMENT_NETWORK_ID.ANY_DECLARATIVE]?: PnAnyDeclarative.ICreationParameters[];
   [ExtensionTypes.PAYMENT_NETWORK_ID.ANY_TO_ERC20_PROXY]?: PnAnyToErc20.ICreationParameters[];
-  [ExtensionTypes.PAYMENT_NETWORK_ID
-    .ETH_FEE_PROXY_CONTRACT]?: PnFeeReferenceBased.ICreationParameters[];
   [ExtensionTypes.PAYMENT_NETWORK_ID.ANY_TO_ETH_PROXY]?: PnAnyToEth.ICreationParameters[];
-  [ExtensionTypes.PAYMENT_NETWORK_ID
-    .ANY_TO_NATIVE_TOKEN]?: PnAnyToAnyConversion.ICreationParameters[];
 }
 
 /** Parameters of declareSentPayment and declareSentRefund action */
