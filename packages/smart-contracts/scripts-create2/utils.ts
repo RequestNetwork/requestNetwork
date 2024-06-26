5d843e156abc3a3b8f271af2f2f5a22faccd0221
MantisClone <david.huntmateo@request.network>
2024-06-26 19:55:42 -0400

    chore: deploy ERC20TransferableReceivable to Sepolia

--- packages/smart-contracts/scripts-create2/utils.ts
+++ packages/smart-contracts/scripts-create2/utils.ts
@@ -8,18 +8,18 @@ import { EvmChains } from '@requestnetwork/currency';
  * If you want to skip deploying one or more, then comment them out in the list bellow.
  */
 export const create2ContractDeploymentList = [
-  'ChainlinkConversionPath',
-  'EthereumProxy',
-  'EthereumFeeProxy',
-  'EthConversionProxy',
-  'ERC20Proxy',
-  'ERC20FeeProxy',
-  'Erc20ConversionProxy',
-  'ERC20SwapToPay',
-  'ERC20SwapToConversion',
-  'BatchConversionPayments',
-  'ERC20EscrowToPay',
-  // 'ERC20TransferableReceivable',
+  // 'ChainlinkConversionPath',
+  // 'EthereumProxy',
+  // 'EthereumFeeProxy',
+  // 'EthConversionProxy',
+  // 'ERC20Proxy',
+  // 'ERC20FeeProxy',
+  // 'Erc20ConversionProxy',
+  // 'ERC20SwapToPay',
+  // 'ERC20SwapToConversion',
+  // 'BatchConversionPayments',
+  // 'ERC20EscrowToPay',
+  'ERC20TransferableReceivable',
 ];
 
 /**
