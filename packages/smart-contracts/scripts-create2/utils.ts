15c5f1d842e85a99e7c7cde557bcd0babe2bde81
MantisClone <david.huntmateo@request.network>
2024-06-27 06:53:24 -0400

    Uncomment all contracts
    Co-authored-by: Alexandre ABRIOUX <alexandre-abrioux@users.noreply.github.com>

--- packages/smart-contracts/scripts-create2/utils.ts
+++ packages/smart-contracts/scripts-create2/utils.ts
@@ -8,17 +8,17 @@ import { EvmChains } from '@requestnetwork/currency';
  * If you want to skip deploying one or more, then comment them out in the list bellow.
  */
 export const create2ContractDeploymentList = [
-  // 'ChainlinkConversionPath',
-  // 'EthereumProxy',
-  // 'EthereumFeeProxy',
-  // 'EthConversionProxy',
-  // 'ERC20Proxy',
-  // 'ERC20FeeProxy',
-  // 'Erc20ConversionProxy',
-  // 'ERC20SwapToPay',
-  // 'ERC20SwapToConversion',
-  // 'BatchConversionPayments',
-  // 'ERC20EscrowToPay',
+  'ChainlinkConversionPath',
+  'EthereumProxy',
+  'EthereumFeeProxy',
+  'EthConversionProxy',
+  'ERC20Proxy',
+  'ERC20FeeProxy',
+  'Erc20ConversionProxy',
+  'ERC20SwapToPay',
+  'ERC20SwapToConversion',
+  'BatchConversionPayments',
+  'ERC20EscrowToPay',
   'ERC20TransferableReceivable',
 ];
 
