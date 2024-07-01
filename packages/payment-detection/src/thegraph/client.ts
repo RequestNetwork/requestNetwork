c69c898e3672f1bc092197a41e247ba761ef6fe8
MantisClone <david.huntmateo@request.network>
2024-07-01 19:16:56 -0400

    chore: add payment subgraph URLs from Alchemy

--- packages/payment-detection/src/thegraph/client.ts
+++ packages/payment-detection/src/thegraph/client.ts
@@ -9,8 +9,8 @@ import { RequestConfig } from 'graphql-request/src/types';
 const THE_GRAPH_STUDIO_URL =
   'https://api.studio.thegraph.com/query/67444/request-payments-$NETWORK/version/latest';
 
-const THE_GRAPH_STUDIO_URL_BASE =
-  'https://api.studio.thegraph.com/query/67444/request-payments-base-2/version/latest';
+const THE_GRAPH_ALCHEMY_URL =
+  'https://subgraph.satsuma-prod.com/e2e4905ab7c8/request-network--434873/request-payments-$NETWORK/api';
 
 const THE_GRAPH_URL_MANTLE_TESTNET =
   'https://graph.testnet.mantle.xyz/subgraphs/name/requestnetwork/request-payments-mantle-testnet';
@@ -101,13 +101,19 @@ export const defaultGetTheGraphClient = (
         `${THE_GRAPH_STUDIO_URL.replace('$NETWORK', network.replace('aurora', 'near'))}`,
         options,
       )
-    : network === 'base'
-    ? getTheGraphEvmClient(THE_GRAPH_STUDIO_URL_BASE, options)
     : network === 'mantle'
     ? getTheGraphEvmClient(THE_GRAPH_URL_MANTLE, options)
     : network === 'mantle-testnet'
     ? getTheGraphEvmClient(THE_GRAPH_URL_MANTLE_TESTNET, options)
     : network === 'core'
     ? getTheGraphEvmClient(THE_GRAPH_URL_CORE, options)
+    : network === 'mainnet' ||
+      network === 'sepolia' ||
+      network === 'matic' ||
+      network === 'bsc' ||
+      network === 'optimism' ||
+      network === 'arbitrum-one' ||
+      network === 'base'
+    ? getTheGraphEvmClient(`${THE_GRAPH_ALCHEMY_URL.replace('$NETWORK', network)}`, options)
     : getTheGraphEvmClient(`${THE_GRAPH_STUDIO_URL.replace('$NETWORK', network)}`, options);
 };
