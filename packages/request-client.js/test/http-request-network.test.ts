638d730f364bd6bcd144fca0f6a2f543b6d479fb
Aimen Sahnoun <aimensahnoun@outlook.com>
2024-07-16 17:21:54 +0300

    test: increase failAtCall

--- packages/request-client.js/test/http-request-network.test.ts
+++ packages/request-client.js/test/http-request-network.test.ts
@@ -94,7 +94,7 @@ describe('HttpRequestNetwork', () => {
     });
 
     it('increase the expected amount', async () => {
-      failAtCall(6);
+      failAtCall(9);
       console.log('Creating request...');
       const request = await createRequest();
       console.log('Waiting for confirmation...');
