f98af25b2386f31cdef45aaba3e89ca8e6477c71
Aimen Sahnoun <aimensahnoun@outlook.com>
2024-07-16 18:26:20 +0300

    feat: create `no-persist-http-data-access` and implement it

--- packages/request-client.js/src/http-request-network.ts
+++ packages/request-client.js/src/http-request-network.ts
@@ -11,6 +11,7 @@ import RequestNetwork from './api/request-network';
 import HttpDataAccess, { NodeConnectionConfig } from './http-data-access';
 import { MockDataAccess } from '@requestnetwork/data-access';
 import { MockStorage } from './mock-storage';
+import { NoPersistHttpDataAccess } from './no-persist-http-data-access';
 
 /**
  * Exposes RequestNetwork module configured to use http-data-access.
@@ -23,8 +24,9 @@ export default class HttpRequestNetwork extends RequestNetwork {
    * @param options.nodeConnectionConfig Configuration options to connect to the node.
    * @param options.useMockStorage When true, will use a mock storage in memory. Meant to simplify local development and should never be used in production.
    * @param options.signatureProvider Module to handle the signature. If not given it will be impossible to create new transaction (it requires to sign).
-   * @param options.currencies custom currency list
-   * @param options.currencyManager custom currency manager (will override `currencies`)
+   * @param options.currencies custom currency list.
+   * @param options.currencyManager custom currency manager (will override `currencies`).
+   * @param options.skipPersistence allows to create a transaction without persisting it.
    */
   constructor(
     {
@@ -35,6 +37,7 @@ export default class HttpRequestNetwork extends RequestNetwork {
       useMockStorage,
       currencyManager,
       paymentOptions,
+      skipPersistence,
     }: {
       decryptionProvider?: DecryptionProviderTypes.IDecryptionProvider;
       httpConfig?: Partial<ClientTypes.IHttpDataAccessConfig>;
@@ -43,13 +46,20 @@ export default class HttpRequestNetwork extends RequestNetwork {
       useMockStorage?: boolean;
       currencyManager?: CurrencyTypes.ICurrencyManager;
       paymentOptions?: Partial<PaymentNetworkOptions>;
+      skipPersistence?: boolean;
     } = {
       httpConfig: {},
       useMockStorage: false,
+      skipPersistence: false,
     },
   ) {
     const dataAccess: DataAccessTypes.IDataAccess = useMockStorage
       ? new MockDataAccess(new MockStorage())
+      : skipPersistence
+      ? new NoPersistHttpDataAccess({
+          httpConfig,
+          nodeConnectionConfig,
+        })
       : new HttpDataAccess({ httpConfig, nodeConnectionConfig });
 
     if (!currencyManager) {
---
+++ packages/request-client.js/src/no-persist-http-data-access.ts
@@ -0,0 +1,45 @@
+import HttpDataAccess, { NodeConnectionConfig } from './http-data-access';
+import { ClientTypes, DataAccessTypes, StorageTypes } from '@requestnetwork/types';
+import { EventEmitter } from 'events';
+
+export class NoPersistHttpDataAccess extends HttpDataAccess {
+  constructor(
+    {
+      httpConfig,
+      nodeConnectionConfig,
+    }: {
+      httpConfig?: Partial<ClientTypes.IHttpDataAccessConfig>;
+      nodeConnectionConfig?: Partial<NodeConnectionConfig>;
+    } = {
+      httpConfig: {},
+      nodeConnectionConfig: {},
+    },
+  ) {
+    super({ httpConfig, nodeConnectionConfig });
+  }
+
+  async persistTransaction(
+    transactionData: DataAccessTypes.ITransaction,
+    channelId: string,
+    topics?: string[],
+  ): Promise<DataAccessTypes.IReturnPersistTransaction> {
+    const data: DataAccessTypes.IReturnPersistTransactionRaw = {
+      meta: {
+        topics: topics || [],
+        transactionStorageLocation: '',
+        storageMeta: {
+          state: StorageTypes.ContentState.PENDING,
+          timestamp: Date.now() / 1000,
+        },
+      },
+      result: {},
+    };
+
+    const result: DataAccessTypes.IReturnPersistTransaction = Object.assign(
+      new EventEmitter() as DataAccessTypes.PersistTransactionEmitter,
+      data,
+    );
+
+    return result;
+  }
+}
