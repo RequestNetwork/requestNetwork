2a4a47b3a5518aa02caee79b4705a093ecebc02c
MantisClone <david.huntmateo@request.network>
2024-10-01 23:24:13 -0400

    Fix test according to CodeRabbit recommendation

--- packages/request-client.js/test/http-data-access.test.ts
+++ packages/request-client.js/test/http-data-access.test.ts
@@ -30,12 +30,18 @@ describe('HttpDataAccess', () => {
         },
       });
       void httpDataAccess.persistTransaction({}, '', []).then((returnPersistTransaction) => {
+        let errorEmitted = false;
         returnPersistTransaction.on('error', (e: any) => {
+          errorEmitted = true;
           expect(e.message).toBe(
             `The Request Network SDK timed-out while polling the Request Node to confirm that the Request was persisted successfully. It is likely that the persisted Request will be confirmed eventually. App Builders are discouraged from calling persistTransaction() a second time, which would create a duplicate Request. Instead, App Builders are recommended to catch this error and continue polling the Request Node using getConfirmedTransaction() or by calling the /getConfirmedTransaction endpoint.  To avoid timeouts in the future, try adjusting the httpConfig values when instantiating the RequestNetwork object: getConfirmationDeferDelay: 0ms, getConfirmationMaxRetries: 0, getConfirmationRetryDelay: 1000ms, getConfirmationExponentialBackoffDelay: 0ms, getConfirmationMaxExponentialBackoffDelay: 30000ms`,
           );
-          done();
         });
+
+        setTimeout(() => {
+          expect(errorEmitted).toBe(true);
+          done();
+        }, 1000);
       });
     });
   });
