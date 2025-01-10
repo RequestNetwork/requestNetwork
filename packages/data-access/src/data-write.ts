dcf76d956c84a64b51b59c543df857808740fdb3
Kevin Dave <kevindavee1505@gmail.com>
2025-01-10 11:12:53 +0700

    remove persist flag

--- packages/data-access/src/data-write.ts
+++ packages/data-access/src/data-write.ts
@@ -1,13 +1,11 @@
 import { EventEmitter } from 'events';
 import Block from './block';
 import { DataAccessTypes, StorageTypes } from '@requestnetwork/types';
-import { getNoPersistTransactionRawData } from './no-persist-transaction-raw-data';
 
 export class DataAccessWrite implements DataAccessTypes.IDataWrite {
   constructor(
     protected readonly storage: StorageTypes.IStorageWrite,
     private readonly pendingStore?: DataAccessTypes.IPendingStore,
-    public readonly persist: boolean = true,
   ) {
     this.pendingStore = pendingStore;
   }
@@ -25,19 +23,6 @@ export class DataAccessWrite implements DataAccessTypes.IDataWrite {
     channelId: string,
     topics?: string[] | undefined,
   ): Promise<DataAccessTypes.IReturnPersistTransaction> {
-    const eventEmitter = new EventEmitter() as DataAccessTypes.PersistTransactionEmitter;
-
-    if (!this.persist) {
-      const result: DataAccessTypes.IReturnPersistTransaction = Object.assign(
-        eventEmitter,
-        getNoPersistTransactionRawData(topics),
-      );
-
-      // Emit confirmation instantly since data is not going to be persisted
-      result.emit('confirmed', result);
-      return result;
-    }
-
     const updatedBlock = Block.pushTransaction(
       Block.createEmptyBlock(),
       transaction,
@@ -47,6 +32,7 @@ export class DataAccessWrite implements DataAccessTypes.IDataWrite {
 
     const storageResult = await this.storage.append(JSON.stringify(updatedBlock));
 
+    const eventEmitter = new EventEmitter() as DataAccessTypes.PersistTransactionEmitter;
     this.pendingStore?.add(channelId, { transaction, storageResult, topics: topics || [] });
 
     const result: DataAccessTypes.IReturnPersistTransactionRaw = {
