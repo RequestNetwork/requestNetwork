ee54c3f1ca9a626061f7df61590018735ac145c7
Aimen Sahnoun <aimensahnoun@outlook.com>
2025-03-28 19:49:15 +0400

    chore(jest): update transformIgnorePatterns to include specific modules for Jest configuration

--- packages/thegraph-data-access/jest.config.js
+++ packages/thegraph-data-access/jest.config.js
@@ -3,4 +3,7 @@ const jestCommonConfig = require('../../jest.config');
 /** @type {import('jest').Config} */
 module.exports = {
   ...jestCommonConfig,
+  transformIgnorePatterns: [
+    '/node_modules/(?!(graphql-request|@graphql-typed-document-node|cross-fetch|extract-files|form-data)/)',
+  ],
 };
