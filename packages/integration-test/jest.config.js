a8c86af4a5a800e8bac1c0a0127b609b0b77e81c
Aimen Sahnoun <aimensahnoun@outlook.com>
2025-03-28 22:39:59 +0400

    chore(jest): update Jest configuration for integration tests to include ts-jest transformations and adjust transformIgnorePatterns

--- packages/integration-test/jest.config.js
+++ packages/integration-test/jest.config.js
@@ -8,4 +8,19 @@ const jestCommonConfig = require('../../jest.config');
 /** @type {import('jest').Config} */
 module.exports = {
   ...jestCommonConfig,
+  transform: {
+    '^.+\\.(ts|tsx)$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/tsconfig.json',
+      },
+    ],
+    'node_modules/graphql-request/.+\\.(j|t)s$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/tsconfig.json',
+      },
+    ],
+  },
+  transformIgnorePatterns: ['/node_modules/(?!graphql-request)/'],
 };
