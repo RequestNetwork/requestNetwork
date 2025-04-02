56e35ddb35cb4922c44daa2a4e385912e8d6b41a
Aimen Sahnoun <aimensahnoun@outlook.com>
2025-04-02 12:05:51 +0400

    chore: add package-lock.json and update yarn.lock for consistent dependency resolution; remove TypeScript preset from Babel configurations in integration-test and request-node packages

--- packages/integration-test/babel.config.js
+++ packages/integration-test/babel.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  presets: [['@babel/preset-env', { targets: { node: 'current' } }], '@babel/preset-typescript'],
+  presets: [['@babel/preset-env', { targets: { node: 'current' } }]],
   plugins: ['@babel/plugin-transform-runtime', '@babel/plugin-transform-modules-commonjs'],
 };
