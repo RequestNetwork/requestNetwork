eac9c9a1492085aa7a8aa5591e2ed09119d852d4
Alexandre ABRIOUX <alexandre-abrioux@users.noreply.github.com>
2025-04-13 00:49:43 +0200

    sinplify

--- packages/utils/src/crypto/ec-utils-legacy.ts
+++ packages/utils/src/crypto/ec-utils-legacy.ts
@@ -43,7 +43,7 @@ const deriveSharedKeyWithSha512 = (
  * https://github.com/RequestNetwork/requestNetwork/blob/4597d373b0284787273471cf306dd9b849c9f76a/packages/utils/src/crypto/ec-utils.ts#L141
  */
 const legacyAes256CbcMacSplit = (str: string) => {
-  const buf = Buffer.from(str, 'hex');
+  const buffer = Buffer.from(str, 'hex');
 
   const ivSize = 16;
   const ephemPublicKeySize = 33;
@@ -51,13 +51,13 @@ const legacyAes256CbcMacSplit = (str: string) => {
   const macSize = 32;
   const macEnd = ephemPublicKeyEnd + macSize;
 
-  const ephemPublicKeyStr = buf.subarray(ivSize, ephemPublicKeyEnd);
+  const ephemPublicKeyStr = buffer.subarray(ivSize, ephemPublicKeyEnd);
   const ephemPublicKey = new PublicKey(ephemPublicKeyStr);
 
   return {
-    iv: Buffer.from(buf.toString('hex', 0, ivSize), 'hex'),
-    mac: Buffer.from(buf.toString('hex', ephemPublicKeyEnd, macEnd), 'hex'),
-    ciphertext: Buffer.from(buf.toString('hex', macEnd, buf.length), 'hex'),
+    iv: buffer.slice(0, ivSize),
+    mac: buffer.slice(ephemPublicKeyEnd, macEnd),
+    ciphertext: buffer.slice(macEnd),
     ephemPublicKey,
   };
 };
