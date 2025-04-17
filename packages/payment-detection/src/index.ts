008fefeda302e91327e5420152d647d5750757ff
Alexandre ABRIOUX <alexandre-abrioux@users.noreply.github.com>
2025-04-17 20:17:11 +0200

    fix isolatedmodule errors

--- packages/payment-detection/src/index.ts
+++ packages/payment-detection/src/index.ts
@@ -8,16 +8,17 @@ import * as PaymentReferenceCalculator from './payment-reference-calculator';
 import * as BtcPaymentNetwork from './btc';
 import { DeclarativePaymentDetector } from './declarative';
 import * as Erc20PaymentNetwork from './erc20';
+import { ERC20TransferableReceivablePaymentDetector } from './erc20';
 import { AnyToERC20PaymentDetector, AnyToEthFeeProxyPaymentDetector } from './any';
 import { EthFeeProxyPaymentDetector, EthInputDataPaymentDetector } from './eth';
 import { getTheGraphClient, getTheGraphEvmClient, getTheGraphNearClient } from './thegraph';
 import {
   calculateEscrowState,
+  flattenRequestByPnId,
   formatAddress,
   getPaymentNetworkExtension,
   getPaymentReference,
   getPaymentReferencesForMetaPnRequest,
-  flattenRequestByPnId,
   hashReference,
   padAmountForChainlink,
   parseLogArgs,
@@ -29,12 +30,11 @@ import { SuperFluidPaymentDetector } from './erc777/superfluid-detector';
 import { EscrowERC20InfoRetriever } from './erc20/escrow-info-retriever';
 import { SuperFluidInfoRetriever } from './erc777/superfluid-retriever';
 import { PaymentNetworkOptions } from './types';
-import { ERC20TransferableReceivablePaymentDetector } from './erc20';
 import { MetaDetector } from './meta-payment-detector';
 
 export type { TheGraphClient } from './thegraph';
 
-export {
+export type {
   PaymentNetworkFactory,
   PaymentNetworkOptions,
   PaymentReferenceCalculator,
