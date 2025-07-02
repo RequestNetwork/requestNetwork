c066310234475a1988a6ec457a1899638f8428de
Aimen Sahnoun <aimensahnoun@outlook.com>
2025-07-02 16:13:28 +0400

    fix(ERC20RecurringPaymentProxy): correct Solidity version declaration

--- packages/smart-contracts/src/contracts/ERC20RecurringPaymentProxy.sol
+++ packages/smart-contracts/src/contracts/ERC20RecurringPaymentProxy.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
+pragma solidity 0.8.9;
 
 import '@openzeppelin/contracts/access/AccessControl.sol';
 import '@openzeppelin/contracts/security/Pausable.sol';
