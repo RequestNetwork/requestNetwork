f60d860e94d59c2818ccae85b08a2673f84db8eb
LeoSlrRf <leo.sollier+gh@request.finance>
2025-11-18 17:42:52 +0100

    fix: sui erc20 address validation

--- packages/currency/src/currencyManager.ts
+++ packages/currency/src/currencyManager.ts
@@ -360,6 +360,9 @@ export class CurrencyManager<TMeta = unknown> implements CurrencyTypes.ICurrency
    */
   validateSuiAddress(address: string): boolean {
     try {
+      if (address.includes(':')) {
+        return !!isValidSuiAddress(address.split(':')[0]);
+      }
       return !!isValidSuiAddress(address);
     } catch {
       return false;
