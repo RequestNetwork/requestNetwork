065c477d4c96c08b69330bd589c426975e7af982
rodrigopavezi <rodrigopavezi@gmail.com>
2026-01-26 23:18:56 -0300

    fix: address remaining PR review comments
    Proto package directory mismatch:
    - Move payments.proto to proto/request/tron/v1/ to match package declaration
    - Update build.rs and substreams.yaml with new path
    
    Transaction success validation:
    - Add TronTransactionInfo interface
    - Add getTransactionInfo method to TronWeb interface
    - Add waitForTransactionConfirmation helper function
    - Add optional waitForConfirmation parameter to approveTrc20 and processTronFeeProxyPayment

--- packages/substreams-tron/build.rs
+++ packages/substreams-tron/build.rs
@@ -1,6 +1,6 @@
 //! Build script for generating protobuf types
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    prost_build::compile_protos(&["proto/payments.proto"], &["proto/"])?;
+    prost_build::compile_protos(&["proto/request/tron/v1/payments.proto"], &["proto/"])?;
     Ok(())
 }
